619dfdcff8fa1105cd7886870e141af0e4290b50
Ashish Sood <Ashish.Sood@synchronoss.com>
2016-11-03 19:42:17 +0530

    Changes for automation code: Change the project name in pom.xml packaging type set to jar Changes to exception handling

--- saw-ui-config-generator/src/com/synchronoss/main/ExcelToJsonConvertor.java
+++ saw-ui-config-generator/src/com/synchronoss/main/ExcelToJsonConvertor.java
@@ -1,9 +1,12 @@
 package com.synchronoss.main;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
+
+import org.apache.log4j.Logger;
 
 import com.google.gson.Gson;
 import com.synchronoss.entity.UIArtifact;
@@ -20,7 +23,7 @@ public class ExcelToJsonConvertor {
 	public String [] requiredExcelColArr;
 	public String [] artifactArray;
 	public String [] dashboardNameArray;
-
+	private static Logger log = Logger.getLogger(ExcelToJsonConvertor.class.getName());
 	public HashMap<String, Integer> jsonColIndexMap = new HashMap<String, Integer>();
 
 	public ExcelToJsonConvertor() {
@@ -52,8 +55,10 @@ public class ExcelToJsonConvertor {
 		}
 		if(valuesFound==requiredExcelColArr.length){
 			isFileValid = true;
+			log.info("File is validated");
 		}else{
-			System.out.println("File not Valid");
+			log.error("File not Valid! Either of the columns: "+Utility.getPropertyValue("requiredExcelColArr")+" not present ");
+			System.exit(1);
 		}
 		return isFileValid;
 	}
@@ -64,7 +69,12 @@ public class ExcelToJsonConvertor {
 	 */
 	public void createJsonObject(){
 		Gson gson = new Gson();
-		HashMap<Integer, HashMap<Integer, String>> excelData = Utility.readExcelFile(filePath);
+		HashMap<Integer, HashMap<Integer, String>> excelData = null;
+		try {
+			excelData = Utility.readExcelFile(filePath);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		if(validateExcelSheet(excelData.get(0))){
 			populateJsonColIndexMap(excelData.get(0));
 			excelData.remove(0);
@@ -72,7 +82,11 @@ public class ExcelToJsonConvertor {
 			for (int i = 0; i < dashboardNameArray.length; i++) {
 				String dashboardName = dashboardNameArray[i];
 				ArrayList<HashMap<Integer, String>> dashboardData = revisedData.get(dashboardName);
-				Utility.writeJsonFile(jsonFileOutputPath, dashboardName, gson.toJson(artifactListMapGenerator(dashboardData,dashboardName)));
+				try {
+					Utility.writeJsonFile(jsonFileOutputPath, dashboardName, gson.toJson(artifactListMapGenerator(dashboardData,dashboardName)));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
