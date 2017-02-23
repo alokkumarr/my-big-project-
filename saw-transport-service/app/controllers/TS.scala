450cf0d20dd178b7de0f321edf2cb5a3faf10bb0
Sergey Ryabov <srya0001@synchronoss.com>
2017-02-23 13:41:22 -0500

    SAW-434

--- saw-transport-service/app/controllers/TS.scala
+++ saw-transport-service/app/controllers/TS.scala
@@ -1,10 +1,9 @@
 package controllers
 
 import com.fasterxml.jackson.databind.node.ObjectNode
-import org.slf4j.{Logger, LoggerFactory}
 import play.api.libs.json.JsValue
 import play.libs.Json
-import play.mvc.{Controller, Http, Result}
+import play.mvc.Result
 import sncr.es.ESQueryHandler
 import sncr.metadata.MetadataDictionary
 import sncr.request.Extractor
@@ -14,63 +13,14 @@ import sncr.security.TokenValidator
 /**
   * Created by srya0001 on 6/28/2016.
   */
-class TS extends Controller {
+class TS extends BaseServiceProvider {
 
-  val m_log: Logger = LoggerFactory.getLogger(classOf[TS].getName)
-
-  def handleEmptyRequest( msg: String ) : Result = {
-    val ctx: Http.Context = Http.Context.current.get
-    val res: ObjectNode = Json.newObject
-    res.put("result", "failure")
-    res.put("reason", "empty request")
-    m_log.debug(s"Empty request with $msg content type came from: ${ctx.request().host()}/${ctx.request().username()}")
-    play.mvc.Results.badRequest(res)
-  }
-
-  def query: Result = {
-
-    val ctx: Http.Context = Http.Context.current.get
-
-    val header = ctx._requestHeader()
-
-    val res: ObjectNode = Json.newObject
-
-    header.contentType match
-    {
-      case None  => if (ctx.request.body.asText == null) handleEmptyRequest("text/plain")
-                           else process(ctx.request.body.asText())
-      case _ =>  header.contentType.get match {
-        case "text/plain" => if (ctx.request.body.asText == null) handleEmptyRequest("text/plain")
-                                    else process(ctx.request.body.asText())
-        case "application/x-www-form-urlencoded" => if (ctx.request.body.asFormUrlEncoded == null) handleEmptyRequest("application/x-www-form-urlencoded")
-                                    else {
-                                        val requestBody =  ctx.request.body.asFormUrlEncoded().toString
-                                        m_log debug s"URL encoded: $requestBody"
-                                        process(requestBody)
-                                    }
-        case "application/json" => if (ctx.request.body.asJson == null) handleEmptyRequest("application/json")
-                                          else { val body = ctx.request.body.asJson
-//                                                      m_log debug ( "App/JSON: " + body.toString)
-                                                     process(body.toString)
-                                               }
-        case "octet/stream" =>  if (ctx.request.body.asBytes.toArray == null) handleEmptyRequest("octet/stream")
-                                       else process(ctx.request.body.asBytes.toArray)
-        case _ =>
-              res.put("result", "failure")
-              res.put("reason", s"Unsupported content type: ${header.contentType}")
-              m_log.debug(s"Unprocessed request: ${ctx._requestHeader.rawQueryString}")
-              play.mvc.Results.badRequest(res)
-
-      }
-    }
-  }
-
-  def process(arr: Array[Byte]): Result =
+  override def process(arr: Array[Byte]): Result =
   {
-     process(play.api.libs.json.Json.parse(arr))
+    process(play.api.libs.json.Json.parse(arr))
   }
 
-  def process(txt: String): Result =
+  override def process(txt: String): Result =
   {
     process(play.api.libs.json.Json.parse(txt))
   }
