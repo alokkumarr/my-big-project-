3eef780b0e0f9814d81cc59ad5f071dbbb0d8494
Barna Mumtyan <Barna.Mumtya@synchronoss.com>
2017-03-06 09:42:04 +0200

    Going to the analyze pages should now redirect to the last viewed child, or to the first canned analysis page.

--- src/main/javascript/app/modules/analyze/transitions.js
+++ src/main/javascript/app/modules/analyze/transitions.js
@@ -1,37 +1,15 @@
 import pick from 'lodash/pick';
+import {LAST_ANALYSES_CATEGORY_ID} from './consts';
 
-export function transitions($transitions, localStorageService, $state, $timeout) {
+export function transitions($transitions, localStorageService) {
   'ngInject';
-  const key = 'lastAnalysesListId';
-
-  let firstPageLoad = true;
-
-  $transitions.onEnter({entering: 'analyze'}, onEnterAnalyze);
   $transitions.onEnter({entering: 'analyze.view'}, onEnterAnalyzeView);
-  $transitions.onEnter({entering: state => state.name.match(/analyze.\w*/)}, setFirstPageLoadFalse);
-
-  function onEnterAnalyze(transition, state) {
-    firstPageLoad = true;
-    // we have to use timeout, because this listener fires when we go to a child as well
-    $timeout(() => {
-      if (firstPageLoad) {
-        const id = localStorageService.get(key);
-        if (id) {
-          $state.go('analyze.view', {id});
-        }
-      }
-    });
-  }
 
   function onEnterAnalyzeView(transition, state) {
-
     const analysesId = transition.params().id;
-    if (analysesId) {
-      localStorageService.set(key, analysesId);
-    }
-  }
 
-  function setFirstPageLoadFalse(transition, state) {
-    firstPageLoad = false;
+    if (analysesId) {
+      localStorageService.set(LAST_ANALYSES_CATEGORY_ID, analysesId);
+    }
   }
 }
