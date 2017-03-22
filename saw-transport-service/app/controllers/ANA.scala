ed1f82bde6fcbdfd89cd2aeda4f96816d1dd816b
Markus Holmberg <markus.holmberg@synchronoss.com>
2017-03-22 11:08:15 -0400

    SAW-499: Add update and delete analysis operations
    Also refactor tests to remove duplication of functionality.

--- saw-transport-service/app/controllers/ANA.scala
+++ saw-transport-service/app/controllers/ANA.scala
@@ -31,24 +31,20 @@ class ANA extends BaseServiceProvider {
     val action = (json \ "contents" \ "action").extract[String].toLowerCase
     val response = action match {
       case "create" => {
-        val analysisListJson = json \ "contents" \ "analysis"
-        val analysisJson = analysisListJson match {
-          case array: JArray => {
-            if (array.arr.length > 1) {
-              throw new RuntimeException("Only one element supported")
-            }
-            if (array.arr.length == 0) {
-              throw new RuntimeException("No element to write found")
-            }
-            array.arr(0)
-          }
-          case _ => throw new RuntimeException(
-            "Expected array: " + analysisListJson)
-        }
-        val analysisNode = new AnalysisNode(analysisJson)
+        val analysisNode = new AnalysisNode(analysisJson(json))
         val (result, message) = analysisNode.write
         if (result != Success.id) {
           throw new RuntimeException("Writing failed: " + message)
+        }
+        json
+      }
+      case "update" => {
+        val analysisId = (json \ "contents" \ "keys")(0)
+        val analysisNode = new AnalysisNode(analysisJson(json))
+        val (result, message) = analysisNode.update(
+          Map("analysisId" -> analysisId))
+        if (result != Success.id) {
+          throw new RuntimeException("Updating failed: " + message)
         }
         json
       }
@@ -67,11 +63,37 @@ class ANA extends BaseServiceProvider {
           case _ => throw new RuntimeException("no match")
         }
       }
+      case "delete" => {
+        val analysisId = (json \ "contents" \ "keys")(0)
+        val analysisNode = new AnalysisNode
+        val result = analysisNode.delete(Map("analysisId" -> analysisId))
+        if (result == Map.empty) {
+          throw new RuntimeException("Deleting failed")
+        }
+        json
+      }
       case _ => {
         throw new RuntimeException("Unknown action: " + action)
       }
     }
     val playJson = Json.parse(compact(render(response)))
     play.mvc.Results.ok(playJson)
+  }
+
+  def analysisJson(json: JValue) = {
+    val analysisListJson = json \ "contents" \ "analysis"
+    analysisListJson match {
+      case array: JArray => {
+        if (array.arr.length > 1) {
+          throw new RuntimeException("Only one element supported")
+        }
+        if (array.arr.length == 0) {
+          throw new RuntimeException("No element to write found")
+        }
+        array.arr(0)
+      }
+      case _ => throw new RuntimeException(
+        "Expected array: " + analysisListJson)
+    }
   }
 }
--- saw-transport-service/test/analysis/AnalysisTest.scala
+++ saw-transport-service/test/analysis/AnalysisTest.scala
@@ -8,7 +8,89 @@ import org.json4s.JsonDSL._
 import org.json4s.native.JsonMethods._
 import org.scalatest._
 
+/* Test analysis service operations */
 class AnalysisTest extends PlaySpec with OneAppPerSuite {
+  /* Reference companion object to trigger static initialization */
+  AnalysisTest
+
+  "Analysis service" should {
+    requireMapr
+    val id = System.currentTimeMillis - 1490100000000L
+    "create analysis" in {
+      /* Write analysis */
+      val body = actionAnalysisMessage("create", analysisJson(id))
+      val response = sendRequest(body)
+      val analysis = (response \ "contents" \ "analysis")(0)
+      val JInt(analysisId) = analysis \ "analysisId"
+      analysisId must be (id)
+    }
+
+    "update analysis" in {
+      /* Update previously created analysis */
+      val body = actionKeyAnalysisMessage("update", id,
+        analysisJson(id, "customer-2"))
+      val response = sendRequest(body)
+      val analysis = (response \ "contents" \ "analysis")(0)
+      val JInt(analysisId) = analysis \ "analysisId"
+      analysisId must be (id)
+    }
+
+    "read analysis" in {
+      /* Read back previously created analysis */
+      val body = actionKeyMessage("read", id)
+      val response = sendRequest(body)
+      val analysis = (response \ "contents" \ "analysis")(0)
+      val JString(name) = analysis \ "name"
+      name must be (s"test-$id")
+      val JString(customerCode) = analysis \ "customer_code"
+      customerCode must be ("customer-2")
+    }
+
+    "delete analysis" in {
+      /* Delete previously created analysis */
+      val body = actionKeyMessage("delete", id)
+      val response = sendRequest(body)
+      val JString(action) = response \ "contents" \ "action"
+      action must be ("delete")
+    }
+}
+
+  def actionAnalysisMessage(action: String, analysis: JValue) = {
+    message(("action" -> action) ~ ("analysis" -> List(analysis)))
+  }
+
+  def actionKeyAnalysisMessage(
+    action: String, key: Long, analysis: JValue) = {
+    message(("action" -> action) ~ ("keys" -> List(key)) ~
+      ("analysis" -> List(analysis)))
+  }
+  def analysisJson(id: Long, customerCode: String = "customer-1") = {
+    ("analysisId" -> id) ~
+    ("module" -> "analyze") ~
+    ("customer_code" -> customerCode) ~
+    ("name" -> s"test-$id")
+  }
+
+  def actionKeyMessage(action: String, id: Long) = {
+    message(("action" -> action) ~ ("keys" -> JArray(List(id))))
+  }
+
+  def message(contents: JValue) = {
+    ("ticket" -> JObject()) ~
+    ("_links" -> JObject()) ~
+    ("contents" -> contents)
+  }
+
+  def sendRequest(body: JValue) = {
+    val headers = FakeHeaders(Seq("Content-type" -> "application/json"))
+    val Some(response) = route(
+      FakeApplication(), FakeRequest(
+        POST, "/analysis", headers, pretty(render(body))))
+    status(response) mustBe OK
+    contentType(response) mustBe Some("application/json")
+    parse(contentAsString(response))
+  }
+
   def requireMapr {
     val hostname = InetAddress.getLocalHost.toString()
     /* Skip these tests that require a MapR-DB connection if not running
@@ -17,51 +99,9 @@ class AnalysisTest extends PlaySpec with OneAppPerSuite {
       pending
     }
   }
+}
 
+object AnalysisTest {
+  /* Add MapR classpath to test runner */
   MaprHelper.addClasspath()
-  "Analysis service" should {
-    requireMapr
-    val id = System.currentTimeMillis - 1490100000000L
-    "create analysis" in {
-      /* Write analysis */
-      val body =
-        ("ticket" -> JObject()) ~
-          ("_links" -> JObject()) ~
-          ("contents" ->
-            ("action" -> "create") ~
-            ("analysis" ->
-              List(
-                ("analysisId" -> id) ~
-                  ("module" -> "analyze") ~
-                  ("customer_code" -> "customer-1") ~
-                  ("name" -> s"test-$id"))))
-      val headers = FakeHeaders(Seq("Content-type" -> "application/json"))
-      val Some(create) = route(
-        FakeRequest(POST, "/analysis", headers, pretty(render(body))))
-      status(create) mustBe OK
-      contentType(create) mustBe Some("application/json")
-      val responseNode = parse(contentAsString(create))
-      val analysis = responseNode \ "contents" \ "analysis"
-      val JInt(v) = (analysis(0) \ "analysisId")
-      v must be (id)
-    }
-
-    "read analysis" in {
-      /* Read back same analysis */
-      val body =
-        ("ticket" -> JObject()) ~
-          ("_links" -> JObject()) ~
-          ("contents" ->
-            ("action" -> "read") ~
-            ("keys" -> List(id)))
-      val headers = FakeHeaders(Seq("Content-type" -> "application/json"))
-      val Some(read) = route(
-        FakeRequest(POST, "/analysis", headers, pretty(render(body))))
-      status(read) mustBe OK
-      contentType(read) mustBe Some("application/json")
-      val responseJson = parse(contentAsString(read))
-      val JString(v) = (responseJson \ "contents" \ "analysis")(0) \ "name"
-      v must be (s"test-$id")
-    }
-  }
 }
