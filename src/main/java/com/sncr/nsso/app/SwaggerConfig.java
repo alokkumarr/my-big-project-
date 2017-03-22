049a7f02b7cee5f2e4d266d9a63d07c4e8736a21
Saurav Paul <saurav.paul@razorsight.com>
2017-03-22 14:01:51 -0400

    SAW-508 : Swagger documentation issue & build issue resolved

--- src/main/java/com/sncr/nsso/app/SwaggerConfig.java
+++ src/main/java/com/sncr/nsso/app/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
     public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)  
           .select()                                  
-          .apis(RequestHandlerSelectors.basePackage("com.synchronoss.saw.composite.controller"))              
+          .apis(RequestHandlerSelectors.basePackage("com.sncr.nsso.app.controller"))              
           .paths(PathSelectors.any())                          
           .build();         
     }
