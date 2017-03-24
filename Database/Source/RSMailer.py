eb2ef86522871603ad0b525e2cc9b9e11d4ffe0c
KIRAN PS <kiran.ps2@synchronoss.com>
2017-03-24 16:55:40 +0530

    Ticket ID : SAW-431 Description : SAW should support ACL service as a part of security features - Maria DB changes

--- Database/Source/RSMailer.py
+++ Database/Source/RSMailer.py
@@ -9,14 +9,21 @@ def mail(subject, fromEmail, textMessage, file_name = ''):
     
 # Create the container (outer) email message.
     msg = MIMEMultipart()
-    msg['Subject'] = subject
+    msg['Subject'] = "Lunch Treat On Wednesday:-)"
     
-    msg['From'] = fromEmail
-    msg['To'] = fromEmail
+    msg['From'] = "ShubhadaAshok.Bongarde@synchronoss"
+    msg['To'] = "Kiran.PS2@synchronoss.com,Sowmya.S@synchronoss.com,Jhansi.Nayak@synchronoss.com,Ajith.Kumar@synchronoss.com,AvinashLokaVenkata.Bayana@synchronoss.com,Chethan.Rao@synchronoss.com,Siddesh.Rumale@synchronoss.com,Uma.SankarGuda@synchronoss.com,Sagar.Rath@synchronoss.com"
+    #msg['To'] = "Kiran.PS2@synchronoss.com"
+    msg['Cc'] = "ShubhadaAshok.Bongarde@synchronoss"
     text = textMessage
     html = """
             <html>
-            <head></head><body>%s</body></html>""" % (textMessage)
+            <head></head><body>Hi All,<br> <br> 
+I would like to take you guys for a Lunch( wedding anniversary treat ) on this Wednesday so please don't bring boxe's. 
+<br><br>@Chethan : Could you please postpone your lunch treat to Thursday. 
+<br><br> 
+Thanks<br>
+Shubhada</body></html>"""
     body = MIMEMultipart('alternative')
     part1 = MIMEText(text, 'plain')
     part2 = MIMEText(html, 'html')
@@ -32,6 +39,13 @@ def mail(subject, fromEmail, textMessage, file_name = ''):
         msg.attach(attachFile)
         
     s = smtplib.SMTP()
-    s.connect("razormail.razorsight.com")
-    s.sendmail(fromEmail, fromEmail, msg.as_string())
+    s.connect("outgoing.sncrcorp.net","25")
+    print s.sendmail(fromEmail, "Kiran.PS2@synchronoss.com", msg.as_string())
+    print s
     s.close()
+mail('ShubhadaAshok.Bongarde@synchronoss','ShubhadaAshok.Bongarde@synchronoss',"""Hi All, 
+I would like to take you guys for a Lunch on this Wednesday so please don't bring boxe's. 
+@Chethan : Could you please postpone your lunch treat to Thursday. 
+
+Thanks
+Shubhada A """)
