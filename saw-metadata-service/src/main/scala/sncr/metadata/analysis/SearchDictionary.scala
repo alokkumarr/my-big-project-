1786338df3e1c12904cf2b31b044361f64f527e1
Markus Holmberg <markus.holmberg@synchronoss.com>
2017-03-27 16:55:55 -0400

    SAW-519: Update analysis search fields according to specification
    Use list of searchable fields from specification in Confluence.

--- saw-metadata-service/src/main/scala/sncr/metadata/analysis/SearchDictionary.scala
+++ saw-metadata-service/src/main/scala/sncr/metadata/analysis/SearchDictionary.scala
@@ -7,12 +7,21 @@ object SearchDictionary {
 
   val searchFields =
     Map(
-    "analysisId" -> "String",
-    "module" -> "String",
-    "customer_code" -> "String",
-    "name" -> "String",
-    "NodeId" -> "String"
+      "NodeId" -> "String",
+      "analysisId" -> "String",
+      "module" -> "String",
+      "userName" -> "String",
+      "dataSecurityKey" -> "String",
+      "type" -> "String",
+      "metric_name" -> "String",
+      "report_id" -> "String",
+      "customer_prod_module_feature_sys_id" -> "String",
+      "number_of_records" -> "Int",
+      "from_record" -> "Int",
+      "customer_code" -> "String",
+      "role_type" -> "String",
+      "sub_category_id" -> "String",
+      "category_id" -> "String",
+      "metric_name" -> "String"
     )
-
-
 }
