f08f79b981e1eeddc14fbfd389675a57da0eee29
Shwetha Somayaji <Shwetha.Somayaji@synchronoss.com>
2017-03-31 18:24:30 +0530

    SAW-397 SAW should support ACL service as a part of security features

--- src/main/javascript/app/layout/components/header/header.component.js
+++ src/main/javascript/app/layout/components/header/header.component.js
@@ -5,12 +5,29 @@ export const LayoutHeaderComponent = {
   template,
   styles: [style],
   controller: class HeaderController {
-    constructor($window, $transitions, $state, UserService) {
+    constructor($window, $transitions, $state, UserService, JwtService) {
       'ngInject';
       this._$window = $window;
       this._$transitions = $transitions;
       this._$state = $state;
       this._UserService = UserService;
+      this._JwtService = JwtService;
+
+      this.hideObserve = true;
+      this.hideAnalyze = true;
+
+      const token = this._JwtService.get();
+      const base64Url = token.split('.')[1];
+      const base64 = base64Url.replace('-', '+').replace('_', '/');
+      const resp = angular.fromJson(this._$window.atob(base64));
+
+      for (let i = 0; i < resp.ticket.productModules.length; i++) {
+        if (resp.ticket.productModules[i].productModCode === 'OBSRV00001') {
+          this.hideObserve = false;
+        } else if (resp.ticket.productModules[i].productModCode === 'ANLYS00001') {
+          this.hideAnalyze = false;
+        }
+      }
     }
 
     isState(stateName) {
