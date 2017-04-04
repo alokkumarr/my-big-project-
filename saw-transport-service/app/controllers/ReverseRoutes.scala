44a06ed061706095aae4ad3543d61a1a37c9cd61
Sergey Ryabov <sergey.ryabov@synchronoss.com>
2017-04-04 12:28:11 -0400

    SAW-436: Reverted unintended changes in transport service

--- saw-transport-service/app/controllers/ReverseRoutes.scala
+++ saw-transport-service/app/controllers/ReverseRoutes.scala
@@ -1,10 +1,14 @@
 
 // @GENERATOR:play-routes-compiler
 // @SOURCE:/Users/markus/saw-services/saw-transport-service/conf/routes
-// @DATE:Thu Mar 23 01:49:42 EDT 2017
+// @DATE:Fri Mar 24 15:52:24 EDT 2017
 
-import play.api.mvc.{Call, QueryStringBindable}
-import play.core.routing.queryString
+import play.api.mvc.{ QueryStringBindable, PathBindable, Call, JavascriptLiteral }
+import play.core.routing.{ HandlerDef, ReverseRouteContext, queryString, dynamicString }
+
+
+import _root_.controllers.Assets.Asset
+import _root_.play.libs.F
 
 // @LINE:4
 package controllers {
@@ -18,7 +22,23 @@ package controllers {
   
     // @LINE:8
     def handleRequest(): Call = {
+      import ReverseRouteContext.empty
       Call("POST", _prefix + { _defaultPrefix } + "analysis")
+    }
+  
+  }
+
+  // @LINE:9
+  class ReverseSemantic(_prefix: => String) {
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:9
+    def handleRequest(): Call = {
+      import ReverseRouteContext.empty
+      Call("POST", _prefix + { _defaultPrefix } + "semantic")
     }
   
   }
@@ -32,25 +52,28 @@ package controllers {
   
     // @LINE:7
     def handleRequest(): Call = {
+      import ReverseRouteContext.empty
       Call("POST", _prefix + { _defaultPrefix } + "md")
     }
   
   }
 
-  // @LINE:10
+  // @LINE:11
   class ReverseMCT(_prefix: => String) {
     def _defaultPrefix: String = {
       if (_prefix.endsWith("/")) "" else "/"
     }
 
   
-    // @LINE:11
+    // @LINE:12
     def extendedTagRequest(LCID:String, indexname:Option[String], objecttype:Option[String]): Call = {
+      import ReverseRouteContext.empty
       Call("POST", _prefix + { _defaultPrefix } + "ObjectSearch" + queryString(List(Some(implicitly[QueryStringBindable[String]].unbind("LCID", LCID)), Some(implicitly[QueryStringBindable[Option[String]]].unbind("indexname", indexname)), Some(implicitly[QueryStringBindable[Option[String]]].unbind("objecttype", objecttype)))))
     }
   
-    // @LINE:10
+    // @LINE:11
     def handleTagRequest(LCID:String, query:Option[String]): Call = {
+      import ReverseRouteContext.empty
       Call("GET", _prefix + { _defaultPrefix } + "ObjectSearch" + queryString(List(Some(implicitly[QueryStringBindable[String]].unbind("LCID", LCID)), Some(implicitly[QueryStringBindable[Option[String]]].unbind("query", query)))))
     }
   
@@ -63,23 +86,27 @@ package controllers {
     }
 
   
-    // @LINE:18
+    // @LINE:19
     def sr(parameters:Option[String]): Call = {
+      import ReverseRouteContext.empty
       Call("GET", _prefix + { _defaultPrefix } + "sr" + queryString(List(Some(implicitly[QueryStringBindable[Option[String]]].unbind("parameters", parameters)))))
     }
   
-    // @LINE:15
+    // @LINE:16
     def executeCmd(CMD:String, PARAMETERS:Option[String]): Call = {
+      import ReverseRouteContext.empty
       Call("GET", _prefix + { _defaultPrefix } + "control" + queryString(List(Some(implicitly[QueryStringBindable[String]].unbind("CMD", CMD)), Some(implicitly[QueryStringBindable[Option[String]]].unbind("PARAMETERS", PARAMETERS)))))
     }
   
-    // @LINE:16
+    // @LINE:17
     def executeExtendedCmd(CMD:String): Call = {
+      import ReverseRouteContext.empty
       Call("POST", _prefix + { _defaultPrefix } + "control" + queryString(List(Some(implicitly[QueryStringBindable[String]].unbind("CMD", CMD)))))
     }
   
     // @LINE:4
     def index(): Call = {
+      import ReverseRouteContext.empty
       Call("GET", _prefix)
     }
   
@@ -94,6 +121,7 @@ package controllers {
   
     // @LINE:6
     def handleRequest(): Call = {
+      import ReverseRouteContext.empty
       Call("POST", _prefix)
     }
   
