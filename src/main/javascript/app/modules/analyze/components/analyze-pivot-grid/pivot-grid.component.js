fb9fcade8749ae2916b975f9c877b7844ccea26a
Barna Mumtyan <Barna.Mumtya@synchronoss.com>
2017-04-05 11:22:31 +0300

    Refactored pivot code to include the label changes requested by Suren in the evextreme pivotGrid.

--- src/main/javascript/app/modules/analyze/components/analyze-pivot-grid/pivot-grid.component.js
+++ src/main/javascript/app/modules/analyze/components/analyze-pivot-grid/pivot-grid.component.js
@@ -1,30 +1,142 @@
 import forEach from 'lodash/forEach';
+import assign from 'lodash/assign';
+import values from 'lodash/values';
+import mapValues from 'lodash/mapValues';
+import keys from 'lodash/keys';
+import compact from 'lodash/compact';
 
 import template from './pivot-grid.component.html';
 
 export const PivotGridComponent = {
   template,
   bindings: {
-    options: '<'
+    updater: '<'
   },
   controller: class PivotGridController {
-    constructor($timeout, $element) {
+    constructor($timeout, $translate, FilterService) {
       'ngInject';
-      this._$element = $element;
+      this._$translate = $translate;
       this._$timeout = $timeout;
+      this._FilterService = FilterService;
       this.warnings = {
         'Drop Data Fields Here': 'SELECT_DATA_FIELDS',
         'Drop Column Fields Here': 'SELECT_COLUMN_FIELDS',
         'Drop Row Fields Here': 'SELECT_ROW_FIELDS'
       };
+
+      $translate(values(this.warnings)).then(translations => {
+        this.warnings = mapValues(this.warnings, warning => translations[warning]);
+      });
     }
 
-    refresh() {
-      /* eslint-disable angular/document-service */
-      const headers = document.querySelectorAll('.dx-empty-area-text');
-      forEach(headers, header => {
-        header.innerHTML = this.warnings[header.innerHTML];
+    $onInit() {
+
+      this.pivotGridOptions = assign({
+        onInitialized: e => {
+          this._gridInstance = e.component;
+        }
+      }, this.getDefaultOptions());
+
+      this._$timeout(() => {
+        // have to repaint the grid because of the animation of the modal
+        // if it's not repainted it appears smaller
+        this._gridInstance.repaint();
+        this.updater.subscribe(updates => this.update(updates));
+        this.replaceWarningLables();
+      }, 500);
+    }
+
+    replaceWarningLables() {
+      this._$timeout(() => {
+        /* eslint-disable angular/document-service */
+        const headers = document.querySelectorAll('.dx-empty-area-text');
+        forEach(headers, header => {
+          if (this.warnings[header.innerHTML]) {
+            header.innerHTML = this.warnings[header.innerHTML];
+          }
+        });
+        /* eslint-enable angular/document-service */
       });
+    }
+
+    update(updates) {
+      /* eslint-disable no-unused-expressions */
+      updates.dataSource && this.updateDataSource(updates.dataSource);
+      updates.field && this.updateField(updates.field);
+      updates.filters && this.updateFilters(updates.filters);
+      updates.sorts && this.updateSorts(updates.sorts);
+      /* eslint-disable no-unused-expressions */
+
+      this.replaceWarningLables();
+    }
+
+    updateDataSource(dataSource) {
+      this._gridInstance.option('dataSource', dataSource);
+    }
+
+    updateField(field) {
+      const pivotGridDataSource = this._gridInstance.getDataSource();
+
+      pivotGridDataSource.field(field.dataField, field.modifierObj);
+      pivotGridDataSource.load();
+    }
+
+    updateFilters(filters) {
+      const pivotGridDataSource = this._gridInstance.getDataSource();
+
+      forEach(filters, filter => {
+        if (this._FilterService.isFilterModelNonEmpty(filter.model)) {
+          const filterValues = compact(keys(filter.model));
+          pivotGridDataSource.field(filter.name, {
+            filterType: 'include',
+            filterValues
+          });
+        } else {
+          pivotGridDataSource.field(filter.name, {
+            filterType: null,
+            filterValues: null
+          });
+        }
+      });
+      pivotGridDataSource.load();
+    }
+
+    updateSorts(sorts) {
+      const pivotGridDataSource = this._gridInstance.getDataSource();
+      forEach(sorts, sort => {
+        pivotGridDataSource.field(sort.field.dataField, {
+          sortOrder: sort.order
+        });
+      });
+      pivotGridDataSource.load();
+    }
+
+    getDefaultOptions() {
+      return {
+        allowSortingBySummary: false,
+        allowSorting: false,
+        allowFiltering: false,
+        allowExpandAll: true,
+        fieldChooser: {
+          enabled: false
+        },
+        fieldPanel: {
+          visible: true,
+          showColumnFields: true, // hides the column field area
+          showRowFields: true, // hides the row field area
+          showDataFields: true, // hides the data field area
+          showFilterFields: false, // hides the filter field area
+          allowFieldDragging: false
+        },
+        export: {
+          enabled: false,
+          fileName: 'Sales'
+        },
+        dataSource: {
+          store: [],
+          fields: []
+        }
+      };
     }
   }
 };
