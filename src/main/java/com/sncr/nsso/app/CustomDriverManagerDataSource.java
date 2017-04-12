88660ce4bd6a38c1b60bff425b7bd1b65f113c5a
Shwetha Somayaji <Shwetha.Somayaji@synchronoss.com>
2017-04-12 17:16:39 +0530

    SAW-528 Automating the password encryption process

--- src/main/java/com/sncr/nsso/app/CustomDriverManagerDataSource.java
+++ src/main/java/com/sncr/nsso/app/CustomDriverManagerDataSource.java
@@ -4,6 +4,10 @@ import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
 
 
 @Configuration
@@ -36,23 +40,27 @@ public class CustomDriverManagerDataSource extends DriverManagerDataSource {
         return base64Decode(password);
     }
 	
-	/**
-	 * @param token
-	 * @return encoded
-	 */
-	public static String base64Encode(String token) {
-		
-		byte[] encodedBytes = Base64.encodeBase64(token.getBytes());
-		return new String(encodedBytes);
-	}
- 
+	
 	/**
 	 * @param token
 	 * @return
 	 */
 	public static String base64Decode(String token) {
-		byte[] decodedBytes = Base64.decodeBase64(token);
-	    return new String(decodedBytes);
+		String key = "Saw12345Saw12345"; // 128 bit key
+		String initVector = "RandomInitVector"; // 16 bytes IV
+		byte[] original = null;
+		try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+
+            original = cipher.doFinal(Base64.decodeBase64(token));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }		
+	    return new String(original);
 	}
 	
 }
---
+++ src/main/java/com/sncr/nsso/common/util/EncryptPassword.java
@@ -0,0 +1,38 @@
+package com.sncr.nsso.common.util;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.tomcat.util.codec.binary.Base64;
+
+public class EncryptPassword {
+
+	/**
+	 * AES Encryption Algorithm with base64
+	 * @param pwd
+	 */
+
+	public static void main(String[] pwd) {
+		if (pwd.length == 1) {
+			String password = pwd[0];
+
+			String key = "Saw12345Saw12345"; // 128 bit key
+			String initVector = "RandomInitVector"; // 16 bytes IV
+			try {
+				IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+				SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+				Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+				cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+
+				byte[] encrypted = cipher.doFinal(password.getBytes());
+				System.out.println(Base64.encodeBase64String(encrypted));
+
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+
+		} 
+	}
+}
