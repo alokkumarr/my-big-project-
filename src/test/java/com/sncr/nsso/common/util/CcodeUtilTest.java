4f17b58fd1fa7e082fa15b54daedf12d6f6db3f0
Shwetha Somayaji <Shwetha.Somayaji@synchronoss.com>
2017-04-14 18:00:35 +0530

    SAW-528

--- src/test/java/com/sncr/nsso/common/util/CcodeUtilTest.java
+++ src/test/java/com/sncr/nsso/common/util/CcodeUtilTest.java
@@ -13,17 +13,14 @@ public class CcodeUtilTest {
 	@Test
 	public void test() {
 		
-		String password = "devpass";
-		String encryptedPassword = "1m0PBBfvPVeSR2IlSGwLWg==";
+		String password = "devpass";			
 		
-		
-		String encryptedPassword_Actual = CcodeUtil.cencode(password);
+		String encryptedPassword = CcodeUtil.cencode(password);
 		String password_Actual = CcodeUtil.cdecode(encryptedPassword);
+		String encrypted_Actual = CcodeUtil.cdecode(password_Actual);
 		
-		
-		assertEquals(encryptedPassword, encryptedPassword_Actual);
-		assertEquals(password, password_Actual);
-		
+		assertEquals(password, password_Actual);		
+		assertEquals(encryptedPassword, encrypted_Actual);
 	}
 
 }
