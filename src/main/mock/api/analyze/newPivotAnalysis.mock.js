74544d6f95361c8d2348ae0f76554c59e7c85dac
Akash Agrawal <akash.agarwal@synchronoss.com>
2017-04-17 21:18:13 +0530

    Fix filename reference

--- src/main/mock/api/analyze/newPivotAnalysis.mock.js
+++ src/main/mock/api/analyze/newPivotAnalysis.mock.js
@@ -2,6 +2,6 @@ export const RequestMock = {
   method: 'GET',
   url: '/api/analyze/newPivotAnalysis',
   response: () => {
-    return [200, require('./newPivotANalysis.json')];
+    return [200, require('./newPivotAnalysis.json')];
   }
 };
