31291a4fef385c1e9e9b7da6922c131fdab04444
nmbg0001 <NithinMalathesh.BG@synchronoss.com>
2017-06-02 18:20:54 +0530

    SAW-686 Resolving Conflict

--- src/main/javascript/app/layout/components/header/header.component.js
+++ src/main/javascript/app/layout/components/header/header.component.js
@@ -1,3 +1,4 @@
+import get from 'lodash/get';
 import template from './header.component.html';
 import style from './header.component.scss';
 
@@ -5,28 +6,26 @@ export const LayoutHeaderComponent = {
   template,
   styles: [style],
   controller: class HeaderController {
-    constructor($window, $transitions, $state, UserService, JwtService) {
+    constructor($window, $transitions, $state, UserService, JwtService, $rootScope) {
       'ngInject';
       this._$window = $window;
       this._$transitions = $transitions;
       this._$state = $state;
       this._UserService = UserService;
       this._JwtService = JwtService;
+      this._$rootScope = $rootScope;
 
-      this.hideObserve = true;
-      this.hideAnalyze = true;
-
-      const token = this._JwtService.get();
-      if (!token) {
-        $window.location.assign('/login.html');
-        return;
-      }
-      const base64Url = token.split('.')[1];
-      const base64 = base64Url.replace('-', '+').replace('_', '/');
-      const resp = angular.fromJson(this._$window.atob(base64));
-      if (resp.ticket.roleType === 'ADMIN') {
+      const token = this._JwtService.getTokenObj();
+      const product = get(token, 'ticket.products.[0]');
+      this.modules = product.productModules;
+      const roleType = get(token, 'ticket.roleType');
+      if (roleType === 'ADMIN') {
         this.showAdmin = true;
       }
+    }
+
+    get showProgress() {
+      return this._$rootScope.showProgress;
     }
 
     isState(stateName) {
@@ -38,9 +37,11 @@ export const LayoutHeaderComponent = {
         this._$window.location.assign('./login.html');
       });
     }
+
     changePwd() {
       this._$window.location.assign('./login.html#!/changePwd');
     }
+
     admin() {
       this._$state.go('admin');
     }
