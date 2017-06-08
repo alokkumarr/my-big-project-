26e1e03ede080faf39836b1218ecce49bbe12494
Saurav Paul <saurav.paul@synchronoss.com>
2017-06-08 17:29:18 -0400

    SAW-814 : JSON Schema & Validation Classes.

---
+++ saw-es-querybuilder/src/main/java/com/synchronoss/querybuilder/model/chart/Filter.java
@@ -0,0 +1,227 @@
+
+package com.synchronoss.querybuilder.model.chart;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "booleanCriteria",
+    "columnName",
+    "isRuntimeFilter",
+    "model",
+    "tableName",
+    "type"
+})
+public class Filter {
+
+    @JsonProperty("booleanCriteria")
+    private Filter.BooleanCriteria booleanCriteria;
+    @JsonProperty("columnName")
+    private String columnName;
+    @JsonProperty("isRuntimeFilter")
+    private Filter.IsRuntimeFilter isRuntimeFilter;
+    @JsonProperty("model")
+    private Model model;
+    @JsonProperty("tableName")
+    private String tableName;
+    @JsonProperty("type")
+    private Filter.Type type;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("booleanCriteria")
+    public Filter.BooleanCriteria getBooleanCriteria() {
+        return booleanCriteria;
+    }
+
+    @JsonProperty("booleanCriteria")
+    public void setBooleanCriteria(Filter.BooleanCriteria booleanCriteria) {
+        this.booleanCriteria = booleanCriteria;
+    }
+
+    @JsonProperty("columnName")
+    public String getColumnName() {
+        return columnName;
+    }
+
+    @JsonProperty("columnName")
+    public void setColumnName(String columnName) {
+        this.columnName = columnName;
+    }
+
+    @JsonProperty("isRuntimeFilter")
+    public Filter.IsRuntimeFilter getIsRuntimeFilter() {
+        return isRuntimeFilter;
+    }
+
+    @JsonProperty("isRuntimeFilter")
+    public void setIsRuntimeFilter(Filter.IsRuntimeFilter isRuntimeFilter) {
+        this.isRuntimeFilter = isRuntimeFilter;
+    }
+
+    @JsonProperty("model")
+    public Model getModel() {
+        return model;
+    }
+
+    @JsonProperty("model")
+    public void setModel(Model model) {
+        this.model = model;
+    }
+
+    @JsonProperty("tableName")
+    public String getTableName() {
+        return tableName;
+    }
+
+    @JsonProperty("tableName")
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+    @JsonProperty("type")
+    public Filter.Type getType() {
+        return type;
+    }
+
+    @JsonProperty("type")
+    public void setType(Filter.Type type) {
+        this.type = type;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    public enum BooleanCriteria {
+
+        OR("OR"),
+        AND("AND");
+        private final String value;
+        private final static Map<String, Filter.BooleanCriteria> CONSTANTS = new HashMap<String, Filter.BooleanCriteria>();
+
+        static {
+            for (Filter.BooleanCriteria c: values()) {
+                CONSTANTS.put(c.value, c);
+            }
+        }
+
+        private BooleanCriteria(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return this.value;
+        }
+
+        @JsonValue
+        public String value() {
+            return this.value;
+        }
+
+        @JsonCreator
+        public static Filter.BooleanCriteria fromValue(String value) {
+            Filter.BooleanCriteria constant = CONSTANTS.get(value);
+            if (constant == null) {
+                throw new IllegalArgumentException(value);
+            } else {
+                return constant;
+            }
+        }
+
+    }
+
+    public enum IsRuntimeFilter {
+
+        FALSE(false),
+        TRUE(true);
+        private final Boolean value;
+        private final static Map<Boolean, Filter.IsRuntimeFilter> CONSTANTS = new HashMap<Boolean, Filter.IsRuntimeFilter>();
+
+        static {
+            for (Filter.IsRuntimeFilter c: values()) {
+                CONSTANTS.put(c.value, c);
+            }
+        }
+
+        private IsRuntimeFilter(Boolean value) {
+            this.value = value;
+        }
+
+        @JsonValue
+        public Boolean value() {
+            return this.value;
+        }
+
+        @JsonCreator
+        public static Filter.IsRuntimeFilter fromValue(Boolean value) {
+            Filter.IsRuntimeFilter constant = CONSTANTS.get(value);
+            if (constant == null) {
+                throw new IllegalArgumentException((value +""));
+            } else {
+                return constant;
+            }
+        }
+
+    }
+
+    public enum Type {
+
+        LONG("long"),
+        STRING("string"),
+        INT("int"),
+        DOUBLE("double"),
+        DATE("date"),
+        TIMESTAMP("timestamp");
+        private final String value;
+        private final static Map<String, Filter.Type> CONSTANTS = new HashMap<String, Filter.Type>();
+
+        static {
+            for (Filter.Type c: values()) {
+                CONSTANTS.put(c.value, c);
+            }
+        }
+
+        private Type(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return this.value;
+        }
+
+        @JsonValue
+        public String value() {
+            return this.value;
+        }
+
+        @JsonCreator
+        public static Filter.Type fromValue(String value) {
+            Filter.Type constant = CONSTANTS.get(value);
+            if (constant == null) {
+                throw new IllegalArgumentException(value);
+            } else {
+                return constant;
+            }
+        }
+
+    }
+
+}
