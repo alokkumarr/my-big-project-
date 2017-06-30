8d21afce4d923f1ba4ab95bc1620f1aa1613a7e5
Shwetha Somayaji <Shwetha.Somayaji@synchronoss.com>
2017-06-30 21:13:12 +0530

    Fixing the DB sql scripts issues

--- src/main/java/com/sncr/saw/security/app/repository/impl/UserRepositoryImpl.java
+++ src/main/java/com/sncr/saw/security/app/repository/impl/UserRepositoryImpl.java
@@ -1480,7 +1480,7 @@ public class UserRepositoryImpl implements UserRepository {
 	@Override
 	public List<Role> getRoletypesDropDownList() {
 		ArrayList<Role> rolesList = null;
-		String sql = "SELECT R.ROLES_TYPES_SYS_ID, R.ROLES_TYPE_NAME FROM ROLES_TYPES R WHERE R.ACTIVE_STATUS_IND = 1";
+		String sql = "SELECT R.ROLES_TYPES_SYS_ID, R.ROLES_TYPE_NAME FROM ROLES_TYPE R WHERE R.ACTIVE_STATUS_IND = 1";
 
 		try {
 			rolesList = jdbcTemplate.query(sql, new PreparedStatementSetter() {
