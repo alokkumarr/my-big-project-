a4a9e3bee53e1709d3db39483602e424205731e9
nmbg0001 <NithinMalathesh.BG@synchronoss.com>
2017-08-02 15:28:39 +0530

    setting the url back to velocity

--- appConfig.js
+++ appConfig.js
@@ -1,6 +1,6 @@
 export default {
   login: {
-    url: 'http://10.49.0.171:9000/saw-security',
+    url: 'https://saw.bda.poc.velocity-va.synchronoss.net/saw-security',
     jwtKey: 'jwtToken'
   },
   api: {
