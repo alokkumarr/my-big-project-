52307553cc88c3a0ec5953738fbeed467ffee3aa
Akash Agrawal <aaga0003@synchronoss.com>
2017-08-22 00:03:06 +0530

    Added environment variables to webpack test config

--- conf/webpack.test.js
+++ conf/webpack.test.js
@@ -3,62 +3,78 @@ const DefinePlugin = require('webpack/lib/DefinePlugin');
 
 const MODULE_DIR = 'node_modules';
 
-module.exports = {
-  resolve: {
-    modules: [
-      MODULE_DIR,
-      webpackHelper.root('src/main/javascript')
-    ]
-  },
+module.exports = function (env) {
+  const isDevelopment = env === 'development';
+  const isProduction = env === 'production';
+  const enableMock = true;
 
-  module: {
-    rules: [
-      // preloaders
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader',
-        options: {
-          fix: false,
-          configFile: webpackHelper.root('conf/eslint-dev-rules.js')
+  const conf = {
+    resolve: {
+      modules: [
+        MODULE_DIR,
+        webpackHelper.root('src/main/javascript')
+      ]
+    },
+
+    module: {
+      rules: [
+        // preloaders
+        {
+          enforce: 'pre',
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'eslint-loader',
+          options: {
+            fix: false,
+            configFile: webpackHelper.root('conf/eslint-dev-rules.js')
+          }
+        },
+        {
+          enforce: 'pre',
+          test: /\.html$/,
+          loader: 'htmlhint-loader'
+        },
+        // loaders
+        {
+          test: /\.json$/,
+          loader: 'json-loader'
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'ng-annotate-loader!babel-loader'
+        },
+        {
+          test: /\.html$/,
+          loader: 'html-loader'
+        },
+        {
+          test: /\.(css|scss)$/,
+          loader: 'null-loader'
+        },
+        {
+          test: /\.(css|scss)$/,
+          loader: 'raw-loader'
+        },
+        {
+          test: /\.(eot|woff|woff2|ttf)$/,
+          loader: 'null-loader'
+        },
+        {
+          test: /\.(png|jpg|svg)$/,
+          loader: 'null-loader'
         }
-      },
-      {
-        enforce: 'pre',
-        test: /\.html$/,
-        loader: 'htmlhint-loader'
-      },
-      // loaders
-      {
-        test: /\.json$/,
-        loader: 'json-loader'
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'ng-annotate-loader!babel-loader'
-      },
-      {
-        test: /\.html$/,
-        loader: 'html-loader'
-      },
-      {
-        test: /\.(css|scss)$/,
-        loader: 'null-loader'
-      },
-      {
-        test: /\.(css|scss)$/,
-        loader: 'raw-loader'
-      },
-      {
-        test: /\.(eot|woff|woff2|ttf)$/,
-        loader: 'null-loader'
-      },
-      {
-        test: /\.(png|jpg|svg)$/,
-        loader: 'null-loader'
-      }
+      ]
+    },
+
+    plugins: [
+      new DefinePlugin({
+        '__DEVELOPMENT__': JSON.stringify(isDevelopment),
+        '__PRODUCTION__': JSON.stringify(isProduction),
+        '__MOCK__': JSON.stringify(enableMock)
+      })
     ]
-  }
+  };
+
+  return conf;
 };
