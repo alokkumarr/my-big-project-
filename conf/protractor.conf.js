a211aa203d44617f223f3140d5a901fae2a82332
Alex <alexander.krivorotko@gmail.com>
2017-08-21 11:31:02 +0300

    fixing tests

--- conf/protractor.conf.js
+++ conf/protractor.conf.js
@@ -37,12 +37,12 @@ exports.config = {
 
     analyses: [
       webpackHelper.root('src/test/e2e-tests/priviliges.test.js'),
-      //webpackHelper.root('src/test/javascript/e2e/spec/analyses.test.js'), //obsolete
+      // webpackHelper.root('src/test/javascript/e2e/spec/analyses.test.js'), // obsolete
       webpackHelper.root('src/test/e2e-tests/goToAnalyze.test.js'),
       webpackHelper.root('src/test/e2e-tests/createChart.test.js'),
       webpackHelper.root('src/test/e2e-tests/createPivot.test.js'),
       webpackHelper.root('src/test/e2e-tests/createReport.test.js')
-      //webpackHelper.root('src/test/e2e-tests/debug.test.js') //for testing purposes
+      // webpackHelper.root('src/test/e2e-tests/debug.test.js') // for testing purposes
     ]
   },
 
