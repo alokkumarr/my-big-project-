8ff42b7baa10ca169987a5c79a82cb7a19804adf
Saurav Paul <Saurav.Paul@synchronoss.com>
2017-08-23 17:35:15 -0400

    SAW-1147 : support existing URL in the gateway service

--- saw-gateway-service/src/main/java/com/synchronoss/saw/gateway/SAWGatewayServiceApplication.java
+++ saw-gateway-service/src/main/java/com/synchronoss/saw/gateway/SAWGatewayServiceApplication.java
@@ -5,8 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 
 
@@ -14,13 +12,6 @@ import org.springframework.web.client.RestTemplate;
 public class SAWGatewayServiceApplication {
 
   private static final Logger LOG = LoggerFactory.getLogger(SAWGatewayServiceApplication.class);
-
-  @Bean
-  RestTemplate restTemplate() {
-    return new RestTemplate();
-  }
-
-
   public static void main(String[] args) {
     ConfigurableApplicationContext ctx =
         SpringApplication.run(SAWGatewayServiceApplication.class, args);
