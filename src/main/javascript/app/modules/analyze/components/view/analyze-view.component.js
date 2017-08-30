bf55905b39b4bd1cbb24ebc3caddf523447c1d76
nmbg0001 <NithinMalathesh.BG@synchronoss.com>
2017-08-30 19:08:19 +0530

    SAW-844-Gen-Enh Left side menu Enhancement done

--- src/main/javascript/app/modules/analyze/components/view/analyze-view.component.js
+++ src/main/javascript/app/modules/analyze/components/view/analyze-view.component.js
@@ -42,8 +42,7 @@ export const AnalyzeViewComponent = {
 
       this.states = {
         reportView: [this.LIST_VIEW, this.CARD_VIEW].indexOf(savedView) >= 0 ?
-          savedView :
-          this.LIST_VIEW,
+          savedView : this.LIST_VIEW,
         analysisType: 'all',
         searchTerm: ''
       };
