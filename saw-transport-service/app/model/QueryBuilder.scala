c4c0f5a4128dc7f9e48f063abd8f97fbcc3c6135
Saurav Paul <Saurav.Paul@synchronoss.com>
2017-10-13 15:12:03 -0400

    SAW-1527 : Prompt Runtime filter bugfix

--- saw-transport-service/app/model/QueryBuilder.scala
+++ saw-transport-service/app/model/QueryBuilder.scala
@@ -146,13 +146,13 @@ object QueryBuilder extends {
 
     val filters = ((sqlBuilder \ "filters") match {
       case filters: JArray => filters.arr
-      case JNothing => List()
+      //case JNothing => List()
       case json: JValue => unexpectedElement(json, "array", "filters")
     }).filter((filter: JValue) => {
        query_logger.trace("isRuntimeFilter value on buildWhere : {}", (filter \ "isRuntimeFilter").extract[Boolean]);
-       query_logger.trace("isRuntimeFilter value on buildWhere evaluation : {}", !((filter \ "isRuntimeFilter").extractOrElse[Boolean](false) || runtime));
+       query_logger.trace("isRuntimeFilter value on buildWhere evaluation : {}", !(filter \ "isRuntimeFilter").extract[Boolean] || runtime);
        query_logger.trace("Runtime parameter value : {}", runtime);
-      !((filter \ "isRuntimeFilter").extractOrElse[Boolean](false) || runtime)
+      !(filter \ "isRuntimeFilter").extract[Boolean] || runtime
     }).map(buildWhereFilterElement)
     if (filters.isEmpty) {
       ""
@@ -173,13 +173,13 @@ object QueryBuilder extends {
     var finalFilter : String = null
     val filters = ((sqlBuilder \ "filters") match {
       case filters: JArray => filters.arr
-      case JNothing => List()
+      //case JNothing => List()
       case json: JValue => unexpectedElement(json, "array", "filters")
     }).filter((filter: JValue) => {
       query_logger.trace("isRuntimeFilter value on buildWhere with DSK : {}", (filter \ "isRuntimeFilter").extract[Boolean]);
-      query_logger.trace("isRuntimeFilter value on buildWhere evaluation : {}", !((filter \ "isRuntimeFilter").extractOrElse[Boolean](false) || runtime));
+      query_logger.trace("isRuntimeFilter value on buildWhere evaluation : {}", !(filter \ "isRuntimeFilter").extract[Boolean] || runtime);
       query_logger.trace("Runtime parameter value : {}", runtime);
-      !((filter \ "isRuntimeFilter").extractOrElse[Boolean](false) || runtime)
+      !(filter \ "isRuntimeFilter").extract[Boolean] || runtime
     }).map(buildWhereFilterElement)
     if (filters.isEmpty) {
       if(DSK!=null && DSK.nonEmpty){
