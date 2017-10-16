45c6f1fbfdfc66799482c03d3198114f506672de
Akash Agrawal <aaga0003@synchronoss.com>
2017-10-16 18:14:43 +0530

    SAW-1527 Revert preview execution mode parameter value

--- src/main/javascript/app/modules/analyze/services/analyze.service.js
+++ src/main/javascript/app/modules/analyze/services/analyze.service.js
@@ -12,7 +12,7 @@ import * as flatMap from 'lodash/flatMap';
 import * as cloneDeep from 'lodash/cloneDeep';
 
 const EXECUTION_MODES = {
-  PREVIEW: 'interactive',
+  PREVIEW: 'preview',
   LIVE: 'live'
 };
 
