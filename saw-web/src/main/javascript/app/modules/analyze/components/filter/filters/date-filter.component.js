599b9dfa8acdc1b619120268c36be34b40035142
Barna Mumtyan <barnamumtyan@gmail.com>
2017-10-26 16:44:42 +0300

    Added the presets for the string filter.

--- saw-web/src/main/javascript/app/modules/analyze/components/filter/filters/date-filter.component.js
+++ saw-web/src/main/javascript/app/modules/analyze/components/filter/filters/date-filter.component.js
@@ -1,3 +1,5 @@
+import * as map from 'lodash/map';
+import * as forEach from 'lodash/forEach';
 import * as template from './date-filter.component.html';
 
 export const DateFilterComponent = {
@@ -7,8 +9,44 @@ export const DateFilterComponent = {
     onChange: '&'
   },
   controller: class DateFilterController {
-    constructor($filter) {
+    constructor($filter, $translate) {
       this._$filter = $filter;
+      this.presets = [{
+        value: 'YTD',
+        keyword: 'YEAR_TO_DATE'
+      }, {
+        value: 'MTD',
+        keyword: 'MONTH_TO_DATE'
+      }, {
+        value: 'LTM',
+        keyword: 'LAST_3_MONTHS'
+      }, {
+        value: 'LSM',
+        keyword: 'LAST_6_MONTHS'
+      }, {
+        value: 'LM',
+        keyword: 'LAST_MONTH'
+      }, {
+        value: 'LQ',
+        keyword: 'LAST_QUARTER'
+      }, {
+        value: 'LW',
+        keyword: 'LAST_WEEK'
+      }, {
+        value: 'TW',
+        keyword: 'THIS_WEEK'
+      }, {
+        value: 'LTW',
+        keyword: 'LAST_2_WEEKS'
+      }, {
+        value: 'CUSTOM',
+        keyword: 'CUSTOM'
+      }];
+      $translate(map(this.presets, 'keyword')).then(translations => {
+        forEach(this.presets, preset => {
+          preset.label = translations[preset.keyword];
+        });
+      });
     }
     $onInit() {
       /* this.model has string values. Change them to actual date objects
@@ -16,9 +54,16 @@ export const DateFilterComponent = {
       */
       this.model = this.model || {};
       this.tempModel = {
+        preset: this.model.preset,
         gte: this.model.gte ? new Date(this.model.gte) : '',
         lte: this.model.lte ? new Date(this.model.lte) : ''
       };
+    }
+
+    onPresetSelected() {
+      this.tempModel.gte = null;
+      this.tempModel.lte = null;
+      this.onChange({model: {preset: this.tempModel.preset}});
     }
 
     onModelChange() {
@@ -27,6 +72,7 @@ export const DateFilterComponent = {
         lte: ''
       };
       this.dateFormat = 'yyyy-MM-dd HH:ss:mm';
+      this.updatedDate.preset = this.tempModel.preset;
       this.updatedDate.gte = this._$filter('date')(this.tempModel.gte, this.dateFormat);
       this.updatedDate.lte = this._$filter('date')(this.tempModel.lte, this.dateFormat);
       this.onChange({model: this.updatedDate});
