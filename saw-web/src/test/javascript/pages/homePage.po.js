8f727d89316487e4995d6fbc1b0b8c5fe5697620
Alex <alexander.krivorotko@gmail.com>
2017-10-27 18:31:05 +0700

    adding missing files

---
+++ saw-web/src/test/javascript/pages/homePage.po.js
@@ -0,0 +1,5 @@
+module.exports = {
+  accountSettingsMenuBtn: element(by.css('button[e2e="account-settings-menu-btn"]')),
+  adminMenuOption: element(by.css('a[e2e="account-settings-selector-admin"]')),
+  changePasswordMenuOption: element(by.css('button[e2e="account-settings-selector-change-password"]'))
+};
