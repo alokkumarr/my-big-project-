ae8ec58aaa3d37df394174bab4496a0275d6d133
Akash Agrawal <aaga0003@synchronoss.com>
2017-10-29 03:06:58 +0530

    Calculate chart dimensions from tile height and width

--- saw-web/src/main/javascript/app/modules/observe/components/create-dashboard/create-dashboard.component.ts
+++ saw-web/src/main/javascript/app/modules/observe/components/create-dashboard/create-dashboard.component.ts
@@ -10,6 +10,8 @@ import { AnalysisChoiceComponent } from '../analysis-choice/analysis-choice.comp
 const template = require('./create-dashboard.component.html');
 require('./create-dashboard.component.scss');
 
+const MARGIN_BETWEEN_TILES = 10;
+
 @Component({
   selector: 'create-dashboard',
   template
@@ -33,20 +35,29 @@ export class CreateDashboardComponent {
       if (this.gridster.columns !== this.columns) {
         this.refreshAllTiles();
       } else {
-        item.updater.next([{ path: 'chart.height', data: this.getHeight(item)}]);
+        this.refreshTile(item);
       }
       this.columns = this.gridster.columns;
     }, 500)
   }
 
-  getHeight(item) {
-    return (this.gridster.curRowHeight - 20) * item.rows;
+  getDimensions(item) {
+    return {
+      width: this.gridster.curColWidth * item.cols - MARGIN_BETWEEN_TILES,
+      height: this.gridster.curRowHeight * item.rows - MARGIN_BETWEEN_TILES
+    };
+  }
+
+  refreshTile(item) {
+    const dimensions = this.getDimensions(item);
+    item.updater.next([
+      {path: 'chart.height', data: dimensions.height},
+      {path: 'chart.width', data: dimensions.width}
+    ])
   }
 
   refreshAllTiles() {
-    forEach(this.dashboard, i => i.updater.next([
-      {path: 'chart.height', data: this.getHeight(i)}
-    ]));
+    forEach(this.dashboard, this.refreshTile.bind(this));
   }
 
   ngOnInit() {
@@ -54,6 +65,7 @@ export class CreateDashboardComponent {
       gridType: 'scrollVertical',
       minCols: this.columns,
       maxCols: 100,
+      margin: MARGIN_BETWEEN_TILES,
       minRows: 4,
       maxRows: 100,
       itemChangeCallback: this.itemChange.bind(this),
