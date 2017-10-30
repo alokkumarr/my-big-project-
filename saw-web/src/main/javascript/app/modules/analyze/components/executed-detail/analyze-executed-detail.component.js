4b4fdbc9778294eabdee2dc524de5664d2c6d0d2
Barna Mumtyan <barnamumtyan@gmail.com>
2017-10-30 15:00:30 +0200

    Removed a redundant import.

--- saw-web/src/main/javascript/app/modules/analyze/components/executed-detail/analyze-executed-detail.component.js
+++ saw-web/src/main/javascript/app/modules/analyze/components/executed-detail/analyze-executed-detail.component.js
@@ -1,6 +1,5 @@
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 import * as get from 'lodash/get';
-import * as keys from 'lodash/keys';
 import {json2csv} from 'json-2-csv';
 
 import {Events} from '../../consts';
