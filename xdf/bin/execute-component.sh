307a79abbebab62f0049faed01e78526a4023a06
Sergey Volkov <Sergey.Volkov@synchronoss.com>
2017-11-15 22:01:44 -0500

    Brush execute-component.sh

--- xdf/bin/execute-component.sh
+++ xdf/bin/execute-component.sh
@@ -3,7 +3,15 @@
 CMD_DIR=$( cd $(dirname $0); pwd -P )
 
 DRYRUN=${DRYRUN:-}
-XDF_DATA_ROOT=hdfs:///data/bda
+XDF_DATA_ROOT=hdfs:///data/bda #
+
+# Component main class
+declare -A COMP_MC=(
+    [sql]=sncr.xdf.sql.SQLComponent
+    [spark-sql]=sncr.xdf.sql.SQLComponent
+    [zero]=sncr.xdf.component.ZeroComponent
+    [parser]=sncr.xdf.parser
+    )
 
 ## Mandatory
 COMPONENT_NAME= # -m
@@ -30,12 +38,10 @@ usage: $0
     -h - print this help and exit
 
  -a, -c, -b -m - mandatory parameters;
-  Components:
+  Components: ${!COMP_MC[*]}
      spark-sql  - XDF Spark SQL Component
      parser     - XDF Parser
      zero       - XDF Zero component
-
-
 
 Example:
     $0 -m zero -a project1 -b BATCHID0001 -c app_conf.jcfg -r hdfs:///data/bda
@@ -54,36 +60,7 @@ warn(){
     errout WARNIG: "$@"
 }
 
-while getopts a:b:c:j:h:m:r:n opt
-do
-    case "$opt" in
-    h)  usage;;
-    n)  DRYRUN=1;;
-    # mandatory common
-    a)  APPLICATION_ID=$OPTARG;;
-    b)  BATCH_ID=$OPTARG;;
-    c)  CONFIG_FILE=$OPTARG;;
-    m)  COMPONENT_NAME=$OPTARG;;
-    # optional parameters
-    r) XDF_DATA_ROOT=$OPTARG;;
-    j) LOG4J_CONF=$OPTARG
-        [[ $LOG4J_CONF = /* ]] || {
-            error absolute file name required for -j, got "'$LOG4J_CONF'"
-            exit 1
-        }
-        ;;
-
-    \?) error unknown flag: -$opt
-        usage 1;;
-    esac
-done
-# Check common mandatory
-: ${BATCH_ID:?ERROR: Batch ID (-b) is not specified}
-: ${CONFIG_FILE:?ERROR: Config file (-c) name is not specified}
-: ${APPLICATION_ID:?ERROR: Application ID (-a) is not specified}
-: ${COMPONENT_NAME:?ERROR: Component (-m) is not specified}
-#####
-
+## Globals
 ## Calculate XDF_DIR, LIB_DIR, VERSION
 # Check xdf_info executable
 $CMD_DIR/xdf_info || exit
@@ -93,120 +70,139 @@ xdf_info() {
 
 XDF_DIR=$( xdf_info optdir )
 : ${XDF_DIR:?no value}
-LIB_DIR=$XDF_DIR/lib
-( cd $LIB_DIR ) || exit
 
 VERSION=$( xdf_info version )
 : ${VERSION:?no value}
 
+LIB_DIR=$XDF_DIR/lib
+( cd $LIB_DIR ) || exit
+
+export SPARK_HOME=/opt/mapr/spark/spark-current
+( cd $SPARK_HOME ) || exit
+
 APPJAR=$XDF_DIR/lib/xdf-rest-${VERSION}-all.jar
+( <$APPJAR ) || exit
+COMPONENT_JAR=$APPJAR
+
+
+## CLI Args
+while getopts a:b:c:j:m:r:nh opt
+do
+    case "$opt" in
+    # mandatory common
+    a)  APPLICATION_ID=$OPTARG;;
+    b)  BATCH_ID=$OPTARG;;
+    c)  CONFIG_FILE=$OPTARG;;
+    m)  COMPONENT_NAME=$OPTARG;;
+    # optional parameters
+    r)  XDF_DATA_ROOT=$OPTARG;;
+    j)  LOG4J_CONF=$OPTARG
+        [[ $LOG4J_CONF = /* ]] || {
+           error absolute file name required for -j, got "'$LOG4J_CONF'"
+           exit 1
+        }
+        ;;
+    n)  DRYRUN=1;;
+    h)  usage;;
+
+    \?) usage 1;;
+    esac
+done
+#(( OPTIND > 1)) && shift $(( OPTIND - 1 ))
+
+# Check common mandatory
+: ${APPLICATION_ID:?ERROR: Application ID (-a) is not specified}
+: ${BATCH_ID:?ERROR: Batch ID (-b) is not specified}
+: ${CONFIG_FILE:?ERROR: Config file (-c) name is not specified}
+: ${COMPONENT_NAME:?ERROR: Component (-m) is not specified}
+#####
+
+# validate COMPONENT_NAME
+MAIN_CLASS=${COMP_MC[$COMPONENT_NAME]}
+[[ $MAIN_CLASS ]] || {
+    error "Unknown XDF component (-m): $COMPONENT_NAME"
+    usage 1
+}
+
+#SR?? Validate config file
+#?? file://
+#( <${CONFIG_FILE} ) || exit
+
+# validate XDF_DATA_ROOT
+hadoop fs -stat $XDF_DATA_ROOT >/dev/null || exit 1
+
+# validate LOG4J_CONF
+: ${LOG4J_CONF:=$XDF_DIR/conf/log4j.properties}
+( <$LOG4J_CONF ) || exit
+
+# Validate spark
+( <$SPARK_HOME/conf/spark-env.sh ) || exit
+# 'export SPARK_HOME' inside spark-env.sh
+source $SPARK_HOME/conf/spark-env.sh
 
 cat<<EEOOTT
-XDF COMPONENT SHELL SCRIPT EXECUTING AT $(date +"%m-%d-%Y %r")
+XDF COMPONENT SHELL SCRIPT EXECUTING AT $(date --rfc-3339=sec)
 ----------------------------------------------------------------
 Command                 : $0 $@
 -------
 XDF home                : $XDF_DIR
 XDF version             : $VERSION
 -------
-Component               : $COMPONENT_NAME
+Component               : $COMPONENT_NAME ($MAIN_CLASS)
 Batch ID                : $BATCH_ID
 Application             : $APPLICATION_ID
 Configuration file      : $CONFIG_FILE
 -------
+Log4j log file          : ${LOG4J_CONF}
+----------------------------------------------------------------
 EEOOTT
 
 #######################################
 
-COMPONENT_JAR=$APPJAR
+JAVA_PROPS=(
+  -Dlog4j.configuration=file:$LOG4J_CONF #SR?? file://
+  -DXDF_DATA_ROOT=$XDF_DATA_ROOT
+  -Djava.security.auth.login.config=/opt/mapr/conf/mapr.login.conf
+  )
 
-CMD=
-PARAM_LIST=( -b $BATCH_ID -c $CONFIG_FILE -a $APPLICATION_ID )
-case "$COMPONENT_NAME" in
-    sql)
-#        COMPONENT_JAR=${LIB_DIR}/xdf-sql-${VERSION}.jar
-        MAIN_CLASS=sncr.xdf.sql.SQLComponent
-        ;;
- 
-    zero)
-#        COMPONENT_JAR=${LIB_DIR}/xdf-component-${VERSION}.jar
-        MAIN_CLASS="sncr.xdf.component.ZeroComponent"
-        ;;
-
-    parser)
-#        COMPONENT_JAR=${LIB_DIR}/xdf-component-${VERSION}.jar
-        MAIN_CLASS="sncr.xdf.parser"
-        ;;
-
-    *)  echo "ERROR: Unknown XDF component: $COMPONENT_NAME"
-        exit 1
-        ;;
-esac
-
-export SPARK_HOME=/opt/mapr/spark/spark-current
-( cd $SPARK_HOME ) || exit
-
-[[ $CMD ]] || {
-    : ${COMPONENT_JAR:?no value}
-    : ${MAIN_CLASS:?no value}
-    ( <$COMPONENT_JAR ) || exit
-
-    : ${LOG4J_CONF:=$XDF_DIR/conf/log4j.properties}
-    cat <<EELL250
--------
-Log4j log file          : ${LOG4J_CONF}
-EELL250
-    ( <$LOG4J_CONF ) || exit
-
-    # Build CSV value
-    JARS=$( echo $LIB_DIR/*.jar | tr ' ' , )
-#    CONF_OPT="spark.driver.extraJavaOptions=-Dlog4j.configuration=file:$LOG4J_CONF"
-    DRIVER_OPTS=("-DXDF_DATA_ROOT=$XDF_DATA_ROOT")
-    DRIVER_OPTS+=("-Djava.security.auth.login.config=/opt/mapr/conf/mapr.login.conf")
-    DRIVER_OPTS+=("-Dlog4j.configuration=file:$LOG4J_CONF")
-
-    # TODO: use array
-    # CONF_OPTS=( --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:$LOG4J_CONF" )
-    # TODO: conditionally add -Dxdf.json.subs.params=true option
-    # CONF_OPTS+=( --conf spark.driver.extraJavaOptions=-Dxdf.json.subs.params=true )
-    # use: "${CONF_OPTS[@]}""
-    CMD=(
-        $SPARK_HOME/bin/spark-submit
-        --verbose
-#        "${CONF_OPT[@]}"
-        --driver-java-options "'"${DRIVER_OPTS[@]}"'"
-        --class $MAIN_CLASS
-#        --jars $JARS
-        $COMPONENT_JAR
-        ${PARAM_LIST[@]}
+# TODO: use array
+# CONF_OPTS=( -Dlog4j.configuration=file:$LOG4J_CONF )
+# TODO: conditionally add -Dxdf.json.subs.params=true option
+# CONF_OPTS+=( -Dxdf.json.subs.params=true )
+# use: --conf "spark.driver.extraJavaOptions=${CONF_OPTS[@]}"
+CMD=(
+    $SPARK_HOME/bin/spark-submit
+    --verbose
+#    --driver-java-options "${JAVA_PROPS[@]}"
+    --conf "spark.driver.extraJavaOptions=${JAVA_PROPS[*]}"
+    --class $MAIN_CLASS
+    $COMPONENT_JAR
+    # main args
+    -a $APPLICATION_ID
+    -b $BATCH_ID
+    -c $CONFIG_FILE
     )
-}
-echo "\
-----------------------------------------------------------------
-"
-#######################################
 
-( <$SPARK_HOME/conf/spark-env.sh ) || exit
-source $SPARK_HOME/conf/spark-env.sh
-
-export XDF_DATA_ROOT
+#####################################
 
 echo "Run XDF Component $COMPONENT_NAME, command:
 ${CMD[@]}"
 
-[[ $DRYRUN ]] && {
+[[ ${DRYRUN:-0} != 0 ]] && {
+    echo CMD WORDS: ${#CMD[@]}
+    for a in "${CMD[@]}"; do echo -- "$a"; done
     echo DRYRUN exit
+#test: DRYRUN=1 ./execute-component.sh -m zero -a p1 -b 01 -c c.cfg
     exit
 }
 
-${CMD[@]}
+export XDF_DATA_ROOT
+"${CMD[@]}"
 RETVAL=$?
 
 ### These CMD work if I run directly from terminal ( not through script )
 #XDF_HOME=/dfs/opt/bda/xdf-ngsr-current /opt/mapr/spark/spark-current/bin/spark-submit --verbose --driver-java-options '-Djava.security.auth.login.config=/opt/mapr/conf/mapr.login.conf -DXDF_DATA_ROOT=hdfs:///data/bda -Dlog4j.configuration=file:/dfs/opt/bda/xdf-ngsr/xdf-ngsr-1.0.0_dev/conf/log4j.properties' --class sncr.xdf.sql.SQLComponent --jars /dfs/opt/bda/xdf-ngsr/xdf-ngsr-1.0.0_dev/lib/xdf-rest-1.0.0_dev-all.jar /dfs/opt/bda/xdf-ngsr/xdf-ngsr-1.0.0_dev/lib/xdf-rest-1.0.0_dev-all.jar -b SQLTEST1 -c file:///dfs/opt/bda/apps/xda-ux-sr-comp-dev-1.0.0_dev/conf/sqlComponentWithMeta.jconf -a xda-ux-sr-comp-dev
 #XDF_HOME=/dfs/opt/bda/xdf-ngsr-current /opt/mapr/spark/spark-current/bin/spark-submit --verbose --driver-java-options '-Djava.security.auth.login.config=/opt/mapr/conf/mapr.login.conf -DXDF_DATA_ROOT=hdfs:///data/bda -Dlog4j.configuration=file:/dfs/opt/bda/xdf-ngsr/xdf-ngsr-1.0.0_dev/conf/log4j.properties' --class sncr.xdf.component.ZeroComponent --jars /dfs/opt/bda/xdf-ngsr/xdf-ngsr-1.0.0_dev/lib/xdf-rest-1.0.0_dev-all.jar /dfs/opt/bda/xdf-ngsr/xdf-ngsr-1.0.0_dev/lib/xdf-rest-1.0.0_dev-all.jar -b TEST2 -c file:///dfs/opt/bda/apps/xda-ux-sr-comp-dev-1.0.0_dev/conf/zeroComponent.jconf -a xda-ux-sr-comp-dev
-
-
 
 cat<<EEOORR
 ----------------------------------------------------------------
