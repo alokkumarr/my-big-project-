80a3789989a8a78f9d5889062f93661991cf6b2e
Pawan Tejwani <ptptejwani@gmail.com>
2017-11-27 17:58:26 +0530

    SAW-1335: adding test cases

--- saw-security/src/main/java/com/sncr/saw/security/app/repository/impl/RoleRepositoryDaoImpl.java
+++ saw-security/src/main/java/com/sncr/saw/security/app/repository/impl/RoleRepositoryDaoImpl.java
@@ -18,42 +18,44 @@ import java.util.Date;
 @Repository
 public class RoleRepositoryDaoImpl implements RoleRepository {
 
-    private static final Logger logger = LoggerFactory.getLogger(RoleRepositoryDaoImpl.class);
+  private static final Logger logger = LoggerFactory.getLogger(RoleRepositoryDaoImpl.class);
 
-    private final JdbcTemplate jdbcTemplate;
+  private final JdbcTemplate jdbcTemplate;
 
-    @Autowired
-    public RoleRepositoryDaoImpl(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+  @Autowired
+  public RoleRepositoryDaoImpl(JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
+
+  // we decided that we will initially create only admin user
+
+  @Override
+  public long createNewAdminRoleDao(Long custId) {
+    if (custId != null) {
+      String sql =
+          "INSERT INTO `ROLES` (`CUSTOMER_SYS_ID`, `ROLE_NAME`, `ROLE_CODE`, `ROLE_DESC`," +
+              "`ROLE_TYPE`, `DATA_SECURITY_KEY`, `ACTIVE_STATUS_IND`, `CREATED_DATE`, `CREATED_BY`)"
+              +
+              " VALUES (?, 'ADMIN', ?, 'Admin User', 'ADMIN', 'NA', '1'," +
+              "?, 'admin')";
+
+      KeyHolder keyHolder = new GeneratedKeyHolder();
+
+      jdbcTemplate.update(
+          new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+              PreparedStatement ps = con.prepareStatement(sql, new String[]{"ROLE_SYS_ID"});
+              ps.setLong(1, custId);
+              ps.setString(2, custId.toString() + "_ADMIN_USER");
+              ps.setDate(3, new java.sql.Date(new Date().getTime()));
+              return ps;
+            }
+          },
+          keyHolder
+      );
+      return (Long) keyHolder.getKey();
     }
-
-    // we decided that we will initially create only admin user
-
-    @Override
-    public long createNewAdminRoleDao(Long custId) {
-        if (custId!=null) {
-            String sql = "INSERT INTO `ROLES` (`CUSTOMER_SYS_ID`, `ROLE_NAME`, `ROLE_CODE`, `ROLE_DESC`," +
-                    "`ROLE_TYPE`, `DATA_SECURITY_KEY`, `ACTIVE_STATUS_IND`, `CREATED_DATE`, `CREATED_BY`)" +
-                    " VALUES (?, 'ADMIN', ?, 'Admin User', 'ADMIN', 'NA', '1'," +
-                    "?, 'admin')";
-
-            KeyHolder keyHolder = new GeneratedKeyHolder();
-
-            jdbcTemplate.update(
-                    new PreparedStatementCreator() {
-                        @Override
-                        public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-                            PreparedStatement ps = con.prepareStatement(sql, new String[]{"ROLE_SYS_ID"});
-                            ps.setLong(1, custId);
-                            ps.setString(2, custId.toString()+"_ADMIN_USER");
-                            ps.setDate(3, new java.sql.Date(new Date().getTime()));
-                            return ps;
-                        }
-                    },
-                    keyHolder
-            );
-            return (Long) keyHolder.getKey();
-        }
-        return -1L;
-    }
+    return -1L;
+  }
 }
