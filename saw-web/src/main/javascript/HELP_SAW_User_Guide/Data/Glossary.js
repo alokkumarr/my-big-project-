663277fb4da0840118ea48edfd99ee6896057134
Akash Agrawal <akagr@outlook.com>
2017-12-05 10:56:00 -0500

    Added help and linked to it in prod mode

---
+++ saw-web/src/main/javascript/HELP_SAW_User_Guide/Data/Glossary.js
@@ -0,0 +1,1 @@
+define({numchunks:1,prefix:'Glossary_Chunk',terms:[{t:'BDA',c:0},{t:'Data Lake',c:0},{t:'DNS',c:0},{t:'Elasticsearch',c:0},{t:'FQDN',c:0},{t:'FTP',c:0},{t:'IP',c:0},{t:'KPI',c:0},{t:'SAW',c:0},{t:'SIP',c:0}]});
