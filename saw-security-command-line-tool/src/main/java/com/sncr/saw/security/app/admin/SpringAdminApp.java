0db07140e79f654a5827da23a316419a97513440
Pawan Tejwani <ptptejwani@gmail.com>
2017-12-18 15:57:57 +0530

    SAW-1335: separating two modules of saw security

---
+++ saw-security-command-line-tool/src/main/java/com/sncr/saw/security/app/admin/SpringAdminApp.java
@@ -0,0 +1,18 @@
+package com.sncr.saw.security.app.admin;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+// package scanning is necessary because we are
+// having module dependency
+@SpringBootApplication(scanBasePackages =
+    {"com.sncr.saw.security.app"})
+public class SpringAdminApp {
+
+  public static void main(String[] args) {
+    ConfigurableApplicationContext context = SpringApplication.run(SpringAdminApp.class, args);
+  }
+
+}
