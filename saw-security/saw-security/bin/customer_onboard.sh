e94340acad423cc44a34bc7dfba8909d6988f246
Pawan <pawan.tejwani@synchronoss.com>
2017-12-28 02:08:07 +0530

    SAW-1335: removing the statement pointing to docker.

--- saw-security/saw-security/bin/customer_onboard.sh
+++ saw-security/saw-security/bin/customer_onboard.sh
@@ -5,7 +5,6 @@ read -e -p "Enter temporary port for running shell (Default: 9999): " port
 port="${port:-${DEFAULT}}"
 echo $port
 
-# assume we are inside docker and start executing from following file
 admin_jar_fnm=( /opt/bda/saw-security/lib/saw-security-command-line-tool-*.jar )
 security_jar_fnm=( /opt/bda/saw-security/lib/saw-security-*-classes.jar )
 
