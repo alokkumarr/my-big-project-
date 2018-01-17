397fdb709e746c221da84f484ed3d187dc2e6aca
Anirudh Venkatesh <Anirudh.NV@Synchronoss.com>
2018-01-17 15:21:43 +0530

    including new drop down with suggested string filters

--- saw-web/src/main/javascript/app/modules/analyze/components/filter/filters/string-filter.component.js
+++ saw-web/src/main/javascript/app/modules/analyze/components/filter/filters/string-filter.component.js
@@ -1,4 +1,6 @@
+import * as map from 'lodash/map';
 import * as isEmpty from 'lodash/isEmpty';
+import * as forEach from 'lodash/forEach';
 import * as template from './string-filter.component.html';
 
 export const StringFilterComponent = {
@@ -8,11 +10,38 @@ export const StringFilterComponent = {
     onChange: '&'
   },
   controller: class StringFilterController {
-    constructor($mdConstant) {
+    constructor($mdConstant, $filter, $translate) {
       'ngInject';
       this.isEmpty = isEmpty;
       const semicolon = 186;
       this.separatorKeys = [$mdConstant.KEY_CODE.ENTER, $mdConstant.KEY_CODE.COMMA, semicolon];
+      this.presets = [{
+        value: 'EQ',
+        keyword: 'EQUALS'
+      }, {
+        value: 'NE',
+        keyword: 'NOT_EQUAL'
+      }, {
+        value: 'IsIn',
+        keyword: 'IS_IN'
+      }, {
+        value: 'IsNotIn',
+        keyword: 'IS_NOT_IN'
+      }, {
+        value: 'Contains',
+        keyword: 'CONTAINS'
+      }, {
+        value: 'SW',
+        keyword: 'STARTS_WITH'
+      }, {
+        value: 'LQ',
+        keyword: 'ENDS_WITH'
+      }];
+      $translate(map(this.presets, 'keyword')).then(translations => {
+        forEach(this.presets, preset => {
+          preset.label = translations[preset.keyword];
+        });
+      });
     }
 
     $onInit() {
