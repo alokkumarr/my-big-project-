17fb3203e7333d8c3eaa8ad281fc7826ec97bd5e
Pradeep Tripathi <pradeep.manitripathi@synchronoss.com>
2018-02-08 10:35:48 +0530

    aggregation for ES-report
    Ticket ID: SAW-1995,SAW-2123

--- saw-services/saw-es-querybuilder/src/main/java/com/synchronoss/SAWElasticTransportService.java
+++ saw-services/saw-es-querybuilder/src/main/java/com/synchronoss/SAWElasticTransportService.java
@@ -10,13 +10,17 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.github.fge.jsonschema.core.exceptions.ProcessingException;
 import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
-import com.synchronoss.ESProxy;
 
+
+import com.synchronoss.querybuilder.ReportAggregationBuilder;
+import com.synchronoss.querybuilder.model.report.DataField;
+import com.synchronoss.querybuilder.model.report.SqlBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,7 +31,6 @@ public class SAWElasticTransportService {
 
   private static String HITS= "hits";
   private static String _SOURCE ="_source";
-  private static String TotalRecords = "total";
 
   private static String execute(String query, String jsonString, String dsk, String username,
     String moduleName,boolean isReport) throws JsonProcessingException, IOException, NullPointerException{
@@ -59,53 +62,27 @@ public class SAWElasticTransportService {
     String responseString = response.body().string();
     logger.trace("responseStringdfd" +responseString);
     JsonNode esResponse = objectMapper.readTree(responseString);
+
     if (esResponse.get("data") == null)
     {
       throw new NullPointerException("Data is not available based on provided query criteria");
     }
-    if(isReport)
-      return  buildReportData(esResponse.get("data")).toString();
-     JsonNode finalResponse = objectMapper.readTree(esResponse.get("data").toString());
+    JsonNode finalResponse = objectMapper.readTree(esResponse.get("data").toString());
+    // For elastic search report data
+    if(isReport) {
+      if (finalResponse.get("aggregations")!=null)
+      {
+         return buildAggregatedReportData(jsonString,
+                 finalResponse.get("aggregations")).toString();
+      }
+      else
+      {
+        return buildReportData(esResponse.get("data")).toString();
+      }
+    }
+     // In case of Pivot and chart
      return finalResponse.get("aggregations").toString();
   }
-
-  private static List<String> executeEsReport(String query, String jsonString, String dsk, String username,
-                                String moduleName) throws JsonProcessingException, IOException, NullPointerException{
-    String url = System.getProperty("url");
-    JsonNode repository = BuilderUtil.getRepositoryNodeTree(jsonString, "esRepository");
-    String indexName = repository.get("indexName").asText();
-    String type = repository.get("type").textValue();
-    OkHttpClient client = new OkHttpClient();
-    MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-    ESProxy esProxy = new ESProxy();
-    esProxy.setStorageType("ES");
-    esProxy.setIndexName(indexName);
-    esProxy.setObjectType(type);
-    esProxy.setVerb("_search");
-    esProxy.setQuery(query);
-    esProxy.setModuleName(moduleName);
-    esProxy.setDsk(dsk);
-    esProxy.setUsername(username);
-    ObjectMapper mapper = new ObjectMapper();
-    mapper.enable(DeserializationFeature.FAIL_ON_READING_DUP_TREE_KEY);
-    mapper.disable(SerializationFeature.INDENT_OUTPUT);
-    RequestBody body = RequestBody.create(JSON, mapper.writeValueAsBytes(esProxy));
-    Request req = new Request.Builder().post(body).url(url).build();
-    logger.trace("Elasticsearch request: {}", req);
-    Response response = client.newCall(req).execute();
-    logger.trace("Elasticsearch response: {}", response);
-    ObjectMapper objectMapper = new ObjectMapper();
-    objectMapper.enable(DeserializationFeature.FAIL_ON_READING_DUP_TREE_KEY);
-    String responseString = response.body().string();
-    logger.trace("responseStringdfd" +responseString);
-    JsonNode esResponse = objectMapper.readTree(responseString);
-    if (esResponse.get("data") == null)
-    {
-      throw new NullPointerException("Data is not available based on provided query criteria");
-    }
-      return  buildReportData(esResponse.get("data"));
-  }
-
 
   /**
    *
@@ -162,4 +139,29 @@ public class SAWElasticTransportService {
     return data;
   }
 
+    /**
+     * Parse the report aggregated data.
+     * @param reportDefinition
+     * @param jsonNode
+     * @return
+     */
+  private static String buildAggregatedReportData(String reportDefinition, JsonNode jsonNode)
+  {
+      JsonNode node =null;
+      try {
+          SqlBuilder sqlBuilderNode = BuilderUtil.getNodeTreeReport(reportDefinition, "sqlBuilder");
+         List<DataField> aggregationField = ReportAggregationBuilder.getAggregationField(sqlBuilderNode.getDataFields());
+          ESReportAggregationParser esReportAggregationParser = new ESReportAggregationParser(
+                  sqlBuilderNode.getDataFields(),aggregationField);
+          List<Object> data = esReportAggregationParser.parseData(jsonNode);
+          ObjectMapper mapper = new ObjectMapper();
+           node = mapper.valueToTree(data);
+      }
+      catch (ProcessingException e) {
+      }
+      catch (IOException e) {
+          e.printStackTrace();
+      }
+      return node.toString();
+  }
 }
