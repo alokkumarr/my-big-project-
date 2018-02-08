a3501ba292e0fa8133f6a88a32327eee8cd79877
Alex <alexander.krivorotko@gmail.com>
2018-02-08 16:14:28 +0700

    add mozilla headless for debug

--- saw-web/conf/protractor.conf.js
+++ saw-web/conf/protractor.conf.js
@@ -3,24 +3,24 @@ const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
 
 /**
  * Note about intervals:
- * Defined to be dependent on environment where tests are executed.
- * Running against distribution package in CI requires extended timeouts
+ * Defined to be dependent on environment where tests are executed. Running against distribution package in CI requires
+ * extended timeouts
  */
 
 
 /**
- * Sets the amount of time to wait for a page load to complete before returning
- * an error.  If the timeout is negative, page loads may be indefinite.
+ * Sets the amount of time to wait for a page load to complete before returning an error.  If the timeout is negative,
+ * page loads may be indefinite.
  */
 const pageLoadTimeout = webpackHelper.distRun() ? 600000 : 30000;
 
 /**
- * Specifies the amount of time the driver should wait when searching for an
- * element if it is not immediately present.
+ * Specifies the amount of time the driver should wait when searching for an element if it is not immediately present.
  */
 
 const implicitlyWait = webpackHelper.distRun() ? 600000 : 10000;
-const extendedImplicitlyWait = webpackHelper.distRun() ? 1200000 : 30000; // = 30 sec; Sometimes element will not appear so fast
+const extendedImplicitlyWait = webpackHelper.distRun() ? 1200000 : 30000; // = 30 sec; Sometimes element will not
+                                                                          // appear so fast
 
 /**
  * Defines the maximum amount of time to wait for a condition
@@ -47,9 +47,8 @@ const allScriptsTimeout = 60000;
 const pageResolveTimeout = 1000;
 
 /**
- * Note: Prefix with "../saw-web" because end-to-end tests are invoked
- * from "saw-dist" when run against the distribution package.  The
- * same path also works when run directly out of "saw-web".
+ * Note: Prefix with "../saw-web" because end-to-end tests are invoked from "saw-dist" when run against the
+ * distribution package. The same path also works when run directly out of "saw-web".
  */
 const testDir = '../saw-web/src/test';
 
@@ -82,6 +81,9 @@ exports.config = {
         '--disable-gpu',
         '--window-size=2880,1800'
       ]
+    },
+    'moz:firefoxOptions': {
+      args: ['--headless']
     }
   },
   jasmineNodeOpts: {
@@ -95,24 +97,24 @@ exports.config = {
   suites: webpackHelper.distRun() ? {
     /**
      * Suites for test run invoked from Maven which is used in Bamboo continuous integration.
-     * Note: In the long term there should just be a single set of suites used everywhere (for both
-     * continuous integration and local front-end development). However, for now use a separate suite
-     * that allows enabling known working tests (working reliably without flakiness) incrementally one
-     * by one in continuous integration, while working on fixing the rest.
+     * Note: In the long term there should just be a single set of suites used everywhere (for both continuous
+     * integration and local front-end development). However, for now use a separate suite that allows enabling known
+     * working tests (working reliably without flakiness) incrementally one by one in continuous integration, while
+     * working on fixing the rest.
      */
     authentication: [
       webpackHelper.root(testDir + '/e2e-tests/goToAnalyze.test.js')
     ],
     charts: [
       /*webpackHelper.root(testDir + '/e2e-tests/charts/applyFiltersToCharts.js'),
-      webpackHelper.root(testDir + '/e2e-tests/charts/createAndDeleteCharts.test.js'),
-      webpackHelper.root(testDir + '/e2e-tests/charts/previewForCharts.test.js')*/
+       webpackHelper.root(testDir + '/e2e-tests/charts/createAndDeleteCharts.test.js'),
+       webpackHelper.root(testDir + '/e2e-tests/charts/previewForCharts.test.js')*/
     ],
     root: [
       /*webpackHelper.root(testDir + '/e2e-tests/analyze.test.js'),
-      webpackHelper.root(testDir + '/e2e-tests/createPivot.test.js'),
-      webpackHelper.root(testDir + '/e2e-tests/createReport.test.js'),
-      webpackHelper.root(testDir + '/e2e-tests/priviliges.test.js'),*/
+       webpackHelper.root(testDir + '/e2e-tests/createPivot.test.js'),
+       webpackHelper.root(testDir + '/e2e-tests/createReport.test.js'),
+       webpackHelper.root(testDir + '/e2e-tests/priviliges.test.js'),*/
     ]
   } : {
     /**
@@ -120,13 +122,13 @@ exports.config = {
      */
     charts: [
       /*webpackHelper.root(testDir + '/e2e-tests/charts/applyFiltersToCharts.js'),
-      webpackHelper.root(testDir + '/e2e-tests/charts/createAndDeleteCharts.test.js'),
-      webpackHelper.root(testDir + '/e2e-tests/charts/previewForCharts.test.js')*/
+       webpackHelper.root(testDir + '/e2e-tests/charts/createAndDeleteCharts.test.js'),
+       webpackHelper.root(testDir + '/e2e-tests/charts/previewForCharts.test.js')*/
     ],
     root: [
       /*webpackHelper.root(testDir + '/e2e-tests/analyze.test.js'),
-      webpackHelper.root(testDir + '/e2e-tests/createPivot.test.js'),
-      webpackHelper.root(testDir + '/e2e-tests/createReport.test.js'),*/
+       webpackHelper.root(testDir + '/e2e-tests/createPivot.test.js'),
+       webpackHelper.root(testDir + '/e2e-tests/createReport.test.js'),*/
       //webpackHelper.root(testDir + '/e2e-tests/debug.test.js') // for testing purposes
       webpackHelper.root(testDir + '/e2e-tests/priviliges.test.js')
     ],
