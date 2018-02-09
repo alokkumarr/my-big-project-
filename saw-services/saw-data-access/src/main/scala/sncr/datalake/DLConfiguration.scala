5ff56cbf3786a8e68eeccd5ff40c818fa1885eef
Pradeep Tripathi <pradeep.manitripathi@synchronoss.com>
2018-02-09 17:50:51 +0530

    Added nginx timeout as configurable property for saw-proxy. Added SQL Executor response "poling timeout" as configurable Ticket ID: SAW-2274

--- saw-services/saw-data-access/src/main/scala/sncr/datalake/DLConfiguration.scala
+++ saw-services/saw-data-access/src/main/scala/sncr/datalake/DLConfiguration.scala
@@ -58,6 +58,7 @@ object DLConfiguration {
   val commonLocation = cfg.getString ("sql-executor.output-location")
   val semanticLayerTempLocation = cfg.getString ("sql-executor.semantic-layer-tmp-location")
   val defaultOutputType = cfg.getString ("sql-executor.output-type")
+  val waitTime= if (cfg.hasPath("sql-executor.wait-time")) cfg.getInt("sql-executor.wait-time") else 60
 
   val jarFiles = HFileOperations.listJarFiles(jarLocation, ".jar")
   logger debug s"Attach to Spark job the following jar files: ${jarFiles.mkString("[", ", ", "]")}"
