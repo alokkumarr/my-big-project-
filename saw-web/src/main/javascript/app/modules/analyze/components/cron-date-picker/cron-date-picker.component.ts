1f87dd6bc3f40cbb99e46ec123206993522ec46b
Anirudh Venkatesh <Anirudh.NV@Synchronoss.com>
2018-02-19 11:15:36 +0530

    removing seconds and adding 0 to single value integers in minutes

--- saw-web/src/main/javascript/app/modules/analyze/components/cron-date-picker/cron-date-picker.component.ts
+++ saw-web/src/main/javascript/app/modules/analyze/components/cron-date-picker/cron-date-picker.component.ts
@@ -15,12 +15,16 @@ export class CronDatePickerComponent implements OnInit {
   @Output() onDateChange = new EventEmitter();
 
   ngOnInit() {
+    const timeArray = [];
+    const d = new Date();
+    const h = d.getHours();
+    const m = d.getMinutes();
     this.selectOptions = {
-      minutes: this.range(1, 59),
-      seconds: this.range(1, 59),
+      minutes: ['00','01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59'],
       hourTypes: ['AM', 'PM']
     };
-    this.selectOptions.hours = this.use24HourTime ? this.range(0, 23) : this.range(0, 12);
+    this.model.hourType = 'AM';
+    this.selectOptions.hours = this.use24HourTime ? this.range(1, 23) : this.range(1, 12);
   }
 
   triggerChange() {
