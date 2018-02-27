6c45e03256bcff981dfc947486e65a1cfcd00e15
Chris Haarstick <Chris.Haarstick@synchronoss.com>
2018-02-27 13:06:21 -0500

    Committing summariser generics
    documentation updated
    
    unit tests added
    
    tests passed

---
+++ R/summariser.R
@@ -0,0 +1,206 @@
+#'DataFrame Summariser function
+#'
+#'Aggregation function. Configuarable inputs for grouping variables, measure
+#'variables and aggregation functions.
+#'
+#'@param df DataFrame
+#'@param group_vars vector of column names. used in the group by aggregration.
+#'  can be zero or more columns. set to NULL for no grouping
+#'@param measure_vars vector of column names. colnames to aggregrate. can be one
+#'  or more columns. Default is NULL which will calculate counts only
+#'@param fun vector of function names. can be one or more columns. default is
+#'  sum. Current functions supported: n_distinct, min, max, sum, mean, variance,
+#'  sd, kurtosis, skewness, and percentile
+#'@param ... additional parameters passed to aggregate function
+#'
+#'@return returns DataFrame with one row for each unique combination of grouping
+#'  variables provided
+#'
+#'@examples
+#'library(dplyr)
+#'
+#'
+#'# Create toy dataset
+#'set.seed(319)
+#'id_vars <- seq(101, 200, by=1)
+#'dates <- seq(from=Sys.Date()-365, to=Sys.Date(), by="day")
+#'cat1 <- c("A", "B")
+#'cat2 <- c("X", "Y", "Z")
+#'
+#'dat <- data.frame()
+#'for(id in id_vars){
+#'   n <- floor(runif(1)*100)
+#'   d <- data.frame(id = id,
+#'                  date = sample(dates, n, replace = TRUE),
+#'                  cat1 = sample(cat1, n, replace = TRUE),
+#'                  cat2 = sample(cat2, n, replace = TRUE),
+#'                  metric1 = sample(1:5, n, replace = TRUE),
+#'                  metric2 = rnorm(n, mean=50, sd = 5))
+#'  dat <- rbind(dat, d)
+#'}
+#'
+#'summariser(dat,
+#'           group_vars = c("id", "date", "cat1", "cat2"),
+#'           measure_vars = c("metric1", "metric2"),
+#'           fun = c("sum"))
+#'@export
+summariser <- function(df, ...) {
+  UseMethod("summariser", df)
+}
+
+
+#' @importFrom magrittr %>%
+#' @importFrom dplyr n
+#' @importFrom dplyr vars
+#' @rdname summariser
+summariser.data.frame <- function(df,
+                                  group_vars,
+                                  measure_vars = NULL,
+                                  fun = c("sum"),
+                                  ...) {
+  if (!all(
+    fun %in% c(
+      "n_distinct",
+      "min",
+      "max",
+      "sum",
+      "mean",
+      "variance",
+      "sd",
+      "kurtosis",
+      "skewness",
+      "percentile"
+    )
+  )) {
+    stop(cat(
+      "aggregation function should be one of the following functions",
+      c(
+        "n_distinct",
+        "min",
+        "max",
+        "sum",
+        "mean",
+        "variance",
+        "sd",
+        "kurtosis",
+        "skewness",
+        "percentile"
+      )
+    ))
+  }
+
+
+  # Check for measure variables provided
+  if (is.null(measure_vars)) {
+    message("No measure vars provided. Providing counts of records only.")
+    fun <- "n_distinct"
+    measure_vars <- "rn"
+    df <- df %>%
+      dplyr::group_by_at(.vars = group_vars) %>%
+      dplyr::mutate(rn = 1:n()) %>%
+      dplyr::ungroup()
+  }
+
+
+  # Aggregate Data
+  all_vars <- c(group_vars, measure_vars)
+  group_by_vars <- if (is.null(group_vars))
+    vars()
+  else
+    group_vars
+  agg <- df %>%
+    dplyr::select_at(all_vars) %>%
+    dplyr::group_by_at(group_by_vars) %>%
+    dplyr::summarise_all(.funs = fun, ...) %>%
+    dplyr::ungroup()
+
+
+  # Rename measure variables
+  new_measure_vars <- setdiff(colnames(agg), group_vars)
+  if (length(measure_vars) < 2 | length(fun) < 2) {
+    agg <-
+      dplyr::rename_(agg, .dots = setNames(new_measure_vars, paste(measure_vars, fun, sep =
+                                                                     "_")))
+  }
+
+  agg
+}
+
+
+
+#' @importFrom magrittr %>%
+#' @importFrom dplyr vars
+#' @rdname summariser
+#' @export
+summariser.tbl_spark <- function(df,
+                                 group_vars,
+                                 measure_vars = NULL,
+                                 fun = c("sum"),
+                                 ...) {
+  if (!all(
+    fun %in% c(
+      "n_distinct",
+      "min",
+      "max",
+      "sum",
+      "mean",
+      "variance",
+      "sd",
+      "kurtosis",
+      "skewness",
+      "percentile"
+    )
+  )) {
+    stop(cat(
+      "aggregation function should be one of the following functions",
+      c(
+        "n_distinct",
+        "min",
+        "max",
+        "sum",
+        "mean",
+        "variance",
+        "sd",
+        "kurtosis",
+        "skewness",
+        "percentile"
+      )
+    ))
+  }
+
+
+  # Check for measure variables provided
+  if (is.null(measure_vars)) {
+    message("No measure vars provided. Providing counts of records only.")
+    fun <- "n_distinct"
+    measure_vars <- "rn"
+    df <- df %>%
+      dplyr::arrange_at(.vars = group_vars) %>%
+      dplyr::mutate(rn = row_number())
+  }
+
+
+  # Aggregate Data
+  all_vars <- c(group_vars, measure_vars)
+  group_by_vars <- if (is.null(group_vars))
+    vars()
+  else
+    group_vars
+  agg <- df %>%
+    dplyr::select_at(all_vars) %>%
+    dplyr::group_by_at(group_by_vars) %>%
+    dplyr::summarise_all(.funs = fun, ...) %>%
+    dplyr::ungroup()
+
+
+  # Rename measure variables
+  if (length(measure_vars) < 2 | length(fun) < 2) {
+    agg <-
+      agg %>% dplyr::select_(.dots = setNames(colnames(agg), c(
+        group_vars, paste(measure_vars, fun, sep =
+                            "_")
+      )))
+  }
+
+  agg
+}
