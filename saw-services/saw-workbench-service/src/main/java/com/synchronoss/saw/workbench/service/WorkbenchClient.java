2ec594ee750cdeb4e772d3a94338fff39168a272
Saurav Paul <Saurav.Paul@synchronoss.com>
2018-03-13 16:18:54 -0400

    SAW-2152 : Deactivating check style for time being to speed the integration test

--- saw-services/saw-workbench-service/src/main/java/com/synchronoss/saw/workbench/service/WorkbenchClient.java
+++ saw-services/saw-workbench-service/src/main/java/com/synchronoss/saw/workbench/service/WorkbenchClient.java
@@ -10,47 +10,46 @@ import java.io.File;
 import java.net.URI;
 
 public class WorkbenchClient {
-    private static final String WORKBENCH_JAR =
-        "/opt/bda/saw-workbench-service/saw-workbench-spark.jar";
-    private final Logger log = LoggerFactory.getLogger(getClass().getName());
+  private static final String WORKBENCH_JAR = "/opt/bda/saw-workbench-service/saw-workbench-spark.jar";
+  private final Logger log = LoggerFactory.getLogger(getClass().getName());
 
-    public void submit(String livyUri, String root, String project,
-                       String component, String config) throws Exception {
-        LivyClient client = new LivyClientBuilder()
-            .setURI(new URI(livyUri))
-            .build();
-        log.debug("Uploading Workbench JAR");
-        client.uploadJar(new File(WORKBENCH_JAR));
-        log.info("Submitting Workbench job");
-        JobHandle<Integer> job = client.submit(
-            new WorkbenchJob(root, project, component, config));
-        job.addListener(new JobHandle.Listener<Integer>() {
-          @Override
-          public void onJobSucceeded(JobHandle<Integer> job, Integer result) {
-            client.stop(true);
-            log.info("Workbench job successfully completed");
-          }
-          
-          @Override
-          public void onJobStarted(JobHandle<Integer> job) {
-            log.info("Starting Workbench job");}
-          
-          @Override
-          public void onJobQueued(JobHandle<Integer> job) {
-            log.info("Workbench job queued");}
-          
-          @Override
-          public void onJobFailed(JobHandle<Integer> job, Throwable cause) {
-            client.stop(true);
-            log.info("Workbench job Failed.");
-          }
-          
-          @Override
-          public void onJobCancelled(JobHandle<Integer> job) {
-            client.stop(true);
-            log.info("Workbench job Cancelled.");
-          }
-        });
-        log.info("Submitted job");
-    }
+  public void submit(String livyUri, String root, String project, String component, String config)
+      throws Exception {
+    LivyClient client = new LivyClientBuilder().setURI(new URI(livyUri)).build();
+    log.debug("Uploading Workbench JAR");
+    client.uploadJar(new File(WORKBENCH_JAR));
+    log.info("Submitting Workbench job");
+    JobHandle<Integer> job = client.submit(new WorkbenchJob(root, project, component, config));
+    job.addListener(new JobHandle.Listener<Integer>() {
+
+      @Override
+      public void onJobSucceeded(JobHandle<Integer> job, Integer result) {
+        client.stop(true);
+        log.info("Workbench job successfully completed");
+      }
+
+      @Override
+      public void onJobStarted(JobHandle<Integer> job) {
+        log.info("Starting Workbench job");
+      }
+
+      @Override
+      public void onJobQueued(JobHandle<Integer> job) {
+        log.info("Workbench job queued");
+      }
+
+      @Override
+      public void onJobFailed(JobHandle<Integer> job, Throwable cause) {
+        client.stop(true);
+        log.info("Workbench job Failed.");
+      }
+
+      @Override
+      public void onJobCancelled(JobHandle<Integer> job) {
+        client.stop(true);
+        log.info("Workbench job Cancelled.");
+      }
+    });
+    log.info("Submitted job");
+  }
 }
