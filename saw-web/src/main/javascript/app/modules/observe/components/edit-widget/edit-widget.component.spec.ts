88d69dc78316de97cad05f6ba13b26e19db7c2f1
Akash Agrawal <aaga0003@synchronoss.com>
2018-04-04 18:39:08 +0530

    SAW-2401 Marked tests async

--- saw-web/src/main/javascript/app/modules/observe/components/edit-widget/edit-widget.component.spec.ts
+++ saw-web/src/main/javascript/app/modules/observe/components/edit-widget/edit-widget.component.spec.ts
@@ -20,29 +20,37 @@ configureTests();
 
 describe('Edit Widget', () => {
   let fixture: ComponentFixture<EditWidgetComponent>, el: HTMLElement;
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [MaterialModule, AddWidgetModule],
-      declarations: [EditWidgetComponent],
-      providers: [{ provide: ObserveService, useValue: ObserveServiceStub }]
-    }).compileComponents();
+  beforeEach(
+    fakeAsync(() => {
+      TestBed.configureTestingModule({
+        imports: [MaterialModule, AddWidgetModule],
+        declarations: [EditWidgetComponent],
+        providers: [{ provide: ObserveService, useValue: ObserveServiceStub }]
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(EditWidgetComponent);
+      fixture = TestBed.createComponent(EditWidgetComponent);
 
-    el = fixture.nativeElement;
+      el = fixture.nativeElement;
 
-    fixture.detectChanges();
-  });
+      fixture.detectChanges();
+    })
+  );
 
-  it('should exist', () => {
-    expect(typeof fixture.componentInstance.prepareKPI).to.equal('function');
-  });
+  it(
+    'should exist',
+    fakeAsync(() => {
+      expect(typeof fixture.componentInstance.prepareKPI).to.equal('function');
+    })
+  );
 
-  it('should not show kpi widget without input', () => {
-    expect(fixture.componentInstance.editItem).to.be.undefined;
+  it(
+    'should not show kpi widget without input',
+    fakeAsync(() => {
+      expect(fixture.componentInstance.editItem).to.be.undefined;
 
-    expect(el.querySelector('widget-kpi')).to.be.null;
-  });
+      expect(el.querySelector('widget-kpi')).to.be.null;
+    })
+  );
 
   it(
     'should show kpi widget if editItem present',
