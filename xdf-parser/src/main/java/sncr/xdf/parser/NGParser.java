7ba6853fc1488b74415308c1fcfbc2379e28ec45
Sergey Ryabov <sergey.ryabov@synchronoss.com>
2018-04-11 12:13:33 -0400

    SAW-2631: Bug fixed in NGParser, AsynchNGParser

--- xdf-parser/src/main/java/sncr/xdf/parser/NGParser.java
+++ xdf-parser/src/main/java/sncr/xdf/parser/NGParser.java
@@ -233,7 +233,7 @@ public class NGParser extends AbstractComponent implements WithDLBatchWriter, Wi
             logger.error(error);
             return rc;
         }
-        ctx.resultDataDesc.add(new MoveDataDescriptor(tempDir, outputDataSetLocation, outputDataSetName, mode, outputFormat,pkeys));
+        ctx.resultDataDesc.add(new MoveDataDescriptor(tempDir  + Path.SEPARATOR + outputDataSetName, outputDataSetLocation, outputDataSetName, mode, outputFormat,pkeys));
         return 0;
     }
 
