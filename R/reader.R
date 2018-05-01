f3f8228f2b7a112b3bf4f3944e062ba26fbcbccc
Chris Haarstick <Chris.Haarstick@synchronoss.com>
2018-05-01 16:07:17 -0400

    Adding reader function to read files into spark
    Updated writer tests to remove hard coded file path

---
+++ R/reader.R
@@ -0,0 +1,38 @@
+
+#' Spark Reader Function
+#'
+#' Function to read files into Spark. Supports several file types, including
+#' text, csv, parquet, and json
+#'
+#' @param sc A spark_connection.
+#' @param name The name to assign to the newly generated table
+#' @param path The path to the file. Needs to be accessible from the cluster.
+#'   Supports the "hdfs://", "s3a://" and "file://" protocols
+#' @param repartition The number of partitions used to distribute the generated
+#'   table. Use 0 (the default) to avoid partitioning.
+#' @param memory Logical; should the data be loaded eagerly into memory? (That
+#'   is, should the table be cached?)
+#' @param overwrite Logical; overwrite the table with the given name if it
+#'   already exists?
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+reader <- function(sc, name, path, repartition = 0, memory = TRUE, overwrite = TRUE, ...) {
+  checkmate::assert_class(sc, "spark_connection")
+  checkmate::assert_character(name)
+  checkmate::assert_file_exists(path)
+  checkmate::assert_number(repartition, lower = 0)
+  checkmate::assert_flag(memory)
+  checkmate::assert_flag(overwrite)
+
+  type <- tools::file_ext(path)
+  checkmate::assert_choice(type, c("csv", "parquet", "json", "jdbc", "source", "table", "text"))
+
+  spark_read_fun <- match.fun(paste("spark_read", type, sep = "_"))
+  spark_read_fun(sc, name, path, repartition = 0, memory = TRUE, overwrite = TRUE, ...)
+}
+
+
---
+++ tests/testthat/test-reader.R
@@ -0,0 +1,42 @@
+
+
+# Reader Unit Tests -------------------------------------------------------
+
+
+library(testthat)
+library(a2munge)
+library(sparklyr)
+library(dplyr)
+library(checkmate)
+
+context("reader function unit tests")
+
+
+# Create Spark Connection and read in some data
+sc <- spark_connect(master = "local")
+
+# Load data into Spark
+mtcars_tbl <- copy_to(sc, mtcars, overwrite = TRUE)
+
+# Temp Directory
+tmp <- paste0(tempdir(), "/reader-tests")
+
+test_that("parquet file type", {
+  writer(mtcars_tbl, path = paste0(tmp, "/mtcars.parquet"))
+  assert_class(reader(sc, "test", path = paste0(tmp, "/mtcars.parquet")), "tbl_spark")
+  file.remove(paste0(tmp, "/mtcars.parquet"))
+})
+
+
+test_that("csv file type", {
+  writer(mtcars_tbl, path = paste0(tmp, "/mtcars.csv"))
+  assert_class(reader(sc, "test", path = paste0(tmp, "/mtcars.csv")), "tbl_spark")
+  file.remove(paste0(tmp, "/mtcars.csv"))
+})
+
+
+test_that("json file type", {
+  writer(mtcars_tbl, path = paste0(tmp, "/mtcars.json"))
+  assert_class(reader(sc, "test", path = paste0(tmp, "/mtcars.json")), "tbl_spark")
+  file.remove(paste0(tmp, "/mtcars.json"))
+})
