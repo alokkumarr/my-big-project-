cc8d82f5b25b989f4bb830f6044484bf89f6924e
Chris Haarstick <Chris.Haarstick@synchronoss.com>
2018-05-09 13:30:11 -0400

    Updated reader to allow for directory path
    updatd documentation and added unit tests

--- tests/testthat/test-reader.R
+++ tests/testthat/test-reader.R
@@ -40,3 +40,18 @@ test_that("json file type", {
   assert_class(reader(sc, "test", path = paste0(tmp, "/mtcars.json")), "tbl_spark")
   file.remove(paste0(tmp, "/mtcars.json"))
 })
+
+
+
+test_that("directory path", {
+  writer(mtcars_tbl, path = paste0(tmp, "/mtcars.json"))
+  read_tbl <- reader(sc, "test", path = tmp)
+  assert_class(read_tbl, "tbl_spark")
+  expect_equal(read_tbl %>%
+                 collect() %>%
+                 arrange(mpg),
+               mtcars_tbl %>%
+                 collect() %>%
+                 arrange(mpg))
+  file.remove(paste0(tmp, "/mtcars.json"))
+})
