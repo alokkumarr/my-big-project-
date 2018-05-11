14ac375fa68bc83d0fc03bb53e7d19bddfde4791
Anirudh Venkatesh <Anirudh.NV@synchronoss.com>
2018-05-11 16:51:48 +0530

    fixing unit tests

--- saw-web/src/test/javascript/pages/homePage.po.js
+++ saw-web/src/test/javascript/pages/homePage.po.js
@@ -5,7 +5,7 @@ const analyzePage = require('../../javascript/pages/analyzePage.po');
 
 module.exports = {
   mainMenuExpandBtn: element(by.css('mat-icon[e2e="main-menu-expand-btn"]')),
-  mainMenuCollapseBtn: element(by.css('button[e2e="main-menu-collapse-btn"]')),
+  mainMenuCollapseBtn: element(by.css('mat-icon[e2e="main-menu-expand-btn"]')),
   accountSettingsMenuBtn: element(by.css('mat-icon[e2e="account-settings-menu-btn"]')),
   adminMenuOption: element(by.css('a[e2e="account-settings-selector-admin"]')),
   changePasswordMenuOption: element(by.css('button[e2e="account-settings-selector-change-password"]')),
