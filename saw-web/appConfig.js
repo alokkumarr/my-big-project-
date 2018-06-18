d0d490ab10e659c2aa517d999eb094c744b716b3
Markus Holmberg <markus.holmberg@synchronoss.com>
2018-06-18 13:29:47 -0400

    SIP-3555: Update apiUrl to use address of new environment
    The shared development enviroment is moving to a new environment,
    because the current (Velocity Lab) is being decommissioned.

--- saw-web/appConfig.js
+++ saw-web/appConfig.js
@@ -7,7 +7,7 @@ if (__PRODUCTION__) {
 } else {
   // Note: To run against a local Docker environment, use localhost
   // apiUrl = 'http://localhost';
-  apiUrl = 'https://sawdev-bda-velocity-vacum-np.sncrcorp.net';
+  apiUrl = 'https://saw-bda-cert-vaste.sncrcorp.net';
 }
 /* eslint-enable */
 apiUrl = apiUrl + '/saw';
