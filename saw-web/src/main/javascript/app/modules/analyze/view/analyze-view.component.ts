c34089da4f6458fe9219baadb68078602b51a343
Barna Mumtyan <Barna.Mumtya@synchronoss.com>
2018-06-27 15:15:38 +0300

    SIP-3737: Executing message now shows up when publishing an analysis.

--- saw-web/src/main/javascript/app/modules/analyze/view/analyze-view.component.ts
+++ saw-web/src/main/javascript/app/modules/analyze/view/analyze-view.component.ts
@@ -118,7 +118,7 @@ export class AnalyzeViewComponent implements OnInit {
     /* Update the new analysis in the current list */
     const index = findIndex(this.analyses, ({id}) => id === analysis.id);
     this.analyses.splice(index, 1, analysis);
-    this._state.go('analyze.view', {id: analysis.categoryId});
+    this.goToAnalysis(analysis)
   }
 
   spliceAnalyses(analysis, replace) {
