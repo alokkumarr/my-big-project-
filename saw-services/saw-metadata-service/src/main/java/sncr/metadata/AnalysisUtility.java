3d026fd5acebb80c8d7b3cf25facfe37b51055b3
NITHIN <nithinmalathesh.bg@synchronoss.com>
2018-07-05 16:00:14 +0530

    SIP-3823: SOP replaced with logger

--- saw-services/saw-metadata-service/src/main/java/sncr/metadata/AnalysisUtility.java
+++ saw-services/saw-metadata-service/src/main/java/sncr/metadata/AnalysisUtility.java
@@ -2,6 +2,7 @@ package sncr.metadata;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.mapr.org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.TableName;
 import org.slf4j.Logger;
@@ -10,10 +11,12 @@ import sncr.metadata.engine.MetadataStore;
 import sncr.metadata.engine.tables;
 import sncr.saw.common.config.SAWServiceConfig;
 
+import java.util.Map;
+
 public class AnalysisUtility extends MetadataStore{
     protected static final Logger logger = LoggerFactory.getLogger(AnalysisUtility.class.getName());
     public static void main(String[] args) throws Exception {
-        System.out.println("Inside AnalysisUtility Class");
+        logger.info("Inside AnalysisUtility Class");
         String table = SAWServiceConfig.metadataConfig().getString("path") + "/" + tables.AnalysisMetadata().toString();
         //"/main/metadata/analysis_metadata";
         TableName tn = TableName.valueOf(table);
@@ -22,18 +25,28 @@ public class AnalysisUtility extends MetadataStore{
         ResultScanner rs = tab.getScanner(scan);
         JsonParser parser = new JsonParser();
         for (Result result : rs) {
-            JsonObject analysis = parser
-                .parse(new String(result.getValue("_source".getBytes(), "content".getBytes())))
-                .getAsJsonObject();
-            if (analysis.has("semanticId")) {
-                Put put = new Put(analysis.get("id").getAsString().getBytes());
-                put.addColumn("_search".getBytes(), "semanticId".getBytes(), analysis.get("semanticId").getAsString().getBytes());
-                tab.put(put);
-                System.out.println(" Analysis Id = " +analysis.get("id").getAsString()+
-                    " and semanticId = "+analysis.get("semanticId").getAsString());
+            int execFlag = 1;
+
+            Map<byte[], byte[]> familyMap = result.getFamilyMap(Bytes.toBytes("_search"));
+            for(Map.Entry<byte[], byte[]> entry : familyMap.entrySet()){
+                if (Bytes.toString(entry.getKey()).equals("semanticId")) {
+                    execFlag = 0;
+                }
+            }
+            if (execFlag == 1) {
+                JsonObject analysis = parser
+                    .parse(new String(result.getValue("_source".getBytes(), "content".getBytes())))
+                    .getAsJsonObject();
+                if (analysis.has("semanticId")) {
+                    Put put = new Put(analysis.get("id").getAsString().getBytes());
+                    put.addColumn("_search".getBytes(), "semanticId".getBytes(), analysis.get("semanticId").getAsString().getBytes());
+                    tab.put(put);
+                    logger.info(" Analysis Id = " +analysis.get("id").getAsString()+
+                        " and semanticId = "+analysis.get("semanticId").getAsString());
+                }
             }
         }
         rs.close();
-        System.out.println("End of AnalysisUtility Class");
+        logger.info("End of AnalysisUtility Class");
     }
 }
