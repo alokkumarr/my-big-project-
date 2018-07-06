606aa0f7fdba2fa46a2f461bfb89ae3a531327d7
Pradeep Tripathi <pradeep.manitripathi@synchronoss.com>
2018-07-06 15:31:05 +0530

    SIP-3647: Added configurable parameter to limit number of rows for publish and on-demand execution. Publish row limit is optional parameter and only get considered if it has been set greater than 0 values, if it not set then no limit will be applied for publishing the report.

--- saw-services/saw-data-access/src/main/scala/sncr/datalake/DLConfiguration.scala
+++ saw-services/saw-data-access/src/main/scala/sncr/datalake/DLConfiguration.scala
@@ -56,7 +56,8 @@ object DLConfiguration {
     * Row limit is configured/hardcoded value
     */
 
-  val rowLimit = if (cfg.hasPath("sql-executor.inline-data-store-limit-rows")) cfg.getInt("sql-executor.inline-data-store-limit-rows") else 100
+  val rowLimit = if (cfg.hasPath("sql-executor.preview-rows-limit")) cfg.getInt("sql-executor.preview-rows-limit") else 100
+  val publishRowLimit = if (cfg.hasPath("sql-executor.publish-rows-limit")) cfg.getInt("sql-executor.publish-rows-limit") else -1
   val jarLocation = cfg.getString ("sql-executor.jar-location")
   val commonLocation = cfg.getString ("sql-executor.output-location")
   val semanticLayerTempLocation = cfg.getString ("sql-executor.semantic-layer-tmp-location")
