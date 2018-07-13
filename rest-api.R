87fe0ecdf68aba84b8a35d785d49c347c51c6f34
Chris Haarstick <Chris.Haarstick@synchronoss.com>
2018-07-13 15:25:06 -0400

    Adding R package dependency JSON file
    Added R package install function
    
    Adding get_datasets function

--- rest-api.R
+++ rest-api.R
@@ -1,5 +1,5 @@
 
-#' Metastore Authenticate Function
+#' SIP Metastore Authenticate Function
 #'
 #' Generates authentication token for metastore communication
 #'
@@ -11,10 +11,10 @@
 #'
 #' @return aToken string
 #' @export
-metastore_authenticate <- function(login,
-                                   password,
-                                   url = "https://saw-bda-cert-vaste.sncrcorp.net/saw/security/doAuthenticate",
-                                   headers = list('Content-Type' = "application/json;charset=UTF-8")){
+sip_authenticate <- function(login,
+                             password,
+                             url = "https://saw-bda-cert-vaste.sncrcorp.net/saw/security/doAuthenticate",
+                             headers = list('Content-Type' = "application/json;charset=UTF-8")){
   checkmate::assert_character(login)
   checkmate::assert_character(password)
   checkmate::assert_character(url)
@@ -33,9 +33,42 @@ metastore_authenticate <- function(login,
   # Body
   body_str <- paste0("{\"masterLoginId\": \"", login, "\", \"password\": \"", password, "\"}")
 
-  # Post reqest
+  # Post request
   response <- httr::POST(url = url, added_headers, body = body_str)
 
   # Return Token
-  content(response)$aToken
+  httr::content(response)$aToken
+}
+
+
+#' SIP Metastore Get Datasets Function
+#'
+#' Returns all datasets in Workbench Metastore
+#'
+#' @param token
+#' @param url
+#' @param headers
+#'
+#' @return
+#' @export
+#'
+#' @examples
+sip_get_datasets <- function(token,
+                             url = "https://saw-bda-cert-vaste.sncrcorp.net/saw/services/internal/workbench/projects/workbench/datasets",
+                             headers = list('Accept' = "application/json, text/plain, */*")){
+  checkmate::assert_character(url)
+  checkmate::assert_list(headers, null.ok = TRUE)
+  if(! is.null(headers)){
+    purrr::map(headers, checkmate::assert_character)
+  }
+
+  # Add Headers
+  headers <- c(headers, list("Authorization" = paste("Bearer", token)))
+  .fun <- get("add_headers", asNamespace("httr"))
+  added_headers <- do.call(".fun", headers)
+
+  # GET request
+  response <- httr::GET(url, added_headers)
+
+  httr::content(response)
 }
