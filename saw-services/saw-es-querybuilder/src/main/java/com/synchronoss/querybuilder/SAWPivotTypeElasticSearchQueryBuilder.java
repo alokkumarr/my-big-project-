daf6e04e4693693543355563633f8cb817c46170
Pradeep Tripathi <pradeep.manitripathi@synchronoss.com>
2018-07-18 13:56:13 +0530

    SIP-3781: Added the global filter type for ES-report to make ES-report global-filter eligible.

--- saw-services/saw-es-querybuilder/src/main/java/com/synchronoss/querybuilder/SAWPivotTypeElasticSearchQueryBuilder.java
+++ saw-services/saw-es-querybuilder/src/main/java/com/synchronoss/querybuilder/SAWPivotTypeElasticSearchQueryBuilder.java
@@ -114,7 +114,8 @@ public String getJsonString() {
 
     for (com.synchronoss.querybuilder.model.pivot.Filter item : filters) 
     {
-      if (!item.getIsRuntimeFilter().value()){
+      if (!item.getIsRuntimeFilter().value() && item.getIsGloblFilter()!=null
+          && !item.getIsGloblFilter().value()){
         if (item.getType().value().equals(Type.DATE.value()) || item.getType().value().equals(Type.TIMESTAMP.value())) {
           if (item.getModel().getPreset()!=null && !item.getModel().getPreset().value().equals(Model.Preset.NA.toString()))
           {
