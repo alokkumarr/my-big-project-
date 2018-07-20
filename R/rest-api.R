a3666eeed8fb1a8760d1e1a019c3a7ecbaac0e2c
Chris Haarstick <Chris.Haarstick@synchronoss.com>
2018-07-20 15:05:40 -0400

    Adding functions for get dataset details
    Updated authentication and get datasets functions - change the parameters to just hostname and removed the headers

--- R/rest-api.R
+++ R/rest-api.R
@@ -5,35 +5,30 @@
 #'
 #' @param login master login character
 #' @param password password character
-#' @param url url to post request to SAW security
-#' @param headers list of header arguments to add to request
+#' @param hostname hostname only of saw security location. No pathing required.
+#'   Ex: hostname = "https://saw-bda-cert-vaste.sncrcorp.net"
 #'
-#' @return aToken string
+#' @return aToken authentication string
 #' @export
 sip_authenticate <- function(login,
                              password,
-                             url = "https://saw-bda-cert-vaste.sncrcorp.net/saw/security/doAuthenticate",
-                             headers = list('Content-Type' = "application/json;charset=UTF-8")){
+                             hostname
+                             ){
   checkmate::assert_character(login)
   checkmate::assert_character(password)
-  checkmate::assert_character(url)
-  checkmate::assert_list(headers, null.ok = TRUE)
-
+  checkmate::assert_character(hostname)
 
   # Headers
-  if(! is.null(headers)){
-    purrr::map(headers, checkmate::assert_character)
-    .fun <- get("add_headers", asNamespace("httr"))
-    added_headers <- do.call(".fun", headers)
-  }else{
-    added_headers <- NULL
-  }
+  headers <- httr::add_headers('Content-Type' = "application/json;charset=UTF-8")
 
   # Body
   body_str <- paste0("{\"masterLoginId\": \"", login, "\", \"password\": \"", password, "\"}")
 
+  # URL
+  url <- paste0(hostname, "/saw/security/doAuthenticate")
+
   # Post request
-  response <- httr::POST(url = url, added_headers, body = body_str)
+  response <- httr::POST(url, config = headers, body = body_str)
 
   # Return Token
   httr::content(response)$aToken
@@ -42,30 +37,67 @@ sip_authenticate <- function(login,
 
 #' SIP Metastore Get Datasets Function
 #'
-#' Returns all datasets in Workbench Metastore
+#' Returns all datasets in Workbench Metastore for a given project
 #'
+#' @param project_id id of project to retrieve datasets from
+#' @inheritParams sip_authenticate
 #' @param token valid SIP token. Result of sip_authenticate function
-#' @param url url path to get request from saw services
-#' @param headers list of header arguments to add to request
 #'
-#' @return list of datasets paths
+#' @return list with datasets metadata
 #' @export
-sip_get_datasets <- function(token,
-                             url = "https://saw-bda-cert-vaste.sncrcorp.net/saw/services/internal/workbench/projects/workbench/datasets",
-                             headers = list('Accept' = "application/json, text/plain, */*")){
-  checkmate::assert_character(url)
-  checkmate::assert_list(headers, null.ok = TRUE)
-  if(! is.null(headers)){
-    purrr::map(headers, checkmate::assert_character)
-  }
+sip_get_datasets <- function(project_id,
+                             hostname,
+                             token
+                             ){
+  checkmate::assert_character(project_id)
+  checkmate::assert_character(hostname)
+  checkmate::assert_character(token)
 
   # Add Headers
-  headers <- c(headers, list("Authorization" = paste("Bearer", token)))
-  .fun <- get("add_headers", asNamespace("httr"))
-  added_headers <- do.call(".fun", headers)
+  headers <- httr::add_headers('Authorization' = paste("Bearer", token),
+                               'Accept' = "application/json, text/plain, */*")
+
+  # URL
+  url <- paste0(hostname, "/saw/services/internal/workbench/projects/", project_id, "/datasets")
 
   # GET request
-  response <- httr::GET(url, added_headers)
+  response <- httr::GET(url, config = headers)
+
+  httr::content(response)
+}
+
+
+
+#' SIP Metastore Get Dataset Details
+#'
+#' Returns dataset metadata details
+#'
+#' @param dataset_name dataset name
+#' @inheritParams sip_get_datasets
+#'
+#' @return list with dataset details
+#' @export
+sip_get_dataset_details <- function(dataset_name,
+                                    project_id,
+                                    hostname,
+                                    token
+                                    ){
+  checkmate::assert_character(dataset_name)
+  checkmate::assert_character(project_id)
+  checkmate::assert_character(hostname)
+  checkmate::assert_character(token)
+
+  # Add Headers
+  headers <- httr::add_headers('Authorization' = paste("Bearer", token),
+                               'Accept' = "application/json, text/plain, */*")
+
+  # URL
+  url <- paste0(hostname,
+                "/saw/services/internal/workbench/projects/", project_id,
+                "/datasets/", paste(project_id, dataset_name, sep = "::"))
+
+  # GET request
+  response <- httr::GET(url, config = headers)
 
   httr::content(response)
 }
