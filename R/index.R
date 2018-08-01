64c472bd90d2760074f473e05918105430865414
Akshatha G <aksg0001@Synchronoss.com>
2018-08-01 13:02:45 +0100

    updated index.R with comments and test suit to add both numeric and integer test cases

--- R/index.R
+++ R/index.R
@@ -1,4 +1,5 @@
 
+
 #' Index Class Constructer
 #'
 #' @param unit index unit. can by time base units such as days, weeks or years
@@ -27,84 +28,65 @@ new_index <- function(unit, periods, start, end) {
 #' Used by Forecaster to create forward looking indicies
 #'
 #' @export
-index <- function(x, unit,periods) {
+index <- function(periods, x, unit) {
   UseMethod("index")
 }
 
+structure_fun <- function(periods, x, unit, class)
+{
+  structure(new_index(
+    unit = unit,
+    periods = periods,
+    start = min(x),
+    end = max(x)
+  ),
+  class = class)
+}
 
 #' @rdname index
 #' @export
-index.numeric <- index.integer <- function(x, unit = NULL) {
+index.numeric <- function(x, unit = NULL) {
   unit <- mean(diff(x))
   if ((unit %% 1) != 0) {
     stop("index not regular")
   }
-  structure(
-    new_index(
-      unit = unit,
-      periods = NULL,
-      start = min(x),
-      end = max(x)
-    ),
-    class = c("numeric_index", "index")
-  )
-}
 
+  structure_fun(unit=unit,periods=NULL,x=x,class=c("numeric_index", "index"))
+}
 
 #' @rdname index
 #' @export
-index.Date <- function(x, unit="days") {
-  checkmate::assert_choice(unit, c("days","weeks","years"))
+
+index.integer <- index.numeric
+
+#' @rdname index
+#' @export
+index.Date <- function(x, unit = "days") {
+  checkmate::assert_choice(unit, c("days", "weeks", "years"))
   #checkmate::assert_numeric(periods, lower = 1, null.ok = TRUE)
   periods <- abs(as.numeric(mean(diff(x))))
-  if (unit=="days")
+  if (unit == "days")
   {
-  if ((periods %% 1) != 0) {
-    stop("index not regular")
-  }
-  structure(
-    new_index(
-      unit = unit,
-      periods = periods,
-      start = min(x),
-      end = max(x)
-    )
-    ,
-    class = c("time_index", "index")
-  )
-  }
-  else if(unit=="weeks")
-  {if ((periods %% 1) != 0) {
-    stop("index not regular")
-  }
-    periods <- round(periods / 7)
-    structure(
-      new_index(
-        unit = unit,
-        periods = periods,
-        start = min(x),
-        end = max(x)
-      )
-      ,
-      class = c("time_index", "index")
-    )
-  }
-  else if(unit=="years")
-    if ((periods %% 1 >= 0 && periods %% 1 <= 1)) {
+    if ((periods %% 1) != 0) {
+      stop("index not regular")
+    }
 
+    structure_fun(unit=unit,periods=periods,x=x,class=c("time_index", "index"))
+  }
+  else if (unit == "weeks")
+  {
+    if ((periods %% 1) != 0) {
+      stop("index not regular")
+    }
+    periods <- round(periods / 7)
+
+    structure_fun(unit=unit,periods=periods,x=x,class=c("time_index", "index"))
+  }
+  else if (unit == "years")
+    if ((periods %% 1 >= 0 && periods %% 1 <= 1)) {
       periods <- round(periods / 365)
 
-      structure(
-        new_index(
-          unit = unit,
-          periods = periods,
-          start = min(x),
-          end = max(x)
-        )
-        ,
-        class = c("time_index", "index")
-      )
-
+    structure_fun(unit=unit,periods=periods,x=x,class=c("time_index", "index"))
     }
   else
   {
@@ -116,23 +98,14 @@ index.Date <- function(x, unit="days") {
 
 #' @rdname index
 #' @export
-index.POSIXct <- function(x, unit = "hours",periods) {
+index.POSIXct <- function(x, unit = "hours") {
   checkmate::assert_choice(unit, c("seconds", "minutes", "hours", "days"))
-  checkmate::assert_numeric(periods, lower = 1, null.ok = TRUE)
+  #checkmate::assert_numeric(lower = 1, null.ok = TRUE)
   periods <- abs(as.numeric(mean(diff(x))))
   if ((periods %% 1) != 0) {
     stop("index not regular")
   }
-  structure(
-    new_index(
-      unit = unit,
-      periods = periods,
-      start = min(x),
-      end = max(x)
-    )
-    ,
-    class = c("time_index", "index")
-  )
+  structure_fun(unit=unit,periods=periods,x=x,class=c("time_index", "index"))
 }
 
 #' @rdname index
@@ -155,9 +128,11 @@ extend <- function(obj, length_out) {
 #' @rdname extend
 #' @export
 extend.index <- function(obj, length_out) {
-  seq(from = obj$end + obj$unit,
-      length.out = length_out,
-      by = obj$unit)
+  seq(
+    from = obj$end + obj$unit,
+    length.out = length_out,
+    by = obj$unit
+  )
 }
 
 
--- tests/testthat/test-index.R
+++ tests/testthat/test-index.R
@@ -23,34 +23,43 @@ sc <- spark_connect(master = "local", spark_home = spark_home_dir)
 
 n <- 20
 
-# Test1:Numeric value indexing ------------------------------------------
+# Test1:Numeric and integer value indexing ------------------------------------------
 
 seq_q <- seq(1:n)
 
-ind_num <- index(seq_q, unit = NULL)
+seq_num <- as.numeric(seq_q)
 
+ind_int <- index(seq_q, unit = NULL)
+ind_num <- index(seq_num, unit = NULL)
+
+ind_int_ext <- extend(ind_int, 5)
 ind_num_ext <- extend(ind_num, 5)
 
 s_q_ext <- seq(21, 25, 1)
 
 test_that("Index for Numeric class", {
-  expect_class(ind_num, "index")
+  expect_class(ind_int, "index")
+  expect_equal(ind_int_ext, s_q_ext)
   expect_equal(ind_num_ext, s_q_ext)
 })
 
 # Test2:Numeric value indexing where unit is not NULL ------------------------------------------
 
-seq_q_unit <- seq(1,20,3)
+seq_q_unit <- seq(1, 20, 3)
+seq_num <- as.numeric(seq_q_unit)
 
 ind_num_unit <- index(seq_q_unit, unit = NULL)
+ind_numeric_unit <- index(seq_num, unit = NULL)
 
 ind_num_ext <- extend(ind_num_unit, 5)
+ind_numeric_ext <- extend(ind_numeric_unit, 5)
 
 seq_q_unit_ext <- seq(22, 35, 3)
 
 test_that("Index for Numeric class", {
   expect_class(ind_num, "index")
   expect_equal(ind_num_ext, seq_q_unit_ext)
+  expect_equal(ind_numeric_ext, seq_q_unit_ext)
 })
 
 
@@ -76,7 +85,7 @@ test_that("Index for Date class", {
 #Test4:Index value for sequence of Weeks ------------------------------------------
 
 dat_time_week <-
-  Sys.Date() + lubridate::weeks(seq(
+  as.Date("2018-07-31") + lubridate::weeks(seq(
     from = 1,
     length.out = 3,
     by = 1
@@ -90,14 +99,14 @@ List_week <-
 
 test_that("Index for Date-Week class", {
   expect_class(date_ind_week, "time_index")
-  expect_equal(date_ind_week_ext,List_week)
+  expect_equal(date_ind_week_ext, List_week)
 
 })
 
 #Test5:Index value for sequence of Years ------------------------------------------
 
 dat_year <-
-  Sys.Date()+365 + lubridate::years(seq(
+  as.Date("2018-07-31") + 365 + lubridate::years(seq(
     from = 1,
     length.out = 5,
     by = 4
@@ -112,7 +121,7 @@ List_year <-
 
 test_that("Test the index for years", {
   expect_class(date_ind, "time_index")
-  expect_equal(date_ind_ext,List_year)
+  expect_equal(date_ind_ext, List_year)
 })
 
 
@@ -124,11 +133,14 @@ day_hours <-
     by = 2
   ))
 
-date_ind <- index(day_hours, unit = "hours",periods=2)
+date_ind <- index(x = day_hours, unit = "hours")
 
-seq_etx <- seq(as_datetime(date_ind$end)+2*60*60, as_datetime(date_ind$end)+6*60*60, by = 2*60*60)
+seq_etx <-
+  seq(as_datetime(date_ind$end) + 2 * 60 * 60,
+      as_datetime(date_ind$end) + 6 * 60 * 60,
+      by = 2 * 60 * 60)
 
-date_in_ext <- extend(date_ind,3)
+date_in_ext <- extend(date_ind, 3)
 
 
 test_that("Index for Posixt class", {
@@ -136,7 +148,7 @@ test_that("Index for Posixt class", {
   expect_equal(date_in_ext, seq_etx)
 })
 
-#Test6:Create Index for POSIXt-Minutes class ------------------------------------------
+#Test7:Create Index for POSIXt-Minutes class ------------------------------------------
 day_minutes <-
   Sys.Date() + lubridate::minutes(seq(
     from = 1,
@@ -144,18 +156,21 @@ day_minutes <-
     by = 2
   ))
 
-date_min_ind <- index(day_minutes, unit = "minutes",periods=2)
+date_min_ind <- index(day_minutes, unit = "minutes")
 
-Min_in_ext <- extend(date_min_ind,3)
+Min_in_ext <- extend(date_min_ind, 3)
 
-seq_min_etx <- seq(as_datetime(date_min_ind$end)+2*60, as_datetime(date_min_ind$end)+6*60, by = 2*60)
+seq_min_etx <-
+  seq(as_datetime(date_min_ind$end) + 2 * 60,
+      as_datetime(date_min_ind$end) + 6 * 60,
+      by = 2 * 60)
 
 test_that("Index for Posixt class", {
   expect_class(date_ind, "index")
   expect_equal(Min_in_ext, seq_min_etx)
 })
 
-#Test7:Create Index for POSIXt-secords class ------------------------------------------
+#Test8:Create Index for POSIXt-secords class ------------------------------------------
 day_seconds <-
   Sys.Date() + lubridate::seconds(seq(
     from = 1,
@@ -163,15 +178,16 @@ day_seconds <-
     by = 3
   ))
 
-date_sec_ind <- index(day_seconds, unit = "seconds",periods=3)
+date_sec_ind <- index(day_seconds, unit = "seconds")
 
-Sec_in_ext <- extend(date_sec_ind,3)
+Sec_in_ext <- extend(date_sec_ind, 3)
 
-seq_sec_etx <- seq(as_datetime(date_sec_ind$end)+3, as_datetime(date_sec_ind$end)+9, by = 3)
+seq_sec_etx <-
+  seq(as_datetime(date_sec_ind$end) + 3,
+      as_datetime(date_sec_ind$end) + 9,
+      by = 3)
 
 test_that("Index for Posixt class", {
   expect_class(date_sec_ind, "index")
   expect_equal(Sec_in_ext, seq_sec_etx)
 })
-
-
