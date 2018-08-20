710302b8c3402aca5608ecd74d1138bb9416ecab
Akshatha G <akshatha.g@synchronoss.com>
2018-08-20 19:57:52 +0530

    SIP-3934: Rest API unit test cases

---
+++ a2sipr/tests/RestApi.R
@@ -0,0 +1,104 @@
+# A2sipr Unit Tests -----------------------------------------------------
+
+library(a2sipr)
+library(testthat)
+library(sparklyr)
+library(dplyr)
+library(checkmate)
+
+context("A2Sipr rest-api function unit tests")
+
+
+jwt_token <-
+  sip_authenticate(
+    "sawadmin@synchronoss.com",
+    "Sawsyncnewuser1!",
+    "http://saw-rd601.ana.dev.vaste.sncrcorp.net"
+  )
+
+
+output_schema_list <- list(
+  list(name = "ID", type = "long"),
+  list(name = "KEY", type = "string"),
+  list(name = "SECRETS", type = "string")
+)
+
+
+# Test 1: RestAPI-Authontication function test ----------------------------
+
+add_ds <- sip_add_dataset(
+  output_name = "DataSet_1",
+  output_format = "parquet",
+  output_schema = output_schema_list,
+  catalog = "data",
+  output_rows = 10,
+  component = "RComponent",
+  script = "/dfs/data/bda/xda-test/Test.R",
+  desc = "Test R new code",
+  created_by = "Aks",
+  project_id = "aa-sb-poc",
+  batch_id = "B252356",
+  input_paths = "/dfs/data/bda/xda-test",
+  input_formats = "csv",
+  input_ids = "123",
+  started = "20180801-113751",
+  finished = "20180801-113807",
+  status = "SUCCESS",
+  hostname = "http://saw-rd601.ana.dev.vaste.sncrcorp.net",
+  token = jwt_token
+)
+
+output_ds <- add_ds$system$name
+output_format <-  add_ds$system$outputFormat
+
+test_that("Physical location matches with expected one", {
+  expect_equal(output_ds, "DataSet_1")
+  expect_equal(output_format, "parquet")
+})
+
+# Test2: Get all the Dataset list from MaprDB -----------------------------
+
+ds_get <- sip_get_datasets(project_id = "aa-sb-poc",
+                           hostname = "http://saw-rd601.ana.dev.vaste.sncrcorp.net",
+                           token = jwt_token)
+
+# Test 3:Rest API for Get Data set details --------------------------------
+
+get_ds_detail <- sip_get_dataset_details(
+  dataset_id = add_ds$`_id`,
+  project_id = projectid,
+  hostname = "http://saw-rd601.ana.dev.vaste.sncrcorp.net",
+  token = jwt_token
+)
+
+output_ds <- add_ds$system$name
+output_format <-  add_ds$system$outputFormat
+output_phys_loc <- add_ds$system$physicalLocation
+output_proj_id <- add_ds$system$project
+get_ds <- get_ds_detail$system$name
+get_ds_output_format <- get_ds_detail$system$outputFormat
+get_ds_physical_loc <- get_ds_detail$system$physicalLocation
+get_ds_project_id <- get_ds_detail$system$project
+
+test_that("Newly added DS content matches with get DS details", {
+  expect_equal(output_ds, get_ds)
+  expect_equal(output_format, get_ds_output_format)
+  expect_equal(output_phys_loc, get_ds_physical_loc)
+  expect_equal(output_proj_id, get_ds_project_id)
+})
+
+
+# Test 4:Rest API to get the Data path from Mapr DB -----------------------
+
+get_dpath <- sip_get_datapath(
+  name = "DataSet_1",
+  project_id = projectid,
+  hostname = "http://saw-rd601.ana.dev.vaste.sncrcorp.net",
+  token = jwt_token
+)
+
+output_phys_loc <- add_ds$system$physicalLocation
+
+test_that("Test if the Data path matches Physical location", {
+  expect_equal(get_dpath, output_phys_loc)
+})
