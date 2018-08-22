055701310872aeedc642d6cdd31a39cb3dc753ef
Akhilesh Obilineni <Akhilesh.Obilineni@synchronoss.com>
2018-08-22 14:50:13 -0400

    SIP-4293: cleaned up unit test cases a little.

--- saw-web/src/main/javascript/app/modules/workbench/services/workbench.service.spec.ts
+++ saw-web/src/main/javascript/app/modules/workbench/services/workbench.service.spec.ts
@@ -29,6 +29,7 @@ describe('Workbench Service', () => {
   let api = fpGet('api.url', APP_CONFIG);
   let wbAPI = `${this.api}/internal/workbench/projects`;
   let userProject = 'workbench';
+  let workbenchService: WorkbenchService;
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
@@ -39,6 +40,7 @@ describe('Workbench Service', () => {
       ]
     }).compileComponents();
     httpMock = TestBed.get(HttpClientTestingModule);
+    workbenchService = TestBed.get(WorkbenchService);
   });
 
   it('should return error if dataset request failed', inject(
@@ -50,109 +52,101 @@ describe('Workbench Service', () => {
     }
   ));
 
-  it('should get the datasets', inject(
-    [WorkbenchService],
-    (WorkbenchService: WorkbenchService) => {
-      const mockResponse = {
-        data: [
-          {
-            id: 0,
-            name: 'DS 1',
-            location: 'location 1',
-            creator: 'creator 1',
-            description: 'Desc 1',
-            size: '10000',
-            last_updated_time: 'updated_time 1',
-            updated_by: 'user 1',
-            status: 'status 1',
-            data_set_type: 'type 1',
-            source_type: 'src 1',
-            file_count: 'count 1',
-            creation_time: 'time 1'
-          },
-          {
-            id: 0,
-            name: 'DS 2',
-            location: 'location 2',
-            creator: 'creator 2',
-            description: 'Desc 2',
-            size: '10000',
-            last_updated_time: 'updated_time 2',
-            updated_by: 'user 2',
-            status: 'status 2',
-            data_set_type: 'type 2',
-            source_type: 'src 2',
-            file_count: 'count 2',
-            creation_time: 'time 2'
-          }
-        ]
-      };
-      WorkbenchService.getDatasets().subscribe((res: any) => {
-        expect(res.length).to.be('2');
-        expect(res).to.equal(mockResponse);
-      });
-      // const request = httpMock.expectOne(`${this.wbAPI}/${this.userProject}/datasets`);
-      // expect(request.request.method).to.be('GET');
-      // request.flush();
-    }
-  ));
+  it('should get the datasets', () => {
+    const mockResponse = {
+      data: [
+        {
+          id: 0,
+          name: 'DS 1',
+          location: 'location 1',
+          creator: 'creator 1',
+          description: 'Desc 1',
+          size: '10000',
+          last_updated_time: 'updated_time 1',
+          updated_by: 'user 1',
+          status: 'status 1',
+          data_set_type: 'type 1',
+          source_type: 'src 1',
+          file_count: 'count 1',
+          creation_time: 'time 1'
+        },
+        {
+          id: 0,
+          name: 'DS 2',
+          location: 'location 2',
+          creator: 'creator 2',
+          description: 'Desc 2',
+          size: '10000',
+          last_updated_time: 'updated_time 2',
+          updated_by: 'user 2',
+          status: 'status 2',
+          data_set_type: 'type 2',
+          source_type: 'src 2',
+          file_count: 'count 2',
+          creation_time: 'time 2'
+        }
+      ]
+    };
+    workbenchService.getDatasets().subscribe((res: any) => {
+      expect(res.length).to.be('2');
+      expect(res).to.equal(mockResponse);
+    });
+    // const request = httpMock.expectOne(`${this.wbAPI}/${this.userProject}/datasets`);
+    // expect(request.request.method).to.be('GET');
+    // request.flush();
+  });
 
-  it('should return error if staging data request failed', inject(
-    [WorkbenchService],
-    (WorkbenchService: WorkbenchService) => {
-      WorkbenchService.getStagingData(
-        `${this.wbAPI}/${this.userProject}/raw/directory`
-      ).subscribe((res: any) => {
+  it('should return error if staging data request failed', () => {
+    workbenchService
+      .getStagingData(`${this.wbAPI}/${this.userProject}/raw/directory`)
+      .subscribe((res: any) => {
         expect(res.failure.error.type).to.be('ERROR_LOADING_STAGING_DATA');
       });
-      // httpMock.verify();
-    }
-  ));
+    // httpMock.verify();
+  });
 
-  it('should get the staging data', inject(
-    [WorkbenchService],
-    (WorkbenchService: WorkbenchService) => {
-      const mockStagingResponse = {
-        delimiter: ',',
-        description:
-          'It is delimited file inspecting to verify & understand the content of file',
-        fields: [
-          { name: 'DOJ', type: 'date', format: ['YYYY-MM-DD'] },
-          { name: 'FirstName', type: 'string' },
-          { name: 'LastName', type: 'string' },
-          { name: 'City', type: 'string' },
-          { name: 'State', type: 'string' },
-          { name: 'Country', type: 'string' }
-        ],
-        info: {
-          totalLines: 6,
-          dataRows: 5,
-          maxFields: 6,
-          minFields: 5,
-          file: '/old_file.csv'
+  it('should get the staging data', () => {
+    const mockStagingResponse = {
+      delimiter: ',',
+      description:
+        'It is delimited file inspecting to verify & understand the content of file',
+      fields: [
+        { name: 'DOJ', type: 'date', format: ['YYYY-MM-DD'] },
+        { name: 'FirstName', type: 'string' },
+        { name: 'LastName', type: 'string' },
+        { name: 'City', type: 'string' },
+        { name: 'State', type: 'string' },
+        { name: 'Country', type: 'string' }
+      ],
+      info: {
+        totalLines: 6,
+        dataRows: 5,
+        maxFields: 6,
+        minFields: 5,
+        file: '/old_file.csv'
+      },
+      samplesParsed: [
+        {
+          DOJ: '2018-01-27',
+          FirstName: 'Employee2',
+          LastName: 'Lastname2',
+          City: 'Reston',
+          State: 'Virginia',
+          Country: 'USA'
         },
-        samplesParsed: [
-          {
-            DOJ: '2018-01-27',
-            FirstName: 'Employee2',
-            LastName: 'Lastname2',
-            City: 'Reston',
-            State: 'Virginia',
-            Country: 'USA'
-          },
-          {
-            DOJ: '2018-01-28',
-            FirstName: 'Employee3',
-            LastName: 'Lastname3',
-            City: 'Reston',
-            State: 'Virginia',
-            Country: 'USA'
-          }
-        ]
-      };
-      WorkbenchService.getStagingData(
-        `${this.wbAPI}/${userProject}/raw/directory`
-      ).subscribe((res: any) => {
+        {
+          DOJ: '2018-01-28',
+          FirstName: 'Employee3',
+          LastName: 'Lastname3',
+          City: 'Reston',
+          State: 'Virginia',
+          Country: 'USA'
+        }
+      ]
+    };
+    workbenchService
+      .getStagingData(`${this.wbAPI}/${userProject}/raw/directory`)
+      .subscribe((res: any) => {
         expect(res.samplesParsed.length).to.be('2');
         expect(res.fields.length).to.equal(6);
         expect(res).to.equal(mockStagingResponse);
@@ -198,41 +192,35 @@ describe('Workbench Service', () => {
         expect(res.samplesParsed[0].State).to.be('Virginia');
         expect(res.samplesParsed[0].Country).to.be('USA');
       });
-      // const request = httpMock.expectOne();
-      // expect(request.request.method).to.be('GET');
-      // request.flush();
-    }
-  ));
+    // const request = httpMock.expectOne();
+    // expect(request.request.method).to.be('GET');
+    // request.flush();
+  });
 
-  it('should return error if raw preview data request failed', inject(
-    [WorkbenchService],
-    (WorkbenchService: WorkbenchService) => {
-      WorkbenchService.getRawPreviewData(
-        `${this.wbAPI}/${userProject}/raw/directory/preview`
-      ).subscribe((res: any) => {
+  it('should return error if raw preview data request failed', () => {
+    workbenchService
+      .getRawPreviewData(`${this.wbAPI}/${userProject}/raw/directory/preview`)
+      .subscribe((res: any) => {
         expect(res.failure.error.type).to.be('ERROR_LOADING_RAW_PREVIEW_DATA');
       });
-      // httpMock.verify();
-    }
-  ));
+    // httpMock.verify();
+  });
 
-  it('should get the rawPreview data', inject(
-    [WorkbenchService],
-    (WorkbenchService: WorkbenchService) => {
-      const mockRawPreviewData = {
-        projectId: 'workbench',
-        path: '/old_file.csv',
-        data: [
-          '2018-01-26, Employee1, LastName1, Reston, Virginia, USA',
-          '2018-01-27, Employee2, LastName2, Reston, Virginia, USA',
-          '2018-01-28, Employee3, LastName3, Reston, Virginia, USA',
-          '2018-01-29, Employee4, LastName4, Reston, Virginia, USA',
-          '2018-01-30, Employee5, LastName5, Reston, Virginia, USA'
-        ]
-      };
-      WorkbenchService.getRawPreviewData(
-        `${this.wbAPI}/${userProject}/raw/directory/preview`
-      ).subscribe((res: any) => {
+  it('should get the rawPreview data', () => {
+    const mockRawPreviewData = {
+      projectId: 'workbench',
+      path: '/old_file.csv',
+      data: [
+        '2018-01-26, Employee1, LastName1, Reston, Virginia, USA',
+        '2018-01-27, Employee2, LastName2, Reston, Virginia, USA',
+        '2018-01-28, Employee3, LastName3, Reston, Virginia, USA',
+        '2018-01-29, Employee4, LastName4, Reston, Virginia, USA',
+        '2018-01-30, Employee5, LastName5, Reston, Virginia, USA'
+      ]
+    };
+    workbenchService
+      .getRawPreviewData(`${this.wbAPI}/${userProject}/raw/directory/preview`)
+      .subscribe((res: any) => {
         expect(res.projectId).to.be('projectId');
         expect(res.path).to.be('/old_file.csv');
         expect(res.data[0].DOJ).to.be('2018-01-26');
@@ -243,118 +231,113 @@ describe('Workbench Service', () => {
         expect(res.data[0].Country).to.be('USA');
         expect(res).to.equal(mockRawPreviewData);
       });
-      // const request = httpMock.expectOne(`${workBenchService.wbAPI}`);
-      // expect(request.request.method).toBe('GET');
-      // request.flush();
-    }
-  ));
+    // const request = httpMock.expectOne(`${workBenchService.wbAPI}`);
+    // expect(request.request.method).toBe('GET');
+    // request.flush();
+  });
 
-  it('should get dataSet details', inject(
-    [WorkbenchService],
-    (WorkbenchService: WorkbenchService) => {
-      let id = 1;
-      const dateSetResponse = {
-        data: [
-          {
-            id: 0,
-            name: 'DS 1',
-            location: 'location 1',
-            creator: 'creator 1',
-            description: 'Desc 1',
-            size: '10000',
-            last_updated_time: 'updated_time 1',
-            updated_by: 'user 1',
-            status: 'status 1',
-            data_set_type: 'type 1',
-            source_type: 'src 1',
-            file_count: 'count 1',
-            creation_time: 'time 1'
-          }
-        ]
-      };
-      WorkbenchService.getDatasetDetails(id).subscribe((res: any) => {
-        expect(res[0].name).to.be('DS 1');
-        expect(res[0].location).to.be('location 1');
-        expect(res[0].creator).to.be('creator 1');
-        expect(res[0].description).to.be('Desc 1');
-        expect(res[0].size).to.be('10000');
-        expect(res[0].last_updated_time).to.be('updated_time 1');
-        expect(res[0].updated_by).to.be('user 1');
-        expect(res[0].status).to.be('status 1');
-        expect(res[0].data_set_type).to.be('type 1');
-        expect(res[0].source_type).to.be('src 1');
-        expect(res[0].file_count).to.be('count 1');
-        expect(res[0].creation_time).to.be('time 1');
-        expect(res).to.equal(dateSetResponse);
-      });
-      // const request = httpMock.expectOne(`${workBenchService.wbAPI}/${projectName}/${id}`);
-      // expect(request.request.method).toBe('GET');
-      // request.flush();
-    }
-  ));
+  it('should get dataSet details', () => {
+    let id = 1;
+    const dateSetResponse = {
+      data: [
+        {
+          id: 0,
+          name: 'DS 1',
+          location: 'location 1',
+          creator: 'creator 1',
+          description: 'Desc 1',
+          size: '10000',
+          last_updated_time: 'updated_time 1',
+          updated_by: 'user 1',
+          status: 'status 1',
+          data_set_type: 'type 1',
+          source_type: 'src 1',
+          file_count: 'count 1',
+          creation_time: 'time 1'
+        }
+      ]
+    };
+    workbenchService.getDatasetDetails(id).subscribe((res: any) => {
+      expect(res[0].name).to.be('DS 1');
+      expect(res[0].location).to.be('location 1');
+      expect(res[0].creator).to.be('creator 1');
+      expect(res[0].description).to.be('Desc 1');
+      expect(res[0].size).to.be('10000');
+      expect(res[0].last_updated_time).to.be('updated_time 1');
+      expect(res[0].updated_by).to.be('user 1');
+      expect(res[0].status).to.be('status 1');
+      expect(res[0].data_set_type).to.be('type 1');
+      expect(res[0].source_type).to.be('src 1');
+      expect(res[0].file_count).to.be('count 1');
+      expect(res[0].creation_time).to.be('time 1');
+      expect(res).to.equal(dateSetResponse);
+    });
+    // const request = httpMock.expectOne(`${workBenchService.wbAPI}/${projectName}/${id}`);
+    // expect(request.request.method).toBe('GET');
+    // request.flush();
+  });
 
-  it('should return error if parsed preview data request failed', inject(
-    [WorkbenchService],
-    (WorkbenchService: WorkbenchService) => {
-      WorkbenchService.getParsedPreviewData(
+  it('should return error if parsed preview data request failed', () => {
+    workbenchService
+      .getParsedPreviewData(
         `${this.wbAPI}/${userProject}/raw/directory/inspect`
-      ).subscribe((res: any) => {
+      )
+      .subscribe((res: any) => {
         expect(res.failure.error.type).to.be(
           'ERROR_LOADING_PARSED_PREVIEW_DATA'
         );
       });
-    }
-  ));
+  });
 
-  it('should get the parsed preview data', inject(
-    [WorkbenchService],
-    (WorkbenchService: WorkbenchService) => {
-      const mockParsedPreviewData = [
-        {
-          _id: 'workbench::Sample',
-          system: {
-            user: 'A_user',
-            project: 'workbench',
-            dstype: 'base',
-            format: 'parquet',
-            name: 'Sample',
-            catalog: 'data',
-            numberOfFiles: 1
-          },
-          asOfNow: {
-            status: 'INIT',
-            started: '20180315-153647',
-            finished: '',
-            aleId: 'workbench::1521128208233',
-            batchId: 'batch-1521128206603'
-          },
-          transformations: []
+  it('should get the parsed preview data', () => {
+    const mockParsedPreviewData = [
+      {
+        _id: 'workbench::Sample',
+        system: {
+          user: 'A_user',
+          project: 'workbench',
+          dstype: 'base',
+          format: 'parquet',
+          name: 'Sample',
+          catalog: 'data',
+          numberOfFiles: 1
         },
-        {
-          _id: 'workbench::test',
-          system: {
-            user: 'A_user',
-            project: 'workbench',
-            dstype: 'base',
-            format: 'parquet',
-            name: 'test',
-            catalog: 'data',
-            numberOfFiles: 1
-          },
-          asOfNow: {
-            status: 'INIT',
-            started: '20180315-155243',
-            finished: '',
-            aleId: 'workbench::1521129164782',
-            batchId: 'batch-1521129162354'
-          },
-          transformations: []
-        }
-      ];
+        asOfNow: {
+          status: 'INIT',
+          started: '20180315-153647',
+          finished: '',
+          aleId: 'workbench::1521128208233',
+          batchId: 'batch-1521128206603'
+        },
+        transformations: []
+      },
+      {
+        _id: 'workbench::test',
+        system: {
+          user: 'A_user',
+          project: 'workbench',
+          dstype: 'base',
+          format: 'parquet',
+          name: 'test',
+          catalog: 'data',
+          numberOfFiles: 1
+        },
+        asOfNow: {
+          status: 'INIT',
+          started: '20180315-155243',
+          finished: '',
+          aleId: 'workbench::1521129164782',
+          batchId: 'batch-1521129162354'
+        },
+        transformations: []
+      }
+    ];
 
-      WorkbenchService.getParsedPreviewData(
+    workbenchService
+      .getParsedPreviewData(
         `${this.wbAPI}/${userProject}/raw/directory/inspect`
-      ).subscribe((res: any) => {
+      )
+      .subscribe((res: any) => {
         expect(res[0]._id).to.be('workbench::Sample');
         expect(res[0].system.user).to.be('A_user');
         expect(res[0].system.project).to.be('workbench');
@@ -371,45 +354,32 @@ describe('Workbench Service', () => {
         expect(res[0].asOfNow.batchId).to.be('batch-1521128206603');
       });
 
-      // const request = httpMock.expectOne(`${WorkbenchService.wbAPI}`);
-      // expect(request.request.method).to.be('GET');
-      // request.flush();
-    }
-  ));
+    // const request = httpMock.expectOne(`${WorkbenchService.wbAPI}`);
+    // expect(request.request.method).to.be('GET');
+    // request.flush();
+  });
 
-  it('createSemantic API should exist and return an Observable', inject(
-    [WorkbenchService],
-    (workbenchService: WorkbenchService) => {
-      expect(
-        workbenchService.createSemantic({}) instanceof Observable
-      ).to.equal(true);
-    }
-  ));
+  it('createSemantic API should exist and return an Observable', () => {
+    expect(workbenchService.createSemantic({}) instanceof Observable).to.equal(
+      true
+    );
+  });
 
-  it('getSemanticList API should exist and return an Observable', inject(
-    [WorkbenchService],
-    (workbenchService: WorkbenchService) => {
-      expect(
-        workbenchService.getListOfSemantic() instanceof Observable
-      ).to.equal(true);
-    }
-  ));
+  it('getSemanticList API should exist and return an Observable', () => {
+    expect(workbenchService.getListOfSemantic() instanceof Observable).to.equal(
+      true
+    );
+  });
 
-  it('getSemanticDetails API should exist and return an Observable', inject(
-    [WorkbenchService],
-    (workbenchService: WorkbenchService) => {
-      expect(
-        workbenchService.getSemanticDetails({}) instanceof Observable
-      ).to.equal(true);
-    }
-  ));
+  it('getSemanticDetails API should exist and return an Observable', () => {
+    expect(
+      workbenchService.getSemanticDetails({}) instanceof Observable
+    ).to.equal(true);
+  });
 
-  it('updateSemantic API should exist and return an Observable', inject(
-    [WorkbenchService],
-    (workbenchService: WorkbenchService) => {
-      expect(
-        workbenchService.updateSemanticDetails({}) instanceof Observable
-      ).to.equal(true);
-    }
-  ));
+  it('updateSemantic API should exist and return an Observable', () => {
+    expect(
+      workbenchService.updateSemanticDetails({}) instanceof Observable
+    ).to.equal(true);
+  });
 });
