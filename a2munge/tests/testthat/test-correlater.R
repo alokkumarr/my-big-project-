1b39b2a552b209d2db117644287cb124f2f92497
Chris Haarstick <Chris.Haarstick@synchronoss.com>
2018-09-06 18:13:51 -0400

    SIP 4467 a2munge add function to extract schema
    * removing spark version print statements which are in conflicted state

--- a2munge/tests/testthat/test-correlater.R
+++ a2munge/tests/testthat/test-correlater.R
@@ -11,10 +11,6 @@ context("correlater function unit tests")
 
 
 # Create Spark Connection and read in some data
-cat("\nSpark Home:", Sys.getenv("SPARK_HOME"), "\n")
-cat("Spark Available Versions:\n")
-print(sparklyr::spark_available_versions())
-cat("\n")
 sc <- spark_connect(master = "local")
 
 # Load data into Spark
