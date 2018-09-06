bb3b936ee4d6c193b65fe2b64047718c533fd8e7
Pradeep Tripathi <pradeep.manitripathi@synchronoss.com>
2018-09-06 11:49:51 +0530

    SIP-2631: Fixed the BaseIT setup for integration test cases.

--- dist/src/test/java/com/synchronoss/saw/BaseIT.java
+++ dist/src/test/java/com/synchronoss/saw/BaseIT.java
@@ -42,8 +42,7 @@ public class BaseIT {
         if (port == null) {
             throw new RuntimeException("Property saw.docker.port unset");
         }
-        //RestAssured.baseURI = "http://" + host + ":" + port;
-        RestAssured.baseURI = "http://localhost:" + port;
+        RestAssured.baseURI = "http://" + host + ":" + port + "/saw";
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
     }
     
