3cdec6b90d65113c18dd7ab764a5198beaae3127
Vinnie Ganesh <vinnie.ganesh@synchronoss.com>
2018-09-26 12:22:03 +0530

    SIP-4659 Altered name for the dateconverter & dateformatter functions in the required places. 1. Detecter component 2. R functions 3. Documentation 4. testthat functions.

--- a2munge/R/converter.R
+++ a2munge/R/converter.R
@@ -27,22 +27,22 @@
 #'
 #'date_func_df <- data.frame(STRING_COL = c("2017-01-01 10:15:15", "2017-09-23 14:26:59", "2017-11-15 05:05:05", "2018-05-11 08:15:18", "2018-03-27 23:59:59"), stringsAsFactors = FALSE)
 #'
-#'dateconverter(date_func_df, "STRING_COL", "yyyy-MM-dd HH:mm:ss", "datetime")
+#'converter(date_func_df, "STRING_COL", "yyyy-MM-dd HH:mm:ss", "datetime")
 
-dateconverter <-
+converter <-
   function(df,
            measure_vars,
            input_format,
            output_type,
            time_zone,
            output_suffix) {
-    UseMethod("dateconverter")
+    UseMethod("converter")
   }
 
 
-#' @rdname dateconverter
+#' @rdname converter
 #' @export
-dateconverter.data.frame <- function(df,
+converter.data.frame <- function(df,
                                      measure_vars,
                                      input_format,
                                      output_type = "datetime",
@@ -101,9 +101,9 @@ dateconverter.data.frame <- function(df,
 
 
 
-#' @rdname dateconverter
+#' @rdname converter
 #' @export
-dateconverter.tbl_spark <- function(df,
+converter.tbl_spark <- function(df,
                                     measure_vars,
                                     input_format,
                                     output_type = "datetime",
--- a2munge/R/formatter.R
+++ a2munge/R/formatter.R
@@ -27,21 +27,21 @@
 #'
 #'date_func_df <- data.frame(TIME_COL = as.POSIXlt(c("2017-01-01 10:15:15", "2017-09-23 14:26:59", "2017-11-15 05:05:05", "2018-05-11 08:15:18", "2018-03-27 23:59:59")), stringsAsFactors = FALSE)
 #'
-#'dateformatter(date_func_df, "TIME_COL", "yyyy-MM-dd HH:mm:ss", "MM/dd/yyyy HH:mm:ss")
+#'formatter(date_func_df, "TIME_COL", "yyyy-MM-dd HH:mm:ss", "MM/dd/yyyy HH:mm:ss")
 
-dateformatter <-
+formatter <-
   function(df,
            measure_vars,
            input_format,
            output_format,
            output_suffix) {
-    UseMethod("dateformatter")
+    UseMethod("formatter")
   }
 
 
-#' @rdname dateformatter
+#' @rdname formatter
 #' @export
-dateformatter.data.frame <- function(df,
+formatter.data.frame <- function(df,
                                      measure_vars,
                                      input_format,
                                      output_format = "yyyy-MM-dd HH:mm:ss",
@@ -149,9 +149,9 @@ dateformatter.data.frame <- function(df,
 
 
 
-#' @rdname dateformatter
+#' @rdname formatter
 #' @export
-dateformatter.tbl_spark <- function(df,
+formatter.tbl_spark <- function(df,
                                     measure_vars,
                                     input_format,
                                     output_format = "yyyy-MM-dd HH:mm:ss",
