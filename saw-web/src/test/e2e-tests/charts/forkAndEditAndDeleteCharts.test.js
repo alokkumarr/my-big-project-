6890575206161afac578f4f6023065ccbb68ff4a
Anudeep Patel <Anudeep.Singh@synchronoss.com>
2018-10-05 01:13:32 +0530

    SIP-4795: created critical suite test data json file.

--- saw-web/src/test/e2e-tests/charts/forkAndEditAndDeleteCharts.test.js
+++ saw-web/src/test/e2e-tests/charts/forkAndEditAndDeleteCharts.test.js
@@ -58,7 +58,6 @@ describe('Fork & Edit and delete charts: forkAndEditAndDeleteCharts.test.js', ()
     it('should fork, edit and delete ' + description +' testDataMetaInfo: '+ JSON.stringify({test:description,feature:'FORKDELETECHARTS', dp:'forkDeleteChartsDataProvider'}), () => {
         try {
           let currentTime = new Date().getTime();
-          let user = data.user;
           let type = data.chartType.split(":")[1];
           let name = data.chartType+' ' + globalVariables.e2eId+'-'+currentTime;
           let description ='Description:'+data.chartType+' for e2e ' + globalVariables.e2eId+'-'+currentTime;
