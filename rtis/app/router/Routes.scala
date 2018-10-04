04cef02121eb0f2ec052276a48b2ec501713b593
ngan0002 <ngan0002@synchronoss.com>
2018-10-04 15:58:29 -0400

    SIP-4496 java docs improved

--- rtis/app/router/Routes.scala
+++ rtis/app/router/Routes.scala
@@ -1,6 +1,6 @@
 
 // @GENERATOR:play-routes-compiler
-// @SOURCE:/Users/nareshgangishetty/swagger-work/sip/rtis/conf/routes
+// @SOURCE:C:/projects/BDA/RTIS-logstash/frontend-server/conf/routes
 // @DATE:Wed Oct 03 15:10:59 EDT 2018
 
 package router
