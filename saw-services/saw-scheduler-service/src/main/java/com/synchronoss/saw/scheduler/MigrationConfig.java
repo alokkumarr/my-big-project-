b077183eefad8586dc153f2a12286ea167dd9492
Markus Holmberg <markus.holmberg@synchronoss.com>
2018-10-10 11:05:06 -0400

    SIP-3317: Only run migration on first Scheduler Service

--- saw-services/saw-scheduler-service/src/main/java/com/synchronoss/saw/scheduler/MigrationConfig.java
+++ saw-services/saw-scheduler-service/src/main/java/com/synchronoss/saw/scheduler/MigrationConfig.java
@@ -3,36 +3,57 @@ package com.synchronoss.saw.scheduler;
 import org.flywaydb.core.Flyway;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.concurrent.ThreadLocalRandom;
+import javax.validation.constraints.NotNull;
 
 @Configuration
 public class MigrationConfig {
-  private final Logger log = LoggerFactory.getLogger(getClass());
+  private final Logger log = LoggerFactory.getLogger(MigrationConfig.class);
+
+  @Value("${sip.service.index}")
+  @NotNull
+  private Integer serviceIndex;
 
   @Bean
   public FlywayMigrationStrategy migrationStrategy() {
     return new FlywayMigrationStrategy() {
       @Override
       public void migrate(Flyway flyway) {
-        log.info("Starting migration");
+        log.info("Starting migration (service index: {})", serviceIndex);
+        /* Run migration only on first node, due to MariaDB Galera
+         * cluster not supporting the locking mechanisms that Flyway
+         * depends on */
+        if (serviceIndex == 0) {
+          log.info("Running migration on first node");
+          flyway.migrate();
+        } else {
+          log.info("Letting migration run on first node");
+          waitForMigration(flyway, 5);
+        }
+        log.info("Finished migration");
+      }
+
+      private void waitForMigration(Flyway flyway, int retries) {
         if (flyway.info().pending().length > 0) {
-          /* Delay migration start to reduce the probability of
-           * multiple instances of the same service attempting
-           * migration at the same */
-          int delay = ThreadLocalRandom.current().nextInt(0, 10000);
-          log.info("Delaying startup: {} ms", delay);
           try {
-            Thread.sleep(delay);
+            log.info("Waiting for first node to complete migration");
+            Thread.sleep(5000);
           } catch (InterruptedException e) {
             log.info("Interrupted");
           }
+          if (retries > 0) {
+            waitForMigration(flyway, retries - 1);
+          } else {
+            log.warn("Out of retries waiting for migration");
+          }
+        } else {
+          log.info("Done waiting for migration");
         }
-        flyway.migrate();
-        log.info("Finished migration");
       }
     };
   }
