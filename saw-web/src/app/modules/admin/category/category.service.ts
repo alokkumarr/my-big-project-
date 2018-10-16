e047515d7e57f449e8d48428f4e916a898493d53
Akash Agrawal <Akash.Agarwal@synchronoss.com>
2018-10-16 14:42:46 +0530

    SIP-4452: Fix observable operator errors in admin

--- saw-web/src/app/modules/admin/category/category.service.ts
+++ saw-web/src/app/modules/admin/category/category.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AdminService } from '../main-view/admin.service';
 import { JwtService } from '../../../common/services';
+import { map } from 'rxjs/operators';
 import { IAdminDataService } from '../admin-data-service.interface';
 
 interface CategoryResponse {
@@ -21,7 +22,6 @@ interface ModulesResponse {
 
 @Injectable()
 export class CategoryService implements IAdminDataService {
-
   customerId;
   constructor(
     public _adminService: AdminService,
@@ -34,8 +34,9 @@ export class CategoryService implements IAdminDataService {
 
   getList() {
     const customerId = parseInt(this.customerId, 10);
-    return this._adminService.request<CategoryResponse>('categories/fetch', customerId)
-      .map(resp => resp.categories)
+    return this._adminService
+      .request<CategoryResponse>('categories/fetch', customerId)
+      .pipe(map(resp => resp.categories))
       .toPromise();
   }
 
@@ -43,36 +44,40 @@ export class CategoryService implements IAdminDataService {
     const options = {
       toast: { successMsg: 'Category is successfully added' }
     };
-    return this._adminService.request<CategoryResponse>('categories/add', user, options)
-      .map(resp => resp.valid ? resp.categories : null)
+    return this._adminService
+      .request<CategoryResponse>('categories/add', user, options)
+      .pipe(map(resp => (resp.valid ? resp.categories : null)))
       .toPromise();
   }
 
   remove(params: {
-    categoryId: number,
-    customerId: number, s
-    masterLoginId: string,
-    categoryCode: number
+    categoryId: number;
+    customerId: number;
+    s;
+    masterLoginId: string;
+    categoryCode: number;
   }) {
     const options = {
       toast: { successMsg: 'Category is successfully deleted' }
     };
-    return this._adminService.request<CategoryResponse>('categories/delete', params, options)
-      .map(resp => resp.valid ? resp.categories : null)
+    return this._adminService
+      .request<CategoryResponse>('categories/delete', params, options)
+      .pipe(map(resp => (resp.valid ? resp.categories : null)))
       .toPromise();
   }
 
   removeSubCategory(params: {
-    categoryId: number,
-    customerId: number,
-    masterLoginId: string,
-    categoryCode: number
+    categoryId: number;
+    customerId: number;
+    masterLoginId: string;
+    categoryCode: number;
   }) {
     const options = {
       toast: { successMsg: 'Subcategory is successfully deleted' }
     };
-    return this._adminService.request<CategoryResponse>('subcategories/delete', params, options)
-      .map(resp => resp.valid ? resp.subCategories : null)
+    return this._adminService
+      .request<CategoryResponse>('subcategories/delete', params, options)
+      .pipe(map(resp => (resp.valid ? resp.subCategories : null)))
       .toPromise();
   }
 
@@ -80,34 +85,38 @@ export class CategoryService implements IAdminDataService {
     const options = {
       toast: { successMsg: 'Category is successfully Updated' }
     };
-    return this._adminService.request<CategoryResponse>('categories/edit', user, options)
-      .map(resp => resp.valid ? resp.categories : null)
+    return this._adminService
+      .request<CategoryResponse>('categories/edit', user, options)
+      .pipe(map(resp => (resp.valid ? resp.categories : null)))
       .toPromise();
   }
 
   getProducts(customerId) {
-    return this._adminService.request<ProuctsResponse>('products/list', customerId)
-      .map(resp => resp.products)
+    return this._adminService
+      .request<ProuctsResponse>('products/list', customerId)
+      .pipe(map(resp => resp.products))
       .toPromise();
   }
 
   getModules(params: {
-    customerId: string,
-    productId: number,
-    moduleId: number
+    customerId: string;
+    productId: number;
+    moduleId: number;
   }) {
-    return this._adminService.request<ModulesResponse>('modules/list', params)
-      .map(resp => resp.modules)
+    return this._adminService
+      .request<ModulesResponse>('modules/list', params)
+      .pipe(map(resp => resp.modules))
       .toPromise();
   }
 
   getParentCategories(params: {
-    customerId: string,
-    productId: number,
-    moduleId: number
+    customerId: string;
+    productId: number;
+    moduleId: number;
   }) {
-    return this._adminService.request<CategoryResponse>('categories/parent/list', params)
-      .map(resp => resp.category)
+    return this._adminService
+      .request<CategoryResponse>('categories/parent/list', params)
+      .pipe(map(resp => resp.category))
       .toPromise();
   }
 }
