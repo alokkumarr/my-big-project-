792cc475462e68034074365934cdeb22cfec4824
Chris Haarstick <Chris.Haarstick@synchronoss.com>
2018-10-19 10:44:53 -0400

    SIP-4762 update logging to r build process
    * Attempting to resolve merge conflict

--- a2modeler/tests/testthat/test-regressor.R
+++ a2modeler/tests/testthat/test-regressor.R
@@ -27,16 +27,16 @@ df <- copy_to(sc, mtcars, name = "df", overwrite = TRUE)
 
 
 test_that("Regressor Constructer", {
-
+  
   r1 <- regressor(df = df, target = "mpg", name = "test")
   expect_class(r1, "regressor")
 })
 
 
 test_that("Regressor Selects Best Model", {
-
+  
   test_pipe <- pipeline(expr = function(x){select(x, mpg, wt, hp)})
-
+  
   r1 <- regressor(df = df, target = "mpg", name = "test") %>%
     add_holdout_samples(splits = c(.5, .5)) %>%
     add_model(pipe = test_pipe,
@@ -45,7 +45,7 @@ test_that("Regressor Selects Best Model", {
               method = "ml_decision_tree_regressor") %>%
     train_models() %>%
     set_final_model(., method = "best", reevaluate = FALSE, refit = FALSE)
-
+  
   expect_subset("spark_model", class(r1$final_model))
   expect_subset("spark_model_regression", class(r1$final_model))
   expect_subset(
@@ -55,8 +55,8 @@ test_that("Regressor Selects Best Model", {
       dplyr::top_n(1, -rmse) %>%
       dplyr::pull(model_uid)
   )
-
-
+  
+  
   p1 <- predict(r1, data = df)
   expect_class(p1, "predictions")
   expect_error(predict(r1, data = select(df, wt)))
