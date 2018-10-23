49e18f6e3e984e51091362857ca4b2516b379d95
Akash Agrawal <Akash.Agarwal@synchronoss.com>
2018-10-23 13:50:15 +0530

    SIP-4620: Fix ts issues in unit tests

--- saw-web/src/app/modules/observe/validators/non-empty.validator.spec.ts
+++ saw-web/src/app/modules/observe/validators/non-empty.validator.spec.ts
@@ -1,14 +1,9 @@
-import { nonEmpty } from "./non-empty.validator";
-import {
-  FormGroup,
-  FormControl,
-  FormBuilder,
-  ReactiveFormsModule
-} from "@angular/forms";
+import { nonEmpty } from './non-empty.validator';
+import { FormGroup, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
-import { TestBed, inject } from "@angular/core/testing";
+import { TestBed } from '@angular/core/testing';
 
-describe("Non Empty Validator", () => {
+describe('Non Empty Validator', () => {
   let form: FormGroup;
 
   beforeEach(() => {
@@ -19,30 +14,30 @@ describe("Non Empty Validator", () => {
     });
     const fb: FormBuilder = TestBed.get(FormBuilder);
     form = fb.group({
-      field1: ["", nonEmpty()]
+      field1: ['', nonEmpty()]
     });
   });
 
-  it("should be invalid if control empty", () => {
-    const expectedObj = { nonEmpty: { value: "" } };
-    form.get("field1").setValue("");
+  it('should be invalid if control empty', () => {
+    const expectedObj = { nonEmpty: { value: '' } };
+    form.get('field1').setValue('');
     expect(form.invalid).toBeTruthy();
-    expect(form.get("field1").errors).toEqual(
+    expect(form.get('field1').errors).toEqual(
       jasmine.objectContaining(expectedObj)
     );
   });
 
-  it("should be valid if control empty", () => {
-    form.get("field1").setValue("      a     b");
+  it('should be valid if control empty', () => {
+    form.get('field1').setValue('      a     b');
 
     expect(form.invalid).toBeFalsy();
   });
 
-  it("should be invalid if control only has spaces", () => {
-    const expectedObj = { nonEmpty: { value: "         " } };
-    form.get("field1").setValue("         ");
+  it('should be invalid if control only has spaces', () => {
+    const expectedObj = { nonEmpty: { value: '         ' } };
+    form.get('field1').setValue('         ');
     expect(form.invalid).toBeTruthy();
-    expect(form.get("field1").errors).toEqual(
+    expect(form.get('field1').errors).toEqual(
       jasmine.objectContaining(expectedObj)
     );
   });
