90c484c75f2993e70e14dca84feb83285933e1b2
Anirudh Venkatesh <Anirudh.NV@synchronoss.com>
2018-10-31 14:27:50 +0530

    SIP-1288: Adding unit tests

---
+++ saw-web/src/app/modules/admin/datasecurity/userassignment.service.spec.ts
@@ -0,0 +1,43 @@
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { UserAssignmentService } from './userassignment.service';
+import { JwtService } from '../../../common/services/jwt.service';
+import { Observable } from 'rxjs';
+
+const mockService = {};
+let userAssignmentService;
+
+describe('User Assignment Service', () => {
+  beforeEach(done => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        UserAssignmentService,
+        { provide: JwtService, useValue: mockService },
+        { provide: Router, useValue: mockService },
+        { provide: ActivatedRoute, useValue: mockService }
+      ]
+    }).compileComponents();
+
+    userAssignmentService = TestBed.get(UserAssignmentService);
+    done();
+  });
+
+  it('should exist', () => {
+    expect(userAssignmentService).not.toBeNull();
+  });
+
+  it('getList function should exist', () => {
+    expect(userAssignmentService.getList()).not.toBeNull();
+  });
+
+  it('executeKPI should exist and return an Observable', inject(
+    [UserAssignmentService],
+    (observe: UserAssignmentService) => {
+      expect(observe.addSecurityGroup({}) instanceof Observable).toEqual(false);
+    }
+  ));
+});
