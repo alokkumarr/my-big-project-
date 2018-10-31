3d3a1118e2311df71f7e63ea953d63a8155f9690
Akhilesh Obilineni <Akhilesh.Obilineni@synchronoss.com>
2018-10-31 00:12:07 -0400

    SIP-4615: Fix for build failure

--- saw-web/src/app/modules/workbench/services/datasource.service.spec.ts
+++ saw-web/src/app/modules/workbench/services/datasource.service.spec.ts
@@ -1,9 +1,27 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { DatasourceService } from './datasource.service';
+import { JwtService } from '../../../common/services';
+import { Observable } from 'rxjs/Observable';
 
+const mockService = {};
+const jwtMockService = {
+  customerCode: 'Synchronoss',
+
+  getUserName() {
+    return 'SIP Admin';
+  }
+};
 describe('DatasourceService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        DatasourceService,
+        { provide: JwtService, useValue: jwtMockService }
+      ]
+    }));
 
   it('should be created', () => {
     const service: DatasourceService = TestBed.get(DatasourceService);
