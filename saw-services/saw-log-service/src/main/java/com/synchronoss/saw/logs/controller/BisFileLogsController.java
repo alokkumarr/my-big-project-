2a2990a04ba66b6bca8b7cf043d9a5ae3f8bf2a8
ngan0002 <ngan0002@synchronoss.com>
2018-11-01 12:13:45 -0400

    SIP-5050 updated for unit tests

--- saw-services/saw-log-service/src/main/java/com/synchronoss/saw/logs/controller/BisFileLogsController.java
+++ saw-services/saw-log-service/src/main/java/com/synchronoss/saw/logs/controller/BisFileLogsController.java
@@ -4,7 +4,9 @@ import com.synchronoss.saw.logs.entities.BisFileLogs;
 import com.synchronoss.saw.logs.repository.FileLogsRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/bisfilelogs")
-public class BisFileLogsControllers {
+public class BisFileLogsController {
 
   @Autowired
   private FileLogsRepository bisLogsRepository;
 
   @GetMapping
-  public List<BisFileLogs> retrieveAllStudents() {
+  public List<BisFileLogs> retrieveAllLogs() {
     return this.bisLogsRepository.findAll();
+  }
+  
+  @GetMapping("/{id}")
+  public BisFileLogs retriveLogById(@PathVariable String id) {
+
+    return this.bisLogsRepository.findByPid(id);
   }
 
 }
--- saw-services/saw-log-service/src/test/java/com/synchronoss/saw/logs/BisFileLogsTest.java
+++ saw-services/saw-log-service/src/test/java/com/synchronoss/saw/logs/BisFileLogsTest.java
@@ -1,51 +1,51 @@
 package com.synchronoss.saw.logs;
 
-import com.synchronoss.saw.logs.entities.BisFileLogs;
-import com.synchronoss.saw.logs.repository.FileLogsRepository;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
-
+import com.synchronoss.saw.logs.repository.FileLogsRepository;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-//@SpringBootTest(classes={SawLogServiceApplication.class})
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.HSQL)
 @ActiveProfiles("test")
-//@TestPropertySource(locations="classpath:application-test.properties")
-//(replace = Replace.NONE)
 public class BisFileLogsTest {
     @Autowired
     private FileLogsRepository bisLogsRepository;
 
-    @Autowired
-    private TestEntityManager entityManager;
-
+    /**
+     * Tests findAll method from repository
+     */
     @Test
     public void testiFndAll() {
-        Iterable<BisFileLogs> items = this.bisLogsRepository.findAll();
-
-        for (BisFileLogs bisFileLogs : items) {
-            System.out.println("$$$$$$" + bisFileLogs.getFileName());
-
-        }
-
-        // BisFileLogs log = this.bisLogsRepository.findByPid("2");
-        // System.out.println(log.getFileName());
+        Assert.assertEquals(this.bisLogsRepository.findAll().size(), 1);
     }
 
+    /**
+     * Tests file name exists or not in the logs
+     */
     @Test
     public void testFindByFileName() {
-        System.out.println("$$$$$$" + this.bisLogsRepository.isFileNameExists("TEST"));
+        Assert.assertTrue(this.bisLogsRepository.isFileNameExists("TEST"));
+        Assert.assertFalse(this.bisLogsRepository.isFileNameExists("xyz"));
+
+    }
+    
+    /**
+     * Tests log entry with given pid exists or not
+     */
+    @Test
+    public void testFindByPid() {
+        Assert.assertNotNull(this.bisLogsRepository.findByPid("2"));
+        Assert.assertNull(this.bisLogsRepository.findByPid(null));
+
     }
 
 }
