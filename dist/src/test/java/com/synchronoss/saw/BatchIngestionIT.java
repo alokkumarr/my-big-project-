cbc15d81a4f1ee209f54915dbb472377f0cde6bc
ngan0002 <ngan0002@synchronoss.com>
2018-11-09 12:12:45 -0500

    SIP-5176 Temporarily ignoring integration tests of batch ingestion service

--- dist/src/test/java/com/synchronoss/saw/BatchIngestionIT.java
+++ dist/src/test/java/com/synchronoss/saw/BatchIngestionIT.java
@@ -14,6 +14,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,6 +22,7 @@ import org.slf4j.LoggerFactory;
 /**
  * Batch Ingestion Service integration tests. CRUD Operation both Route & Channel
  */
+@Ignore
 public class BatchIngestionIT extends BaseIT {
   private static final String BATCH_CHANNEL = "channels";
   private static final String BATCH_ROUTE = "routes";
