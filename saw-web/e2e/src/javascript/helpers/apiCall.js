680b4528ad375a120328d50223b8cc4eae041435
Anudeep Patel <Anudeep.Singh@synchronoss.com>
2018-11-20 19:24:40 +0530

    SIP-5017: added logger and exiting the process if test failed

--- saw-web/e2e/src/javascript/helpers/apiCall.js
+++ saw-web/e2e/src/javascript/helpers/apiCall.js
@@ -1,5 +1,5 @@
 const request = require('sync-request');
-
+const logger = require('../../../v2/conf/logger')(__filename);
 /*
  * Helper to perform API calls
  * Documentation: https://www.npmjs.com/package/sync-request
@@ -7,21 +7,53 @@ const request = require('sync-request');
 module.exports = {
   post: (url, payload, token) => {
 
-    return JSON.parse(request('POST', url, {
-      headers: {'Authorization': token},
+    logger.info('Api call url :'+url);
+    logger.warn('Api call payload :'+JSON.stringify(payload));
+    logger.debug('Api call token :'+token);
+    let headers = {};
+    if (token) {
+      headers['Authorization'] = token;
+    }
+    let response = request('POST', url, {
+      headers: headers,
       json: payload
-    }).getBody());
+    });
+    if (response.statusCode === 200) {
+      return JSON.parse(response.getBody());
+    } else {
+      logger.error('Api call failed with status code:'+response.statusCode+' url: '+url+' ,Hence exiting this process');
+      process.exit(1);
+    }
+
   },
   get: (url, token) => {
 
-    return JSON.parse(request('GET', url, {
-      headers: {'Authorization': token}
-    }).getBody());
+    logger.info('Api call url :'+url);
+    logger.debug('Api call token :'+token);
+    let response = request('GET', url, {
+      headers: { Authorization: token }
+    });
+
+    if (response.statusCode === 200 || response.statusCode === 202) {
+      return JSON.parse(response.getBody());
+    } else {
+      logger.error('Api call failed with status code:'+response.statusCode+' url: '+url+' ,Hence exiting this process');
+      process.exit(1);
+    }
   },
   delete: (url, token) => {
 
-    return JSON.parse(request('DELETE', url, {
-      headers: {'Authorization': token}
-    }).getBody());
+    logger.info('Api call url :'+url);
+    logger.debug('Api call token :'+token);
+    let response = request('DELETE', url, {
+      headers: { Authorization: token }
+    });
+
+    if (response.statusCode === 200) {
+      return JSON.parse(response.getBody());
+    } else {
+      logger.error('Api call failed with status code:'+response.statusCode+' url: '+url+' ,Hence exiting this process');
+      process.exit(1);
+    }
   }
 };
