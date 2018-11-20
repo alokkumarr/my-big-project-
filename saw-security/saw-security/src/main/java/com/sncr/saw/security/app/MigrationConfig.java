e3d486350794c8ffd710d4371d28d25d30dbe4bf
Pradeep Tripathi <pradeep.manitripathi@synchronoss.com>
2018-11-20 19:37:22 +0530

    SIP-4811 : Increase database migration waiting retries , Also limit the deployment to single saw-Node to narrow down issue (Temp changes) .

--- saw-security/saw-security/src/main/java/com/sncr/saw/security/app/MigrationConfig.java
+++ saw-security/saw-security/src/main/java/com/sncr/saw/security/app/MigrationConfig.java
@@ -34,7 +34,7 @@ public class MigrationConfig {
           flyway.migrate();
         } else {
           log.info("Letting migration run on first node");
-          waitForMigration(flyway, 5);
+          waitForMigration(flyway, 20);
         }
         log.info("Finished migration");
       }
