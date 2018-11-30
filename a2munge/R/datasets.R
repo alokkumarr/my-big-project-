7470bcc0b4a5064e8e0a05cc00c285955fbb34ea
Chris Haarstick <Chris.Haarstick@synchronoss.com>
2018-11-30 15:04:13 -0500

    SIP- improve a2f testing
    * updated documentation and added anomaly df documentation

--- a2munge/R/datasets.R
+++ a2munge/R/datasets.R
@@ -15,3 +15,16 @@
 #'   \item{metric2}{numeric type. numerical metric example.}
 #'   }
 "sim_df"
+
+#' Simulated Time Series with Anomalies Dataset
+#'
+#' A simulated time series dataset with anomalies to be used in detecter testing
+#'
+#' @format A tibble with 100 rows and 4 variables 
+#'  \describe{
+#'   \item{index}{integer type with sequential index of records}
+#'   \item{date}{date type. date of record}
+#'   \item{y}{numeric type. time series value. created with arima.sim function. anomalies added}
+#'   \item{flag}{integer type. flag for anomalies added. 1 = positive anomaly, -1 = negative anomaly, 0 = no anomaly }
+#'   }
+"sim_df_anom"
