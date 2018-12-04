1380aa1b1808c81b95aafe5982b2927cc6b823cc
Anirudh Venkatesh <Anirudh.NV@synchronoss.com>
2018-12-04 16:20:33 +0530

    SIP-5366: Commenting version for now in footer to get success in compliling

--- saw-web/src/app/layout/components/footer/footer.component.ts
+++ saw-web/src/app/layout/components/footer/footer.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { VERSION } from '../../../../environments/version';
+//import { VERSION } from '../../../../environments/version';
 
 @Component({
   selector: 'layout-footer',
   templateUrl: 'footer.component.html'
 })
 export class LayoutFooterComponent {
-  version = VERSION.semverString;
+  //version = VERSION.semverString;
 }
