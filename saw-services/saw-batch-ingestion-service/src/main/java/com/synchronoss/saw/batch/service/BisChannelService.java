b8a1409accafaa6b96adea00b4f087a838605b97
ngan0002 <Naresh.Gangishetty@synchronoss.com>
2018-12-06 12:57:13 -0500

    SIP-5311: updated missing package and classes

---
+++ saw-services/saw-batch-ingestion-service/src/main/java/com/synchronoss/saw/batch/service/BisChannelService.java
@@ -0,0 +1,78 @@
+package com.synchronoss.saw.batch.service;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.synchronoss.saw.batch.controller.SawBisRouteController;
+import com.synchronoss.saw.batch.entities.BisChannelEntity;
+import com.synchronoss.saw.batch.entities.repositories.BisChannelDataRestRepository;
+import com.synchronoss.saw.batch.exception.ResourceNotFoundException;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BisChannelService {
+
+  @Autowired
+  BisChannelDataRestRepository bisChannelRepository;
+
+  @Autowired
+  BisRouteService bisRouteService;
+
+  private static final Logger logger = LoggerFactory.getLogger(BisChannelService.class);
+  
+  /**
+   * Activates or deactivates a channel to temporarily
+   * stop or resume.
+   */
+  public void activateOrDeactivateChannel(Long channelId, boolean isActivate) {
+
+    Optional<BisChannelEntity> bisChannel = bisChannelRepository.findById(channelId);
+    if (bisChannel.isPresent()) {
+      BisChannelEntity bisChannelEntity = bisChannel.get();
+      Long status;
+      if (isActivate) {
+        status = 1L;
+      } else {
+        status = 0L;
+      }
+      bisChannelEntity.setStatus(status);
+      bisChannelRepository.saveAndFlush(bisChannelEntity);
+      bisRouteService.activateOrDeactivateRoutes(channelId, isActivate);
+    } else {
+      throw new ResourceNotFoundException("No resource found ");
+    }
+  }
+  
+  /**
+   * Checks if there is any channel already
+   * exists with given name.
+   */
+  public boolean isChannelNameExists(String channelName) {
+    List<BisChannelEntity> channelEntities = bisChannelRepository.findAll();
+    ObjectMapper objectMapper = new ObjectMapper();
+    Optional<BisChannelEntity> channels = channelEntities.stream().filter(bisChannelEntity -> {
+      JsonNode metaDataNode;
+      JsonNode existingChannel;
+      try {
+        metaDataNode = objectMapper.readTree(bisChannelEntity.getChannelMetadata());
+        existingChannel = metaDataNode.get("channelName");
+        if (existingChannel != null && existingChannel.asText().equalsIgnoreCase(channelName)) {
+          return true;
+        }
+      } catch (IOException exception) {
+        logger.error(exception.getMessage());
+      }
+      return false;
+    }).findAny();
+
+    return channels.isPresent();
+  }
+
+}
