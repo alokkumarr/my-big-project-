eaf5289e9bce417fb6ab700d535ffacf3eb34a90
Pradeep Tripathi <pradeep.manitripathi@synchronoss.com>
2018-12-10 21:38:04 +0530

    SIP-5362 : Corrected indentation for integration Tests cases.

--- dist/src/test/java/com/synchronoss/saw/SipDslIT.java
+++ dist/src/test/java/com/synchronoss/saw/SipDslIT.java
@@ -15,19 +15,18 @@ import static io.restassured.RestAssured.given;
 public class SipDslIT extends BaseIT {
   @Ignore("Placeholder integration test, dependency on SIP-5433")
   @Test
-  public void testSIPDSL() throws IOException {
+  public void testSipDsl() throws IOException {
     ClassLoader classLoader = getClass().getClassLoader();
     File file = new File(classLoader.getResource("sample-DSL.json").getPath());
     ObjectMapper objectMapper = new ObjectMapper();
     JsonNode jsonNode = objectMapper.readTree(file);
     Response response = given(spec)
-      .header("Authorization", "Bearer " + token)
-      .body(jsonNode)
-      .when().post("/saw/services/internal/proxy/storage/fetch")
-      .then().assertThat().statusCode(200)
-      .extract().response();
-      ObjectNode root = response.as(ObjectNode.class);
-
+       .header("Authorization", "Bearer " + token)
+       .body(jsonNode)
+       .when().post("/saw/services/internal/proxy/storage/fetch")
+       .then().assertThat().statusCode(200)
+       .extract().response();
+    ObjectNode root = response.as(ObjectNode.class);
   }
 
 }
