51a1c75caa7e685e37e9460951e50033efe9c747
Saurav Paul <Saurav.Paul@synchronoss.com>
2018-12-14 17:07:48 -0500

    SIP-1674 : dynamic ports

--- saw-services/saw-gateway-service/src/test/java/com/synchronoss/saw/gateway/GatewayServiceControllerTest.java
+++ saw-services/saw-gateway-service/src/test/java/com/synchronoss/saw/gateway/GatewayServiceControllerTest.java
@@ -11,6 +11,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -29,23 +30,25 @@ import com.synchronoss.saw.gateway.controller.GatewayController;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(GatewayController.class)
-@TestPropertySource(properties = {
-		"security.service.host=http://localhost:9081" }, locations = "application-test.properties")
+@TestPropertySource(locations = "application-test.properties")
 public class GatewayServiceControllerTest {
 
 	@Rule
-	public WireMockRule wireMockRule_1 = new WireMockRule(options().port(9081));
+	public WireMockRule wireMockRule_1 = new WireMockRule(options().port(9093));
 	WireMockServer wireMockServer = null;
 
 	@Before
 
 	public void setup() throws Exception {
-		wireMockServer = new WireMockServer(options().port(9091));
+		wireMockServer = new WireMockServer(options().dynamicPort());
 		wireMockServer.start();
 	}
 
 	@Autowired
 	private MockMvc mockMvc;
+	
+	@Value("${security.service.host}")
+	private String securityUrl;
 
 	@Test
 	public void gatewayControllerTestWithAuthorizationHeader() throws Exception {
@@ -53,9 +56,9 @@ public class GatewayServiceControllerTest {
 				.withRequestBody(WireMock.equalTo(""))
 				.willReturn(aResponse().withStatus(200).withHeader("Content-Type", "application/json").withBody("")));
 		HttpHeaders requestHeaders = new HttpHeaders();
-		requestHeaders.set("host", "localhost");
 		requestHeaders.set("authorization", "localhost");
-		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.post("/api/md").headers(requestHeaders)
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.post(securityUrl + wireMockServer.port());
+		builder.headers(requestHeaders)
 				.contentType(MediaType.APPLICATION_JSON);
 		mockMvc.perform(builder).andExpect(MockMvcResultMatchers.status().isNotFound())
 				.andDo(MockMvcResultHandlers.print());
