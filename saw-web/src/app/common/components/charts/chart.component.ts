edc8388a494080b09e2e2abfea7ab231decf0559
Akash Agrawal <Akash.Agarwal@synchronoss.com>
2018-12-26 18:32:23 +0530

    SIP-5305: Fix chart options mutation pollution

--- saw-web/src/app/common/components/charts/chart.component.ts
+++ saw-web/src/app/common/components/charts/chart.component.ts
@@ -17,6 +17,7 @@ import * as filter from 'lodash/filter';
 import * as set from 'lodash/set';
 import * as get from 'lodash/get';
 import * as clone from 'lodash/clone';
+import * as cloneDeep from 'lodash/cloneDeep';
 import * as isArray from 'lodash/isArray';
 import * as find from 'lodash/find';
 
@@ -32,16 +33,12 @@ export const UPDATE_PATHS = {
   X_AXIS: 'xAxis'
 };
 
-export const CHART_SETTINGS_OBJ = [
-  { type: 'default', config: chartOptions },
-  { type: 'highStock', config: stockChartOptions },
-  { type: 'bullet', config: bulletChartOptions }
-];
-
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'chart',
-  template: `<div #container></div>`
+  template: `
+    <div #container></div>
+  `
 })
 export class ChartComponent implements OnInit, AfterViewInit, OnDestroy {
   @Input()
@@ -52,6 +49,19 @@ export class ChartComponent implements OnInit, AfterViewInit, OnDestroy {
   enableExport: boolean;
   @ViewChild('container')
   container: ElementRef;
+
+  /**
+   * Cloning all the options to create a new copy per object.
+   * Otherwise, any changes to these options will be shared between
+   * all instances of chart.
+   *
+   * @memberof ChartComponent
+   */
+  chartSettings = [
+    { type: 'default', config: cloneDeep(chartOptions) },
+    { type: 'highStock', config: cloneDeep(stockChartOptions) },
+    { type: 'bullet', config: cloneDeep(bulletChartOptions) }
+  ];
 
   public highcharts: any = Highcharts;
   public highstocks: any = Highstock;
@@ -95,13 +105,13 @@ export class ChartComponent implements OnInit, AfterViewInit, OnDestroy {
     // set the appropriate config based on chart type
     this.cType = this.isStockChart ? 'highStock' : options.chart.type;
     this.config = defaultsDeep(
-      options,
-      this.config,
       get(
-        find(CHART_SETTINGS_OBJ, ['type', this.cType]),
+        find(this.chartSettings, ['type', this.cType]),
         'config',
         chartOptions
-      )
+      ),
+      options,
+      this.config
     );
     if (this.enableExport) {
       this.config.exporting = {
