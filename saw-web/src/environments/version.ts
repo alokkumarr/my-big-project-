557dd89ec7c37798cff4e94754c6c7716daf402d
Saurav Paul <Saurav.Paul@synchronoss.com>
2019-01-02 18:23:46 -0500

    Revert "SIP-5404 : Remove version.ts  & package-lock"
    This reverts commit ea829aa0cc8df51b629783c788fd01388a0aa267.

---
+++ saw-web/src/environments/version.ts
@@ -0,0 +1,22 @@
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+export const VERSION = {
+    "dirty": true,
+    "raw": "v3.0.0-2350-gd48939307-dirty",
+    "hash": "gd48939307",
+    "distance": 2350,
+    "tag": "v3.0.0",
+    "semver": {
+        "raw": "v3.0.0",
+        "major": 3,
+        "minor": 0,
+        "patch": 0,
+        "prerelease": [],
+        "build": [],
+        "version": "3.0.0"
+    },
+    "suffix": "2350-gd48939307-dirty",
+    "semverString": "3.0.0+2350.gd48939307",
+    "version": "0.0.0"
+};
+/* tslint:enable */
