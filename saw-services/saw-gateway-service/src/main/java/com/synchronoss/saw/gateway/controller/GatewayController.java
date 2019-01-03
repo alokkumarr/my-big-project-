732abf841744abee2f62973df39bbe4131ccbe63
Markus Holmberg <markus.holmberg@synchronoss.com>
2019-01-03 14:28:23 -0500

    SIP-4612: Remove annotations allowing access from any origin
    Allowing access from any origin is a potential security issue, so
    remove these CORS annotations.

--- saw-services/saw-gateway-service/src/main/java/com/synchronoss/saw/gateway/controller/GatewayController.java
+++ saw-services/saw-gateway-service/src/main/java/com/synchronoss/saw/gateway/controller/GatewayController.java
@@ -46,7 +46,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -99,7 +98,6 @@ public class GatewayController {
    * @throws ServletException 
    * @throws FileUploadException 
  */
-  @CrossOrigin(origins = "*")
 @RequestMapping(value = "/{path:^(?!actuator).*$}/**", method = {GET, POST, DELETE, OPTIONS, PUT})
   @ResponseBody
   /* Note: Spring Boot Actuator paths are excluded from proxying above
