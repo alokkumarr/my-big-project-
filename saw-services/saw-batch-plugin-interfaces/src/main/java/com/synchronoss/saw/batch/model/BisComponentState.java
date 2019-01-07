b2d34507297fcafdbe67c0ee015c184e4a073c1a
ngan0002 <Naresh.Gangishetty@synchronoss.com>
2019-01-07 14:56:43 -0500

    bugfix/SIP-5755-incorrect-state-in-db-by-bis-process

--- saw-services/saw-batch-plugin-interfaces/src/main/java/com/synchronoss/saw/batch/model/BisComponentState.java
+++ saw-services/saw-batch-plugin-interfaces/src/main/java/com/synchronoss/saw/batch/model/BisComponentState.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum BisComponentState {
-  DATA_RECEIVED("DATA_RECEIVED"), DATA_REMOVED("DATA_REMOVED");
+  DATA_RECEIVED("DATA_RECEIVED"), DATA_REMOVED("DATA_REMOVED"), HOST_NOT_REACHABLE("HOST_NOT_REACHABLE");
   private final String value;
   private static final Map<String, BisComponentState> CONSTANTS =
       new HashMap<String, BisComponentState>();
