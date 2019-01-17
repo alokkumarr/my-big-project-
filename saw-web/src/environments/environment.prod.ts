3f77700697d505cea016e25a3766fcbde38dc65c
Barna Mumtyan <Barna.Mumtya@synchronoss.com>
2019-01-17 16:29:53 +0200

    SIP-5742: Fixed a build isssue.

--- saw-web/src/environments/environment.prod.ts
+++ saw-web/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoiYWtpbDMiLCJhIjoiY2plb3hnanJkMGNidjMzbHRzdTFsNHM4dSJ9.m0Q12LAgXXMW2c7zU1FPkQ'
+  }
 };
