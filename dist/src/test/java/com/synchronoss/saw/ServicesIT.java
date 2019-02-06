03aaf58a84af4d9d999a283245c6199c1ac8fa26
Prabhulingappa A S <pras0004@inc02rpb1ug8wp.synchronoss.net>
2019-02-06 15:07:59 +0530

    SIP-5893: Added Integration tests

--- dist/src/test/java/com/synchronoss/saw/ServicesIT.java
+++ dist/src/test/java/com/synchronoss/saw/ServicesIT.java
@@ -1,12 +1,14 @@
 package com.synchronoss.saw;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
 import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 /**
  * Services integration tests.  Tests health checks.
@@ -24,5 +26,43 @@ public class ServicesIT extends BaseIT {
     given(spec).accept("application/json")
         .when().get("/services/actuator/health")
         .then().statusCode(200).body("status", equalTo("UP"));
+  }
+
+  @Test
+  public void testAddDocument() {
+    ObjectNode root = mapper.createObjectNode();
+    root.put("source", "testValue");
+    given(authSpec)
+      .contentType(ContentType.JSON)
+      .body(root)
+      .when().post("/services/internal/proxy/storage/product-module/bda_pc_bt::app_rating")
+      .then().assertThat().statusCode(200)
+      .body("valid", equalTo(true));
+  }
+
+  @Test
+  public void testUpdateDocument() {
+    ObjectNode root = mapper.createObjectNode();
+    root.put("source", "testValue123");
+    given(authSpec)
+      .contentType(ContentType.JSON)
+      .body(root)
+      .when().put("/services/internal/proxy/storage/product-module/bda_pc_bt::app_rating")
+      .then().assertThat().statusCode(200)
+      .body("valid", equalTo(true));
+  }
+
+  @Test
+  public void testGetDocument() {
+    given(authSpec)
+      .when().get("/services/internal/proxy/storage/product-module/bda_pc_bt::app_rating")
+      .then().assertThat().statusCode(200);
+  }
+
+  @Test
+  public void testDeleteDocument() {
+    given(authSpec)
+      .when().delete("/services/internal/proxy/storage/product-module/bda_pc_bt::app_rating")
+      .then().assertThat().statusCode(200);
   }
 }
