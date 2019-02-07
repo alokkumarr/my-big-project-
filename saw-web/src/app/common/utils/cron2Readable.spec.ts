ea8425e5b6c25a0295653921d6e14c47a1c2f493
Anirudh Venkatesh <Anirudh.NV@Synchronoss.com>
2019-02-07 21:22:18 +0900

    SIP-5766: Fixing breaking unit tests for cron readable file

--- saw-web/src/app/common/utils/cron2Readable.spec.ts
+++ saw-web/src/app/common/utils/cron2Readable.spec.ts
@@ -1,19 +1,20 @@
 import { generateSchedule } from './cron2Readable';
-import * as moment from 'moment';
+import * as moment from 'moment-timezone';
+import cronstrue from 'cronstrue';
 
 const hourlyCron = {
   cronExpression: '0 02 0/1 1/1 * ? *',
   activeTab: 'hourly',
-  timezone: 'Asia/Calcutta'
+  timezone: 'Etc/GMT'
 };
 
 const nonHourlyCron = {
-  cronExpression: '0 30 23 ? * MON *',
+  cronExpression: '0 30 03 ? * MON *',
   activeTab: 'weeklybasis',
-  timezone: 'Asia/Calcutta'
+  timezone: 'Etc/GMT'
 };
 
-const timezone = 'Asia/Calcutta';
+const timezone = 'Etc/GMT';
 
 describe('Cron to Readable (cron2Readable.ts)', () => {
   it('should have generateSchedule function', () => {
@@ -33,7 +34,6 @@ describe('Cron to Readable (cron2Readable.ts)', () => {
       minutes: parseInt(minutes, 10)
     };
     const localTime = moment(momentInput)
-      .local()
       .format(timeFormat);
     expect(generateSchedule(cronExpression, activeTab, timezone)).toEqual(
       `At ${localTime} minutes past the hour`
@@ -42,18 +42,14 @@ describe('Cron to Readable (cron2Readable.ts)', () => {
 
   it('should generate non-hourly schedule for local time', () => {
     // the time format used by cronstrue library
-    const timeFormat = 'hh:mm A';
     const { cronExpression, activeTab, timezone } = nonHourlyCron;
-    const [, minutes, hours] = cronExpression.split(' ');
-    const momentInput = {
-      hours: parseInt(hours, 10),
-      minutes: parseInt(minutes, 10)
-    };
-    const localTime = moment(momentInput)
-      .local()
-      .format(timeFormat);
+    const splitArray = cronExpression.split(' ');
+    const timeInLocal = moment.tz(`${splitArray[1]} ${splitArray[2]}`, 'mm HH', timezone || 'Etc/GMT').toDate();
+    const extractMinuteHour = moment(timeInLocal).format('mm HH').split(' ');
+    splitArray[1] = extractMinuteHour[0];
+    splitArray[2] = extractMinuteHour[1];
     expect(generateSchedule(cronExpression, activeTab, timezone)).toEqual(
-      `At ${localTime}, only on Monday`
+      cronstrue.toString(splitArray.join(' '))
     );
   });
 });
