01ef5ccc7d139fb06b300241d03fb553830672ff
Prabhulingappa A S <pras0004@inc02rpb1ug8wp.synchronoss.net>
2019-02-07 15:46:26 +0530

    SIP-5893: Indentation fix

--- bda-meta/meta-api/src/main/java/sncr/bda/store/generic/schema/Category.java
+++ bda-meta/meta-api/src/main/java/sncr/bda/store/generic/schema/Category.java
@@ -1,53 +1,52 @@
 package sncr.bda.store.generic.schema;
 
+import java.util.HashMap;
+import java.util.Map;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public enum Category {
-    ServicesIT.java
-    DataSet("DataSet"),
-    Transformation("Transformation"),
-    DataPod("DataPod"),
-    DataSegment("DataSegment"),
-    PortalDataset("PortalDataset"),
-    Project("Project"),
-    AuditLog("AuditLog"),
-    StorageProxy("StorageProxy"),
-    Semantic("Semantic");
-    private final String value;
-    private final static Map<String, Category> CONSTANTS = new HashMap<String, Category>();
+  
+  DataSet("DataSet"),
+  Transformation("Transformation"),
+  DataPod("DataPod"),
+  DataSegment("DataSegment"),
+  PortalDataset("PortalDataset"),
+  Project("Project"),
+  AuditLog("AuditLog"),
+  StorageProxy("StorageProxy"),
+  Semantic("Semantic");  
+private final String value;
+private final static Map<String, Category> CONSTANTS = new HashMap<String, Category>();
 
-    static {
-        for (Category c: values()) {
-            CONSTANTS.put(c.value, c);
-        }
-    }
+static {
+for (Category c: values()) {
+CONSTANTS.put(c.value, c);
+}
+}
 
-    private Category(String value) {
-        this.value = value;
-    }
+private Category(String value) {
+this.value = value;
+}
 
-    @Override
-    public String toString() {
-        return this.value;
-    }
+@Override
+public String toString() {
+return this.value;
+}
 
-    @JsonValue
-    public String value() {
-        return this.value;
-    }
+@JsonValue
+public String value() {
+return this.value;
+}
 
-    @JsonCreator
-    public static Category fromValue(String value) {
-        Category constant = CONSTANTS.get(value);
-        if (constant == null) {
-            throw new IllegalArgumentException(value);
-        } else {
-            return constant;
-        }
-    }
+@JsonCreator
+public static Category fromValue(String value) {
+Category constant = CONSTANTS.get(value);
+if (constant == null) {
+throw new IllegalArgumentException(value);
+} else {
+return constant;
+}
+}
 
 }
