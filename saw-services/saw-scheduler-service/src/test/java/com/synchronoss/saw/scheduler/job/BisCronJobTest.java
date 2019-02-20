e14ab72962899f24ca9fc42289a3bda942786d4c
ngan0002 <Naresh.Gangishetty@synchronoss.com>
2019-02-20 10:04:36 -0500

    SIP-6043: updated as per source code change

--- saw-services/saw-scheduler-service/src/test/java/com/synchronoss/saw/scheduler/job/BisCronJobTest.java
+++ saw-services/saw-scheduler-service/src/test/java/com/synchronoss/saw/scheduler/job/BisCronJobTest.java
@@ -71,7 +71,7 @@ public class BisCronJobTest {
       verify(jobDetail).getJobDataMap();
       verify(jobDataMap).get("JOB_DATA_MAP");
       //verify(context).getMergedJobDataMap();
-      verify(jobDataMap).getString("myKey");
+     // verify(jobDataMap).getString("myKey");
     } catch (JobExecutionException e) {
       logger.trace("Exception occured " + e);
     }
