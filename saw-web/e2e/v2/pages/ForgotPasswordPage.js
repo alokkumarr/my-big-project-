5482c54c0ac3c44dad9ec42406e16ad82df26287
Akshatha G <akshatha.g@synchronoss.com>
2019-02-28 07:39:41 +0000

    SIP-6125: Changes suggested in the pull request are implemented

--- saw-web/e2e/v2/pages/ForgotPasswordPage.js
+++ saw-web/e2e/v2/pages/ForgotPasswordPage.js
@@ -7,10 +7,6 @@ const users = require('../helpers/data-generation/users');
 class ForgotPasswordPage {
 
   constructor() {
-    // this._userName = element(by.css(`[e2e='username-input']`));
-    // this._password = element(by.css(`[e2e='password-input']`));
-    // this._loginButton = element(by.css(`[e2e='login-btn']`));
-    // this._errorMessage = element(by.css(`[e2e='error-msg-section']`));
     this._ForgotPassword = element(by.css(`[e2e="forgot-password"]`));
   }
 
