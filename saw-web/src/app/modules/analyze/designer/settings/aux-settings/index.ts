d9709177dd8804f2083b75c68520119307e7562d
Barna Mumtyan <Barna.Mumtya@synchronoss.com>
2019-03-07 16:08:08 +0200

    SIP-5920: Cleaned up some unused components.

--- saw-web/src/app/modules/analyze/designer/settings/aux-settings/index.ts
+++
@@ -1,6 +0,0 @@
-export {
-  DesignerSettingsAuxComponent
-} from './designer-settings-aux.component';
-export { DesignerSettingsAuxMapChartComponent } from './map-chart';
-
-export { DesignerSettingsAuxChartComponent } from './chart';
--- saw-web/src/app/modules/analyze/designer/settings/index.ts
+++ saw-web/src/app/modules/analyze/designer/settings/index.ts
@@ -1,18 +1,7 @@
 import { DesignerSettingsSingleTableComponent } from './single';
 import { DesignerSettingsMultiTableComponent } from './multi';
-import { DesignerSettingsGroupComponent } from './group';
 import { DesignerSettingsQueryComponent } from './query';
 import { DesignerSelectedFieldsComponent } from './selected-fields';
-import {
-  ExpandableFieldComponent,
-  ExpandDetailPivotComponent,
-  ExpandDetailChartComponent
-} from './expandable-field';
-import {
-  DesignerSettingsAuxComponent,
-  DesignerSettingsAuxChartComponent,
-  DesignerSettingsAuxMapChartComponent
-} from './aux-settings';
 import {
   DesignerAnalysisOptionsComponent,
   DesignerDataOptionFieldComponent,
@@ -30,13 +19,6 @@ export {
   DesignerSettingsMultiTableComponent,
   DesignerSettingsQueryComponent,
   DesignerSelectedFieldsComponent,
-  DesignerSettingsGroupComponent,
-  DesignerSettingsAuxComponent,
-  DesignerSettingsAuxChartComponent,
-  DesignerSettingsAuxMapChartComponent,
-  ExpandableFieldComponent,
-  ExpandDetailPivotComponent,
-  ExpandDetailChartComponent,
   DesignerAnalysisOptionsComponent,
   DesignerDataOptionFieldComponent,
   DesignerDateFormatSelectorComponent,
