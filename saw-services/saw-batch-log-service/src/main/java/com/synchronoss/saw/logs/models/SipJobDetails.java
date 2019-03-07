e935a23c51cda5ecf1e8e92ef97875c3acdc41d3
ngan0002 <Naresh.Gangishetty@synchronoss.com>
2019-03-07 12:12:09 -0500

    SIP-6269 updated for checkstyle

--- saw-services/saw-batch-log-service/src/main/java/com/synchronoss/saw/logs/models/SipJobDetails.java
+++ saw-services/saw-batch-log-service/src/main/java/com/synchronoss/saw/logs/models/SipJobDetails.java
@@ -3,13 +3,13 @@ package com.synchronoss.saw.logs.models;
 import java.io.Serializable;
 import java.util.Date;
 
-public class SipJobDetails implements Serializable{
-  
+public class SipJobDetails implements Serializable {
+
   /**
-   * serialization id
+   * serialization id.
    */
   private static final long serialVersionUID = -3249526955895533260L;
-  
+
   private Long jobId;
   private String jobName;
   private Date startTime;
@@ -23,84 +23,109 @@ public class SipJobDetails implements Serializable{
   private String createdBy;
   private Date updatedDate;
   private String updatedBy;
-  
+
   public Long getJobId() {
     return jobId;
   }
+
   public void setJobId(Long jobId) {
     this.jobId = jobId;
   }
+
   public String getJobName() {
     return jobName;
   }
+
   public void setJobName(String jobName) {
     this.jobName = jobName;
   }
+
   public Date getStartTime() {
     return startTime;
   }
+
   public void setStartTime(Date startTime) {
     this.startTime = startTime;
   }
+
   public Date getEndTime() {
     return endTime;
   }
+
   public void setEndTime(Date endTime) {
     this.endTime = endTime;
   }
+
   public String getJobStatus() {
     return jobStatus;
   }
+
   public void setJobStatus(String jobStatus) {
     this.jobStatus = jobStatus;
   }
+
   public long getTotalCount() {
     return totalCount;
   }
+
   public void setTotalCount(long totalCount) {
     this.totalCount = totalCount;
   }
+
   public long getSuccessCount() {
     return successCount;
   }
+
   public void setSuccessCount(long successCount) {
     this.successCount = successCount;
   }
+
   public String getFilePattern() {
     return filePattern;
   }
+
   public void setFilePattern(String filePattern) {
     this.filePattern = filePattern;
   }
+
   public String getJobType() {
     return jobType;
   }
+
   public void setJobType(String jobType) {
     this.jobType = jobType;
   }
+
   public Date getCreatedDate() {
     return createdDate;
   }
+
   public void setCreatedDate(Date createdDate) {
     this.createdDate = createdDate;
   }
+
   public String getCreatedBy() {
     return createdBy;
   }
+
   public void setCreatedBy(String createdBy) {
     this.createdBy = createdBy;
   }
+
   public Date getUpdatedDate() {
     return updatedDate;
   }
+
   public void setUpdatedDate(Date updatedDate) {
     this.updatedDate = updatedDate;
   }
+
   public String getUpdatedBy() {
     return updatedBy;
   }
+
   public void setUpdatedBy(String updatedBy) {
     this.updatedBy = updatedBy;
   }
-  
+
 }
--- saw-services/saw-batch-log-service/src/main/java/com/synchronoss/saw/logs/repository/SipJobDataRepository.java
+++ saw-services/saw-batch-log-service/src/main/java/com/synchronoss/saw/logs/repository/SipJobDataRepository.java
@@ -1,10 +1,12 @@
 package com.synchronoss.saw.logs.repository;
 
+import com.synchronoss.saw.logs.entities.SipJobEntity;
+
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.synchronoss.saw.logs.entities.SipJobEntity;
+
 
 
 public interface SipJobDataRepository 
