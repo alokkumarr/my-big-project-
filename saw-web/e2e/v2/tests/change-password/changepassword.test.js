dc3bd05ca775f0a8148633699edaa05eeb5c9972
draj0008 <draj0008@inc02r88chg8wp.wisor.com>
2019-03-12 19:43:51 +0530

    SIP-6015: Updated script as per review comments

--- saw-web/e2e/v2/tests/change-password/changepassword.test.js
+++ saw-web/e2e/v2/tests/change-password/changepassword.test.js
@@ -6,9 +6,9 @@ const LoginPage = require('../../pages/LoginPage');
 const ChangePassword = require('../../pages/ChangePasswordPage');
 const Header = require('../../pages/components/Header');
 const commonFunctions = require('../../pages/utils/commonFunctions')
+const users = require('../../helpers/data-generation/users');
 
 describe('Executing login tests from ChangePasswd.test.js', () => {
-
   beforeAll(() => {
     logger.info('Starting login tests...');
     jasmine.DEFAULT_TIMEOUT_INTERVAL = protractorConf.timeouts.timeoutInterval;
@@ -28,11 +28,8 @@ describe('Executing login tests from ChangePasswd.test.js', () => {
     }, protractorConf.timeouts.pageResolveTimeout);
   });
 
-  // Negative Test Cases for Change password
-
-    using(testDataReader.testData['CHANGEPWD']['negativeTests'] ? testDataReader.testData['CHANGEPWD']['negativeTests'] : {}, (data, id) => {
+  using(testDataReader.testData['CHANGEPWD']['negativeTests'] ? testDataReader.testData['CHANGEPWD']['negativeTests'] : {}, (data, id) => {
         it(`${id}:${data.description}`, () => {
-
         let loginPage = new LoginPage();
         loginPage.loginAs(data.user, /analyze/);
         let header = new Header();
@@ -46,26 +43,21 @@ describe('Executing login tests from ChangePasswd.test.js', () => {
       }).result.testInfo = { testId: id, data: data, feature: 'CHANGEPWD', dataProvider: 'negativeTests' };
   });
 
-  // Positive Test Cases for Change password
-
   using(testDataReader.testData['CHANGEPWD']['positiveTests'] ? testDataReader.testData['CHANGEPWD']['positiveTests'] : {}, (data, id) => {
-    it(`${id}:${data.description}`, () => {
-
-    let loginPage = new LoginPage();
-    loginPage.loginAs(data.user, /analyze/);
-    let header = new Header();
-    header.verifyLogo();
-    header.doChangePassword();
-    let changePwd = new ChangePassword();
-    changePwd.doChangePwd(data.password,data.newpassword,data.confirmpwd)
-    changePwd.clickOnChangeButton();
-    changePwd.verifyError(data.expected.message);
-    header.verifyChangePassword();
-    changePwd.doLogin(data.user,data.newpassword);
-    header.verifyLogo();
-    header.doLogout();
-
+      it(`${id}:${data.description}`, () => {
+      let loginPage = new LoginPage();
+      loginPage.loginAs(data.user, /analyze/);
+      let header = new Header();
+      header.verifyLogo();
+      header.doChangePassword();
+      let changePwd = new ChangePassword();
+      changePwd.doChangePwd(data.password,data.newpassword,data.confirmpwd)
+      changePwd.clickOnChangeButton();
+      changePwd.verifyError(data.expected.message);
+      header.verifyChangePassword();
+      loginPage.doLogin(users[data.user].loginId,data.newpassword);
+      header.verifyLogo();
+      header.doLogout();
   }).result.testInfo = { testId: id, data: data, feature: 'CHANGEPWD', dataProvider: 'positiveTests' };
 });
-
 });
