64bd680b8b99dec7d2e367f5db5983474314aab3
Akash Agrawal <Akash.Agarwal@synchronoss.com>
2019-03-12 16:23:35 +0530

    SIP-6073: Calculate grid height in js for route log to get around safari issue

--- saw-web/src/app/modules/workbench/components/datasource-management/logs-dialog/logs-dialog.component.ts
+++ saw-web/src/app/modules/workbench/components/datasource-management/logs-dialog/logs-dialog.component.ts
@@ -1,14 +1,13 @@
-
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { DatasourceService } from '../../../services/datasource.service';
 import { generateSchedule } from '../../../../../common/utils/cron2Readable';
 
 interface ILogObject {
-  pid: string; /* don't show */
-  routeSysId: number; /* don't show */
-  channelSysId: number; /* don't show */
-  channelType: string; /* don't show */
+  pid: string /* don't show */;
+  routeSysId: number /* don't show */;
+  channelSysId: number /* don't show */;
+  channelType: string /* don't show */;
   filePattern: string;
   fileName: string;
   actualFileRecDate: number;
@@ -25,7 +24,6 @@ interface ILogObject {
   styleUrls: ['./logs-dialog.component.scss']
 })
 export class LogsDialogComponent implements OnInit {
-
   public logs: ILogObject[];
   public lastFireTime: number;
   public nextFireTime: number;
@@ -38,11 +36,13 @@ export class LogsDialogComponent implements OnInit {
 
   ngOnInit() {
     const { bisChannelSysId, bisRouteSysId } = this.routeData;
-    this._datasourceService.getRoutesLogs(bisChannelSysId, bisRouteSysId).subscribe(resp => {
-      this.logs = resp.logs;
-      this.lastFireTime = resp.lastFireTime;
-      this.nextFireTime = resp.nextFireTime;
-    });
+    this._datasourceService
+      .getRoutesLogs(bisChannelSysId, bisRouteSysId)
+      .subscribe(resp => {
+        this.logs = resp.logs;
+        this.lastFireTime = resp.lastFireTime;
+        this.nextFireTime = resp.nextFireTime;
+      });
   }
 
   close() {
@@ -54,4 +54,18 @@ export class LogsDialogComponent implements OnInit {
     return generateSchedule(cronexp, activeTab, timezone);
   }
 
+  /**
+   * Returns height of logs grid calculated from container.
+   * This is necessary because in safari, the implementation of CSS's calc
+   * is buggy.
+   *
+   * @returns {number}
+   * @memberof LogsDialogComponent
+   */
+  customGridHeight(): number {
+    const container = document.getElementById(
+      'route-log__dialog-content-container'
+    );
+    return container.scrollHeight - container.children[0].scrollHeight;
+  }
 }
