f3dbb8c429b412236abd2dcfc67eb7bce7c9c77d
ngan0002 <Naresh.Gangishetty@synchronoss.com>
2019-03-16 16:42:07 -0400

    SIP-6300: Removed unused import

--- saw-services/saw-batch-plugin-sftp/src/main/java/com/synchronoss/saw/batch/plugin/controllers/SftpServiceImpl.java
+++ saw-services/saw-batch-plugin-sftp/src/main/java/com/synchronoss/saw/batch/plugin/controllers/SftpServiceImpl.java
@@ -55,7 +55,6 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.mortbay.log.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
