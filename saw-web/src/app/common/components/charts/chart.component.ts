392080eb067dca32b63ce793251b5b53c58b1853
Barna Mumtyan <Barna.Mumtya@synchronoss.com>
2019-03-20 10:51:21 +0200

    SIP-5920: Fixed an issue with charts in designer not taking full width.

--- saw-web/src/app/common/components/charts/chart.component.ts
+++ saw-web/src/app/common/components/charts/chart.component.ts
@@ -37,7 +37,7 @@ export const UPDATE_PATHS = {
   // tslint:disable-next-line:component-selector
   selector: 'chart',
   template: `
-    <div #container></div>
+    <div style="width: 100%;" #container></div>
   `
 })
 export class ChartComponent implements OnInit, AfterViewInit, OnDestroy {
@@ -86,6 +86,9 @@ export class ChartComponent implements OnInit, AfterViewInit, OnDestroy {
     if (this.enableExport) {
       this.enableExporting(this.config);
     }
+    setTimeout(() => {
+      this.chart.reflow();
+    });
   }
 
   ngOnInit() {
