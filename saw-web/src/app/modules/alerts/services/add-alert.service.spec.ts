a77900830ef62dfb0b47a1e370038b3d5988474e
Akhilesh Obilineni <Akhilesh.Obilineni@synchronoss.com>
2019-04-09 13:32:48 -0400

    SIP-6489: Fix for unit test

--- saw-web/src/app/modules/alerts/services/add-alert.service.spec.ts
+++ saw-web/src/app/modules/alerts/services/add-alert.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { AddAlertService } from './add-alert.service';
 
 describe('AddAlertService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    })
+  );
 
   it('should be created', () => {
     const service: AddAlertService = TestBed.get(AddAlertService);
