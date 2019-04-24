1c85d3045b7008b627fc556ba4229244b0229ef8
Akhilesh Obilineni <Akhilesh.Obilineni@synchronoss.com>
2019-04-24 17:21:33 -0400

    SIP-6943: Display alert states in time order

---
+++ saw-web/src/app/modules/alerts/components/alerts-view/alerts-grid/alerts-grid.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'alerts-grid',
+  templateUrl: './alerts-grid.component.html',
+  styleUrls: ['./alerts-grid.component.scss']
+})
+export class AlertsGridComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
