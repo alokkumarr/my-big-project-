d665066a635fb13ca0138c76a9f34786341386ff
Barna Mumtyan <Barna.Mumtya@synchronoss.com>
2019-05-08 19:20:34 +0300

    Downloading dashboard with map now uses mapbox static api.

--- sip-web/src/app/common/map-box/map-box.component.ts
+++ sip-web/src/app/common/map-box/map-box.component.ts
@@ -1,6 +1,13 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  HostBinding,
+  ElementRef
+} from '@angular/core';
 import map from 'lodash/map';
 import first from 'lodash/first';
+import last from 'lodash/last';
 import split from 'lodash/split';
 import reduce from 'lodash/reduce';
 import get from 'lodash/get';
@@ -8,24 +15,8 @@ import fpPipe from 'lodash/fp/pipe';
 import fpMap from 'lodash/fp/map';
 import fpFilter from 'lodash/fp/filter';
 import fpToPairs from 'lodash/fp/toPairs';
+import { environment } from '../../../environments/environment';
 import { MarkerDataPoint } from './types';
-import { AGGREGATE_TYPES_OBJ } from '../../common/consts';
-
-interface GeoFeature {
-  index: number;
-  type: 'Feature';
-  properties: {
-    aggregates: any[];
-  };
-  geometry: {
-    type: 'Point';
-    coordinates: number[];
-  };
-}
-interface GeoJson {
-  type: 'FeatureCollection';
-  features: GeoFeature[];
-}
 
 @Component({
   selector: 'map-box',
@@ -33,12 +24,13 @@ interface GeoJson {
   styleUrls: ['./map-box.component.scss']
 })
 export class MapBoxComponent implements OnChanges {
-  public selectedPoint: GeoFeature;
+  public selectedPoint: GeoJSON.Feature<GeoJSON.Point>;
   dataFields: any[];
   coordinateField: any;
   center: number[];
+  zoom = 6;
   mapStyle: string;
-  geoJson: GeoJson;
+  geoJson: GeoJSON.GeoJSON;
 
   @Input('mapSettings') set setMapSettings(settings) {
     this.mapStyle = settings.mapStyle;
@@ -51,7 +43,9 @@ export class MapBoxComponent implements OnChanges {
 
   @Input() data: any[];
 
-  constructor() {
+  @HostBinding('attr.data-image-url') imageUrl: string;
+
+  constructor(private _elemRef: ElementRef) {
     // preserveDrawingBuffer has to be set to true so that when downloading the dashboard,
     // so that when using canvas.toDataURL() it still has the data preserved
     HTMLCanvasElement.prototype.getContext = (function(origFn) {
@@ -66,17 +60,32 @@ export class MapBoxComponent implements OnChanges {
     })(HTMLCanvasElement.prototype.getContext);
   }
 
-  ngOnChanges() {
+  ngOnChanges(changes) {
     if (this.data && this.coordinateField && this.dataFields) {
       setTimeout(() => {
         this.setGeoJson(this.data, this.coordinateField, this.dataFields);
-        if (this.selectedPoint) {
-          this.selectedPoint = {
-            ...this.geoJson.features[this.selectedPoint.index]
-          };
-        }
+        setTimeout(() => {
+          this.setImageUrl();
+        }, 10);
       }, 10);
     }
+  }
+
+  setImageUrl() {
+    const token = environment.mapbox.accessToken;
+    const canvas = this._elemRef.nativeElement.querySelector(
+      '.mapboxgl-canvas'
+    );
+    const { height, width } = canvas.style;
+    const size = `${parseInt(width, 10)}x${parseInt(height, 10)}`;
+    // console.log('attributeStyleMap', canvas[0]);
+    const [lng, lat] = this.center;
+    const style = last(split(this.mapStyle, '/'));
+    const base = 'https://api.mapbox.com/styles/v1/mapbox';
+    const url = `${base}/${style}/static/${lng},${lat},${
+      this.zoom
+    },0,0/${size}?access_token=${token}`;
+    this.imageUrl = url;
   }
 
   setGeoJson(data, coordinateField, dataFields) {
@@ -104,7 +113,11 @@ export class MapBoxComponent implements OnChanges {
     this.selectedPoint = { ...point };
   }
 
-  data2geoJsonFeatures(data, coordinateField, dataFields): Array<GeoFeature> {
+  data2geoJsonFeatures(
+    data,
+    coordinateField,
+    dataFields
+  ): Array<GeoJSON.Feature> {
     const allFields = [coordinateField, ...dataFields];
     const fieldsMap = reduce(
       allFields,
@@ -114,7 +127,7 @@ export class MapBoxComponent implements OnChanges {
       },
       {}
     );
-    return map(data, (datum, index) => {
+    return map(data, datum => {
       const coordinatesKey = coordinateField.columnName;
       const [lng, lat] = split(datum[coordinatesKey], ',');
       const lnglat = [parseFloat(lng), parseFloat(lat)];
@@ -122,21 +135,18 @@ export class MapBoxComponent implements OnChanges {
         fpToPairs,
         fpFilter(([key]) => key !== coordinatesKey),
         fpMap(([key, value]) => {
-          const { alias, aliasName, displayName, aggregate } = get(
-            fieldsMap,
-            key
-          );
-          const aggregateFun = AGGREGATE_TYPES_OBJ[aggregate].designerLabel;
-          return {
+          const alias = get(fieldsMap, `${key}.alias`);
+          const displayName = get(fieldsMap, `${key}.displayName`);
+          const aggregate: MarkerDataPoint = {
             key,
             value,
-            label: `${aggregateFun}(${alias || aliasName || displayName})`
+            label: alias || displayName
           };
+          return aggregate;
         })
       )(datum);
 
       return {
-        index,
         type: 'Feature',
         properties: {
           aggregates
