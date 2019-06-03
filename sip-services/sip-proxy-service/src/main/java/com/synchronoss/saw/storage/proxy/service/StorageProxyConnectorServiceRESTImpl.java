13421bb721d2d99bd3f29df3f6f2378a10b0d6ab
Pradeep Tripathi <pradeep.manitripathi@synchronoss.com>
2019-06-03 12:18:33 +0530

    SIP-6813: Added elasticsearch Support for client-server communication encryption.

--- sip-services/sip-proxy-service/src/main/java/com/synchronoss/saw/storage/proxy/service/StorageProxyConnectorServiceRESTImpl.java
+++ sip-services/sip-proxy-service/src/main/java/com/synchronoss/saw/storage/proxy/service/StorageProxyConnectorServiceRESTImpl.java
@@ -1,7 +1,31 @@
 package com.synchronoss.saw.storage.proxy.service;
 
 import static java.util.Collections.emptyMap;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.base.Preconditions;
+import com.synchronoss.saw.model.Store;
+import com.synchronoss.saw.storage.proxy.model.StorageProxy;
+import com.synchronoss.saw.storage.proxy.model.StorageProxy.Action;
+import com.synchronoss.saw.storage.proxy.model.StorageProxy.Storage;
+import com.synchronoss.saw.storage.proxy.model.StoreField;
+import com.synchronoss.saw.storage.proxy.model.response.ClusterAliasesResponse;
+import com.synchronoss.saw.storage.proxy.model.response.ClusterIndexResponse;
+import com.synchronoss.saw.storage.proxy.model.response.CountESResponse;
+import com.synchronoss.saw.storage.proxy.model.response.CreateAndDeleteESResponse;
+import com.synchronoss.saw.storage.proxy.model.response.SearchESResponse;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.KeyStore;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -11,9 +35,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import javax.net.ssl.SSLContext;
 import javax.validation.constraints.NotNull;
-
-import com.synchronoss.saw.model.Store;
 import org.apache.hadoop.fs.Path;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
@@ -27,8 +50,9 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.apache.http.nio.entity.NStringEntity;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.SSLContexts;
 import org.apache.http.util.EntityUtils;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
@@ -37,23 +61,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.google.common.base.Preconditions;
-import com.synchronoss.saw.storage.proxy.model.StorageProxy;
-import com.synchronoss.saw.storage.proxy.model.StorageProxy.Action;
-import com.synchronoss.saw.storage.proxy.model.StorageProxy.Storage;
-import com.synchronoss.saw.storage.proxy.model.StoreField;
-import com.synchronoss.saw.storage.proxy.model.response.ClusterAliasesResponse;
-import com.synchronoss.saw.storage.proxy.model.response.ClusterIndexResponse;
-import com.synchronoss.saw.storage.proxy.model.response.CountESResponse;
-import com.synchronoss.saw.storage.proxy.model.response.CreateAndDeleteESResponse;
-import com.synchronoss.saw.storage.proxy.model.response.SearchESResponse;
 
 @Service
 public class StorageProxyConnectorServiceRESTImpl implements StorageConnectorService {
@@ -77,14 +84,23 @@ public class StorageProxyConnectorServiceRESTImpl implements StorageConnectorSer
   @NotNull
   private String[] hosts;
 
+  @Value("${elastic-search.sslEnabled}")
+  private Boolean esSslEnabled;
+
+  @Value("${elastic-search.keyStorePath}")
+  private String keyStorePath;
+
+  @Value("${elastic-search.storePassword}")
+  private String storePassword;
+
   @Value("${elastic-search.transport-ports}")
   @NotNull
   private String[] ports;
-  
+
   private final String SEARCH = "_search";
   private final String COUNT = "_count";
-  
-  
+
+
   @Override
   public SearchESResponse<?> searchDocuments(String query, StorageProxy proxyDetails) throws Exception {
     Preconditions.checkArgument(query != null && !"".equals(query), "query cannnot be null.");
@@ -92,7 +108,7 @@ public class StorageProxyConnectorServiceRESTImpl implements StorageConnectorSer
     Response response = null;
     RestClient client = null;
     SearchESResponse<?> searchResponse = null;
-    String endpoint = proxyDetails.getObjectType()!=null? proxyDetails.getIndexName() + "/" + proxyDetails.getObjectType() + "/" + SEARCH : 
+    String endpoint = proxyDetails.getObjectType()!=null? proxyDetails.getIndexName() + "/" + proxyDetails.getObjectType() + "/" + SEARCH :
       proxyDetails.getIndexName()  + "/" + SEARCH;
     try{
         HttpEntity requestPaylod = new NStringEntity(query, ContentType.APPLICATION_JSON);
@@ -143,7 +159,7 @@ public class StorageProxyConnectorServiceRESTImpl implements StorageConnectorSer
         return jsonNode;
     }
 
- 
+
   @Override
   public CreateAndDeleteESResponse deleteDocumentById(String id, StorageProxy proxyDetails) throws Exception {
     Preconditions.checkArgument(id != null && !"".equals(id), "Entity Id to be deleted cannnot be null.");
@@ -227,8 +243,8 @@ public class StorageProxyConnectorServiceRESTImpl implements StorageConnectorSer
     logger.trace("Count Response", countResponse.toString());
     return countResponse;
   }
-  
-  
+
+
   @Override
   public List<ClusterIndexResponse> catClusterIndices(StorageProxy proxyDetails) throws Exception {
     Response response = null;
@@ -276,7 +292,7 @@ public class StorageProxyConnectorServiceRESTImpl implements StorageConnectorSer
     logger.trace("Cluster Index Response", catClusterIndexResponse.toString());
     return catClusterIndexResponse;
   }
-  
+
   @Override
   public List<StoreField> getMappingbyIndex(StorageProxy proxyDetails) throws Exception {
     Response response = null;
@@ -287,7 +303,7 @@ public class StorageProxyConnectorServiceRESTImpl implements StorageConnectorSer
     final String BUCKETS = "buckets";
     final String PROPERTIES = "properties";
     final String KEY = "key";
-    
+
     Preconditions.checkNotNull(proxyDetails.getIndexName(), "Index Name cannot be null");
     String endpoint = proxyDetails.getIndexName() + "/" + "_mappings";
     try{
@@ -433,7 +449,12 @@ public class StorageProxyConnectorServiceRESTImpl implements StorageConnectorSer
     HttpHost [] httpHosts = new HttpHost[hosts.length];
     HttpHost httpHost = null;
     for (int i=0; i<hosts.length;i++){
-      httpHost = new HttpHost(hosts[i], Integer.parseInt(ports[i]), "http"); 
+      if (esSslEnabled) {
+        httpHost = new HttpHost(hosts[i], Integer.parseInt(ports[i]), "https");
+        }
+      else {
+          httpHost = new HttpHost(hosts[i], Integer.parseInt(ports[i]), "http");
+      }
       httpHosts[i] = httpHost;
     }
     return httpHosts;
@@ -445,14 +466,24 @@ public class StorageProxyConnectorServiceRESTImpl implements StorageConnectorSer
         final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
         credentialsProvider.setCredentials(AuthScope.ANY,
                 new UsernamePasswordCredentials(username, password));
-        restClient = RestClient.builder(prepareHostAddresses(hosts, ports))
-                .setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider)).setRequestConfigCallback(new RestClientBuilder.RequestConfigCallback() {
-                  @Override
-                  public RequestConfig.Builder customizeRequestConfig(RequestConfig.Builder requestConfigBuilder) {
-                      return requestConfigBuilder.setConnectTimeout(5000)
-                              .setSocketTimeout(60000);
-                  }
-              }).setMaxRetryTimeoutMillis(60000)
+      if (esSslEnabled) {
+          return prepareRESTESConnectionwithSsl(credentialsProvider);
+      }
+        restClient =
+            RestClient.builder(prepareHostAddresses(hosts, ports))
+                .setHttpClientConfigCallback(
+                    httpClientBuilder ->
+                        httpClientBuilder
+                            .setDefaultCredentialsProvider(credentialsProvider))
+                .setRequestConfigCallback(
+                    new RestClientBuilder.RequestConfigCallback() {
+                      @Override
+                      public RequestConfig.Builder customizeRequestConfig(
+                          RequestConfig.Builder requestConfigBuilder) {
+                        return requestConfigBuilder.setConnectTimeout(5000).setSocketTimeout(60000);
+                      }
+                    })
+                .setMaxRetryTimeoutMillis(60000)
                 .build();
       } else {
         restClient = RestClient.builder(prepareHostAddresses(hosts, ports))
@@ -463,17 +494,46 @@ public class StorageProxyConnectorServiceRESTImpl implements StorageConnectorSer
       }
       return restClient;
   }
-  
-  
- 
-  
+
+  private RestClient prepareRESTESConnectionwithSsl(CredentialsProvider credentialsProvider)
+      throws Exception {
+    KeyStore truststore = KeyStore.getInstance("jks");
+    File file = new File(keyStorePath);
+
+    try (InputStream is = Files.newInputStream(Paths.get(file.toURI()))) {
+      truststore.load(is, storePassword.toCharArray());
+    }
+    SSLContextBuilder sslBuilder = SSLContexts.custom().loadTrustMaterial(truststore, null);
+
+    final SSLContext sslContext = sslBuilder.build();
+      RestClient restClient =
+        RestClient.builder(prepareHostAddresses(hosts, ports))
+            .setHttpClientConfigCallback(
+                httpClientBuilder ->
+                    httpClientBuilder
+                        .setDefaultCredentialsProvider(credentialsProvider)
+                        .setSSLContext(sslContext))
+            .setRequestConfigCallback(
+                new RestClientBuilder.RequestConfigCallback() {
+                  @Override
+                  public RequestConfig.Builder customizeRequestConfig(
+                      RequestConfig.Builder requestConfigBuilder) {
+                    return requestConfigBuilder.setConnectTimeout(5000).setSocketTimeout(60000);
+                  }
+                })
+            .setMaxRetryTimeoutMillis(60000)
+            .build();
+
+    return restClient;
+  }
+
   public static void main(String[] args) throws IOException {
     //withOutXPACK();
 /*    String query = "{\"query\":{\"bool\":{\"must\":[{\"match\":{\"SOURCE_OS.keyword\":{\"query\":\"android\",\"operator\":\"AND\",\"analyzer\":\"standard\",\"prefix_length\":0,\"max_expansions\":50,\"fuzzy_transpositions\":false,\"lenient\":false,\"zero_terms_query\":\"ALL\",\"boost\":1.0}}},{\"match\":{\"TARGET_MANUFACTURER.keyword\":{\"query\":\"motorola\",\"operator\":\"AND\",\"analyzer\":\"standard\",\"prefix_length\":0,\"max_expansions\":50,\"fuzzy_transpositions\":false,\"lenient\":false,\"zero_terms_query\":\"ALL\",\"boost\":1.0}}}],\"disable_coord\":false,\"adjust_pure_negative\":true,\"boost\":1.0}},\"sort\":[{\"TRANSFER_DATE\":{\"order\":\"asc\"}}],\"aggregations\":{\"node_field_1\":{\"date_histogram\":{\"field\":\"TRANSFER_DATE\",\"format\":\"MMM YYYY\",\"interval\":\"1M\",\"offset\":0,\"order\":{\"_key\":\"desc\"},\"keyed\":false,\"min_doc_count\":0},\"aggregations\":{\"AVAILABLE_ITEMS\":{\"sum\":{\"field\":\"AVAILABLE_ITEMS\",\"from\":1,\"size\":1,}}}}}}";
     System.out.println(query.contains("size"));
     System.out.println(query.contains("from"));
-    
-    String regEx=".*?(size|from).*?(\\d+).*?(from|size).*?(\\d+)";   
+
+    String regEx=".*?(size|from).*?(\\d+).*?(from|size).*?(\\d+)";
 
     Pattern p = Pattern.compile(regEx, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
     Matcher m = p.matcher(query);
@@ -488,16 +548,16 @@ public class StorageProxyConnectorServiceRESTImpl implements StorageConnectorSer
     ObjectMapper objectMapper = new ObjectMapper();
     objectMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
     objectMapper.enable(DeserializationFeature.FAIL_ON_READING_DUP_TREE_KEY);
-  
-  String mappingString = "{\"mct_tmo_session\":{\"mappings\":{\"session\":{\"dynamic\":\"strict\",\"properties\":{\"APP_KEY\":{\"type\":\"text\",\"fields\":{\"keyword\":{\"type\":\"keyword\"}}},\"AVAILABLE_BYTES\":{\"type\":\"double\"}}}}}}"; 
+
+  String mappingString = "{\"mct_tmo_session\":{\"mappings\":{\"session\":{\"dynamic\":\"strict\",\"properties\":{\"APP_KEY\":{\"type\":\"text\",\"fields\":{\"keyword\":{\"type\":\"keyword\"}}},\"AVAILABLE_BYTES\":{\"type\":\"double\"}}}}}}";
   String result ="{\"took\":7,\"timed_out\":false,\"_shards\":{\"total\":5,\"successful\":5,\"skipped\":0,\"failed\":0},\"hits\":{\"total\":538335,\"max_score\":0,\"hits\":[]},\"aggregations\":{\"typeAgg\":{\"doc_count_error_upper_bound\":0,\"sum_other_doc_count\":0,\"buckets\":[{\"key\":\"session\",\"doc_count\":538335}]}}}";
-  
+
   JsonNode mappingNode  = objectMapper.readTree(mappingString);
   ObjectNode rootNode = (ObjectNode) mappingNode;
   ObjectNode mappingDataNode = (ObjectNode)rootNode.get("mct_tmo_session").get("mappings");
 
   System.out.println(mappingDataNode);
-  
+
   JsonNode aggregationNode  = objectMapper.readTree(result);
   ArrayNode bucketNode = (ArrayNode) aggregationNode.get("aggregations").get("typeAgg").get("buckets");
   List<String> typesOfIndex = new ArrayList<String>();
@@ -511,19 +571,19 @@ public class StorageProxyConnectorServiceRESTImpl implements StorageConnectorSer
      typeNode  = (ObjectNode)mappingDataNode.get(typeName).get("properties");
      System.out.println(typeNode);
   }
-  
+
   Map<?,?> objectDefination = new HashMap<String, String>();
   objectDefination = objectMapper.readValue(objectMapper.writeValueAsBytes(typeNode), HashMap.class);
-  
+
   Iterator<?> itr =   objectDefination.keySet().iterator();
-  
+
   while (itr.hasNext()) {
     String attributeName = itr.next().toString();
     System.out.println(attributeName);
     System.out.println(objectDefination.get(attributeName));
     StoreField storeField = objectMapper.readValue(objectMapper.writeValueAsString(objectDefination.get(attributeName)),StoreField.class);
     storeField.setName(attributeName);
-    System.out.println(storeField.toString()); 
+    System.out.println(storeField.toString());
   }
 }
    private static void withOutXPACK()throws IOException
@@ -538,14 +598,14 @@ public class StorageProxyConnectorServiceRESTImpl implements StorageConnectorSer
          })
          .setMaxRetryTimeoutMillis(60000)
          .build();
-     
+
      //final HttpEntity payload = new  NStringEntity("{\"size\":1,\"query\":{\"bool\":{\"must\":[{\"match\":{\"SOURCE_OS.keyword\":{\"query\":\"android\",\"operator\":\"AND\",\"analyzer\":\"standard\",\"prefix_length\":0,\"max_expansions\":50,\"fuzzy_transpositions\":false,\"lenient\":false,\"zero_terms_query\":\"ALL\",\"boost\":1.0}}},{\"match\":{\"TARGET_MANUFACTURER.keyword\":{\"query\":\"motorola\",\"operator\":\"AND\",\"analyzer\":\"standard\",\"prefix_length\":0,\"max_expansions\":50,\"fuzzy_transpositions\":false,\"lenient\":false,\"zero_terms_query\":\"ALL\",\"boost\":1.0}}}],\"disable_coord\":false,\"adjust_pure_negative\":true,\"boost\":1.0}},\"sort\":[{\"TRANSFER_DATE\":{\"order\":\"asc\"}}],\"aggregations\":{\"node_field_1\":{\"date_histogram\":{\"field\":\"TRANSFER_DATE\",\"format\":\"MMM YYYY\",\"interval\":\"1M\",\"offset\":0,\"order\":{\"_key\":\"desc\"},\"keyed\":false,\"min_doc_count\":0},\"aggregations\":{\"AVAILABLE_ITEMS\":{\"sum\":{\"field\":\"AVAILABLE_ITEMS\"}}}}}}",ContentType.APPLICATION_JSON);
      //final HttpEntity payload = new  NStringEntity("",ContentType.APPLICATION_JSON);
      //final HttpEntity payload = new  NStringEntity("{\"city\":\"Baltimore\"}",ContentType.APPLICATION_JSON);
     // final Response response = restClient.performRequest(HttpPost.METHOD_NAME, "/mct_tmo_session/session/_search", emptyMap(), payload);
-    
+
      final Response response = restClient.performRequest(HttpGet.METHOD_NAME, "/mct_tmo_session/_mappings/");
-     
+
      //Response response = restClient.performRequest(HttpDelete.METHOD_NAME, "lower/lowerCase/AWEQIWg3jV2L1EGZ4Mac", emptyMap());
      ObjectMapper objectMapper = new ObjectMapper();
      objectMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
@@ -563,33 +623,33 @@ public class StorageProxyConnectorServiceRESTImpl implements StorageConnectorSer
      String query = "{\"from\":5,\"query\":{\"bool\":{\"must\":[{\"match\":{\"SOURCE_OS.keyword\":{\"query\":\"android\",\"operator\":\"AND\",\"analyzer\":\"standard\",\"prefix_length\":0,\"max_expansions\":50,\"fuzzy_transpositions\":false,\"lenient\":false,\"zero_terms_query\":\"ALL\",\"boost\":1.0}}},{\"match\":{\"TARGET_MANUFACTURER.keyword\":{\"query\":\"motorola\",\"operator\":\"AND\",\"analyzer\":\"standard\",\"prefix_length\":0,\"max_expansions\":50,\"fuzzy_transpositions\":false,\"lenient\":false,\"zero_terms_query\":\"ALL\",\"boost\":1.0}}}],\"disable_coord\":false,\"adjust_pure_negative\":true,\"boost\":1.0}},\"sort\":[{\"TRANSFER_DATE\":{\"order\":\"asc\"}}],\"aggregations\":{\"node_field_1\":{\"date_histogram\":{\"field\":\"TRANSFER_DATE\",\"format\":\"MMM YYYY\",\"interval\":\"1M\",\"offset\":0,\"order\":{\"_key\":\"desc\"},\"keyed\":false,\"min_doc_count\":0},\"aggregations\":{\"AVAILABLE_ITEMS\":{\"sum\":{\"field\":\"AVAILABLE_ITEMS\",\"size\":1}}}}}}";
      System.out.println(query.contains("size"));
      System.out.println(query.contains("from"));
-     
-     String re1=".*?";  
-     
-     String re2="(size)";    
-     String re3=".*?";   
-     String re4="(\\d+)";    
-     String re5=".*?";   
-     
-     String re6="(from)";    
-     String re7=".*?";   
-     String re8="(\\d+)";    
-     
- 
-     String re9=".*?";  
-     String re10="(from)";   
-     String re11=".*?";   
-     String re12="(\\d+)";    
-     String re13=".*?";   
-     String re14="(size)";    
-     String re15=".*?";   
-     String re16="(\\d+)";    
-     
+
+     String re1=".*?";
+
+     String re2="(size)";
+     String re3=".*?";
+     String re4="(\\d+)";
+     String re5=".*?";
+
+     String re6="(from)";
+     String re7=".*?";
+     String re8="(\\d+)";
+
+
+     String re9=".*?";
+     String re10="(from)";
+     String re11=".*?";
+     String re12="(\\d+)";
+     String re13=".*?";
+     String re14="(size)";
+     String re15=".*?";
+     String re16="(\\d+)";
+
 
      Pattern p = Pattern.compile("["+re1+re2+re3+re4+re5+re6+re7+re8+"|"+re9+re10+re11+re12+re13+re14+re15+re16+"]", Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.COMMENTS | Pattern.MULTILINE );
      Matcher matcher = p.matcher(query);
-     
-     
+
+
      if (matcher.find())
      {
        String word1=matcher.group(1);
@@ -603,10 +663,10 @@ public class StorageProxyConnectorServiceRESTImpl implements StorageConnectorSer
 
 
 
-  
-  
- 
-  
+
+
+
+
 }
 
 
