956c8fc638acc2a85d2e45f462e803cb2db1639d
Anirudh Venkatesh <Anirudh.NV@Synchronoss.com>
2019-06-06 17:48:08 +0530

    SIP-7370: fixing unit tests

--- sip-web/src/app/common/components/chart-grid/chart-grid.component.spec.ts
+++ sip-web/src/app/common/components/chart-grid/chart-grid.component.spec.ts
@@ -10,6 +10,7 @@ import { BehaviorSubject } from 'rxjs';
 import { ChartGridComponent } from './chart-grid.component';
 import { UChartModule } from '../../components/charts';
 import { ChartService } from '../../services';
+import { HeaderProgressService } from './../../../common/services';
 
 @Component({
   selector: 'dx-data-grid',
@@ -42,6 +43,10 @@ class ChartStubService {
   getChartConfigFor = () => ({});
 }
 
+class HeaderProgressStubService {
+  show = () => (true);
+}
+
 describe('Chart Grid Component', () => {
   let fixture: ComponentFixture<ChartGridComponent>;
   let component;
@@ -49,7 +54,8 @@ describe('Chart Grid Component', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [UChartModule],
-      providers: [{ provide: ChartService, useClass: ChartStubService }],
+      providers: [{ provide: ChartService, useClass: ChartStubService },
+        { provide: HeaderProgressService, useClass: HeaderProgressStubService }],
       declarations: [ChartGridComponent, DxDataGridStubComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
