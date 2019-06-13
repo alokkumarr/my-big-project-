ef43b2152adf14afed622f499f8b0dc56cc426c5
Prabhulingappa A S <Prabhulingappa.AS@synchronoss.com>
2019-06-13 07:32:14 +0000

    SIP-7386: Set up data moved to DslIT

--- dist/src/test/java/com/synchronoss/saw/BaseIT.java
+++ dist/src/test/java/com/synchronoss/saw/BaseIT.java
@@ -11,16 +11,13 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-
 import java.util.UUID;
 import java.util.regex.Pattern;
-
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
@@ -49,8 +46,6 @@ public class BaseIT {
   protected RequestSpecification authSpec;
   protected ObjectMapper mapper;
   protected String token;
-  protected JsonObject testData;
-  protected JsonObject sipQuery = null;
 
   @BeforeClass
   public static void setUpClass() {
@@ -66,168 +61,6 @@ public class BaseIT {
     RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
   }
 
-  /** Set up data for All DSL queries. */
-  public void setUpData() {
-    testData = new JsonObject();
-    testData.addProperty("type", "chart");
-    testData.addProperty("type", "chart");
-    testData.addProperty("semanticId", "d23c6142-2c10-459e-b1f6-29edd1b2ccfe");
-    testData.addProperty("id", "f37cde24-b833-46ba-ae2d-42e286c3fc39");
-    testData.addProperty("customerCode", "SYNCHRONOSS");
-    testData.addProperty("projectCode", "sip-sample");
-    testData.addProperty("module", "productSpecific/ANALYZE");
-    testData.addProperty("createdTime", 1543921879);
-    testData.addProperty("createdBy", "sipadmin@synchronoss.com");
-    testData.addProperty("modifiedTime", 1543921879);
-    testData.addProperty("modifiedBy", "sipadmin@synchronoss.com");
-    testData.addProperty("category","5");
-    testData.addProperty("userId",1);
-
-    sipQuery = new JsonObject();
-
-    JsonObject artifact1 = new JsonObject();
-    artifact1.addProperty("artifactName", "sample");
-
-    JsonObject field1 = new JsonObject();
-    field1.addProperty("dataField", "string");
-    field1.addProperty("area", "x-axis");
-    field1.addProperty("alias", "String");
-    field1.addProperty("columnName", "string.keyword");
-    field1.addProperty("displayName", "String");
-    field1.addProperty("type", "string");
-    JsonArray artifactFields = new JsonArray();
-    artifactFields.add(field1);
-
-    JsonObject field2 = new JsonObject();
-    field2.addProperty("dataField", "integer");
-    field2.addProperty("area", "y-axis");
-    field2.addProperty("columnName", "integer");
-    field2.addProperty("displayName", "integer");
-    field2.addProperty("type", "integer");
-    field2.addProperty("aggregate", "avg");
-    artifactFields.add(field2);
-
-    JsonObject field3 = new JsonObject();
-    field3.addProperty("dataField", "long");
-    field3.addProperty("area", "z-axis");
-    field3.addProperty("columnName", "long");
-    field3.addProperty("displayName", "Long");
-    field3.addProperty("type", "long");
-    field3.addProperty("aggregate", "avg");
-    artifactFields.add(field3);
-
-    JsonObject field4 = new JsonObject();
-    field4.addProperty("dataField", "date");
-    field4.addProperty("area", "g-axis");
-    field4.addProperty("columnName", "date");
-    field4.addProperty("displayName", "Date");
-    field4.addProperty("type", "date");
-    field4.addProperty("groupInterval", "year");
-    artifactFields.add(field4);
-
-    JsonObject field5 = new JsonObject();
-    field5.addProperty("dataField", "double");
-    field5.addProperty("area", "m-axis");
-    field5.addProperty("columnName", "double");
-    field5.addProperty("displayName", "Double");
-    field5.addProperty("type", "double");
-    field5.addProperty("aggregate", "avg");
-    artifactFields.add(field5);
-
-    JsonObject field6 = new JsonObject();
-    field6.addProperty("dataField", "float");
-    field6.addProperty("area", "m-axis");
-    field6.addProperty("columnName", "float");
-    field6.addProperty("displayName", "Float");
-    field6.addProperty("type", "double");
-    field6.addProperty("aggregate", "avg");
-    artifactFields.add(field6);
-
-    artifact1.add("fields", artifactFields);
-    JsonArray artifacts = new JsonArray();
-    artifacts.add(artifact1);
-    sipQuery.add("artifacts", artifacts);
-    sipQuery.addProperty("booleanCriteria", "AND");
-
-    JsonObject filter1 = new JsonObject();
-    filter1.addProperty("type", "long");
-    filter1.addProperty("artifactsName", "sample");
-    filter1.addProperty("isOptional", false);
-    filter1.addProperty("columnName", "long");
-    filter1.addProperty("isRuntimeFilter", false);
-    filter1.addProperty("isGlobalFilter", false);
-    JsonObject model = new JsonObject();
-    model.addProperty("operator", "EQ");
-    model.addProperty("value", 1000);
-    filter1.add("model", model);
-    JsonArray filters = new JsonArray();
-    filters.add(filter1);
-
-    JsonObject filter2 = new JsonObject();
-    filter2.addProperty("type", "date");
-    filter2.addProperty("tableName", "sample");
-    filter2.addProperty("isOptional", false);
-    filter2.addProperty("columnName", "date");
-    filter2.addProperty("isRuntimeFilter", false);
-    filter2.addProperty("isGlobalFilter", false);
-    model = new JsonObject();
-    model.addProperty("format", "epoch_second");
-    model.addProperty("operator", "NEQ");
-    model.addProperty("value", 1483228800000L);
-    model.addProperty("otherValue", 1483228800000L);
-    filter2.add("model", model);
-    filters.add(filter2);
-
-    JsonObject filter3 = new JsonObject();
-    filter3.addProperty("type", "date");
-    filter3.addProperty("tableName", "sample");
-    filter3.addProperty("isOptional", false);
-    filter3.addProperty("columnName", "date");
-    filter3.addProperty("isRuntimeFilter", false);
-    filter3.addProperty("isGlobalFilter", false);
-    model = new JsonObject();
-    model.addProperty("format", "epoch_millis");
-    model.addProperty("operator", "BTW");
-    model.addProperty("value", 1451606400000L);
-    model.addProperty("otherValue", 1551606400000L);
-    filter3.add("model", model);
-    filters.add(filter3);
-
-    JsonObject filter4 = new JsonObject();
-    filter4.addProperty("type", "date");
-    filter4.addProperty("tableName", "sample");
-    filter4.addProperty("isOptional", false);
-    filter4.addProperty("columnName", "date");
-    filter4.addProperty("isRuntimeFilter", false);
-    filter4.addProperty("isGlobalFilter", false);
-    model = new JsonObject();
-    model.addProperty("format", "YYYY");
-    model.addProperty("operator", "GTE");
-    model.addProperty("value", 2017);
-    filter4.add("model", model);
-    filters.add(filter4);
-
-    sipQuery.add("filters", filters);
-
-    JsonObject sort1 = new JsonObject();
-    sort1.addProperty("artifacts", "sample");
-    sort1.addProperty("aggregate", "sum");
-    sort1.addProperty("columnName", "long");
-    sort1.addProperty("type", "long");
-    sort1.addProperty("order", "asc");
-    JsonArray sorts = new JsonArray();
-    sorts.add(sort1);
-
-    sipQuery.add("sorts", sorts);
-
-    JsonObject store = new JsonObject();
-    store.addProperty("dataStore", "sampleAlias/sample");
-    store.addProperty("storageType", "ES");
-
-    sipQuery.add("store", store);
-    testData.add("sipQuery", sipQuery);
-  }
-
   @Before
   public void setUp() throws JsonProcessingException {
     mapper = new ObjectMapper();
@@ -240,7 +73,6 @@ public class BaseIT {
             .addFilter(documentationConfiguration(restDocumentation))
             .build()
             .header("Authorization", "Bearer " + token);
-    setUpData();
   }
 
   private static final String TEST_USERNAME = "sawadmin@synchronoss.com";
--- dist/src/test/java/com/synchronoss/saw/SipDslIT.java
+++ dist/src/test/java/com/synchronoss/saw/SipDslIT.java
@@ -6,13 +6,180 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import io.restassured.response.Response;
 import java.io.IOException;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SipDslIT extends BaseIT {
   String analysisId;
+  protected JsonObject testData = null;
+  protected JsonObject sipQuery = null;
+
+  @Before
+  public void setUpData() {
+    testData = new JsonObject();
+    testData.addProperty("type", "chart");
+    testData.addProperty("type", "chart");
+    testData.addProperty("semanticId", "d23c6142-2c10-459e-b1f6-29edd1b2ccfe");
+    testData.addProperty("id", "f37cde24-b833-46ba-ae2d-42e286c3fc39");
+    testData.addProperty("customerCode", "SYNCHRONOSS");
+    testData.addProperty("projectCode", "sip-sample");
+    testData.addProperty("module", "productSpecific/ANALYZE");
+    testData.addProperty("createdTime", 1543921879);
+    testData.addProperty("createdBy", "sipadmin@synchronoss.com");
+    testData.addProperty("modifiedTime", 1543921879);
+    testData.addProperty("modifiedBy", "sipadmin@synchronoss.com");
+    testData.addProperty("category", "5");
+    testData.addProperty("userId", 1);
+
+    sipQuery = new JsonObject();
+
+    JsonObject artifact1 = new JsonObject();
+    artifact1.addProperty("artifactName", "sample");
+
+    JsonObject field1 = new JsonObject();
+    field1.addProperty("dataField", "string");
+    field1.addProperty("area", "x-axis");
+    field1.addProperty("alias", "String");
+    field1.addProperty("columnName", "string.keyword");
+    field1.addProperty("displayName", "String");
+    field1.addProperty("type", "string");
+    JsonArray artifactFields = new JsonArray();
+    artifactFields.add(field1);
+
+    JsonObject field2 = new JsonObject();
+    field2.addProperty("dataField", "integer");
+    field2.addProperty("area", "y-axis");
+    field2.addProperty("columnName", "integer");
+    field2.addProperty("displayName", "integer");
+    field2.addProperty("type", "integer");
+    field2.addProperty("aggregate", "avg");
+    artifactFields.add(field2);
+
+    JsonObject field3 = new JsonObject();
+    field3.addProperty("dataField", "long");
+    field3.addProperty("area", "z-axis");
+    field3.addProperty("columnName", "long");
+    field3.addProperty("displayName", "Long");
+    field3.addProperty("type", "long");
+    field3.addProperty("aggregate", "avg");
+    artifactFields.add(field3);
+
+    JsonObject field4 = new JsonObject();
+    field4.addProperty("dataField", "date");
+    field4.addProperty("area", "g-axis");
+    field4.addProperty("columnName", "date");
+    field4.addProperty("displayName", "Date");
+    field4.addProperty("type", "date");
+    field4.addProperty("groupInterval", "year");
+    artifactFields.add(field4);
+
+    JsonObject field5 = new JsonObject();
+    field5.addProperty("dataField", "double");
+    field5.addProperty("area", "m-axis");
+    field5.addProperty("columnName", "double");
+    field5.addProperty("displayName", "Double");
+    field5.addProperty("type", "double");
+    field5.addProperty("aggregate", "avg");
+    artifactFields.add(field5);
+
+    JsonObject field6 = new JsonObject();
+    field6.addProperty("dataField", "float");
+    field6.addProperty("area", "m-axis");
+    field6.addProperty("columnName", "float");
+    field6.addProperty("displayName", "Float");
+    field6.addProperty("type", "double");
+    field6.addProperty("aggregate", "avg");
+    artifactFields.add(field6);
+
+    artifact1.add("fields", artifactFields);
+    JsonArray artifacts = new JsonArray();
+    artifacts.add(artifact1);
+    sipQuery.add("artifacts", artifacts);
+    sipQuery.addProperty("booleanCriteria", "AND");
+
+    JsonObject filter1 = new JsonObject();
+    filter1.addProperty("type", "long");
+    filter1.addProperty("artifactsName", "sample");
+    filter1.addProperty("isOptional", false);
+    filter1.addProperty("columnName", "long");
+    filter1.addProperty("isRuntimeFilter", false);
+    filter1.addProperty("isGlobalFilter", false);
+    JsonObject model = new JsonObject();
+    model.addProperty("operator", "EQ");
+    model.addProperty("value", 1000);
+    filter1.add("model", model);
+    JsonArray filters = new JsonArray();
+    filters.add(filter1);
+
+    JsonObject filter2 = new JsonObject();
+    filter2.addProperty("type", "date");
+    filter2.addProperty("tableName", "sample");
+    filter2.addProperty("isOptional", false);
+    filter2.addProperty("columnName", "date");
+    filter2.addProperty("isRuntimeFilter", false);
+    filter2.addProperty("isGlobalFilter", false);
+    model = new JsonObject();
+    model.addProperty("format", "epoch_second");
+    model.addProperty("operator", "NEQ");
+    model.addProperty("value", 1483228800000L);
+    model.addProperty("otherValue", 1483228800000L);
+    filter2.add("model", model);
+    filters.add(filter2);
+
+    JsonObject filter3 = new JsonObject();
+    filter3.addProperty("type", "date");
+    filter3.addProperty("tableName", "sample");
+    filter3.addProperty("isOptional", false);
+    filter3.addProperty("columnName", "date");
+    filter3.addProperty("isRuntimeFilter", false);
+    filter3.addProperty("isGlobalFilter", false);
+    model = new JsonObject();
+    model.addProperty("format", "epoch_millis");
+    model.addProperty("operator", "BTW");
+    model.addProperty("value", 1451606400000L);
+    model.addProperty("otherValue", 1551606400000L);
+    filter3.add("model", model);
+    filters.add(filter3);
+
+    JsonObject filter4 = new JsonObject();
+    filter4.addProperty("type", "date");
+    filter4.addProperty("tableName", "sample");
+    filter4.addProperty("isOptional", false);
+    filter4.addProperty("columnName", "date");
+    filter4.addProperty("isRuntimeFilter", false);
+    filter4.addProperty("isGlobalFilter", false);
+    model = new JsonObject();
+    model.addProperty("format", "YYYY");
+    model.addProperty("operator", "GTE");
+    model.addProperty("value", 2017);
+    filter4.add("model", model);
+    filters.add(filter4);
+
+    sipQuery.add("filters", filters);
+
+    JsonObject sort1 = new JsonObject();
+    sort1.addProperty("artifacts", "sample");
+    sort1.addProperty("aggregate", "sum");
+    sort1.addProperty("columnName", "long");
+    sort1.addProperty("type", "long");
+    sort1.addProperty("order", "asc");
+    JsonArray sorts = new JsonArray();
+    sorts.add(sort1);
+
+    sipQuery.add("sorts", sorts);
+
+    JsonObject store = new JsonObject();
+    store.addProperty("dataStore", "sampleAlias/sample");
+    store.addProperty("storageType", "ES");
+
+    sipQuery.add("store", store);
+    testData.add("sipQuery", sipQuery);
+  }
 
   @Test
   public void testSipDsl() throws IOException {
@@ -41,7 +208,6 @@ public class SipDslIT extends BaseIT {
   public void testSipDslExecute() throws IOException {
     ObjectMapper objectMapper = new ObjectMapper();
     JsonNode jsonNode = objectMapper.readTree(sipQuery.toString());
-    System.out.println("json Node : " + sipQuery.toString());
     String testStringFilter = "string 1";
     Response response =
         given(spec)
@@ -96,7 +262,6 @@ public class SipDslIT extends BaseIT {
   @Test
   public void testGetAnalysis() throws IOException {
     ObjectMapper objectMapper = new ObjectMapper();
-    JsonNode jsonNode = objectMapper.readTree(testData.toString());
     given(spec)
         .header("Authorization", "Bearer " + token)
         .get("/sip/services/dslanalysis/" + analysisId)
@@ -108,7 +273,6 @@ public class SipDslIT extends BaseIT {
   @Test
   public void testGetAnalysisByCategory() throws IOException {
     ObjectMapper objectMapper = new ObjectMapper();
-    JsonNode jsonNode = objectMapper.readTree(testData.toString());
     given(spec)
         .header("Authorization", "Bearer " + token)
         .get("/sip/services/dslanalysis/" + analysisId + "?category=5")
@@ -120,7 +284,6 @@ public class SipDslIT extends BaseIT {
   @Test
   public void testDeleteAnalysis() throws IOException {
     ObjectMapper objectMapper = new ObjectMapper();
-    JsonNode jsonNode = objectMapper.readTree(testData.toString());
     given(spec)
         .header("Authorization", "Bearer " + token)
         .delete("/sip/services/dslanalysis/" + analysisId)
@@ -132,7 +295,6 @@ public class SipDslIT extends BaseIT {
   @Test
   public void testListExecutions() throws IOException {
     ObjectMapper objectMapper = new ObjectMapper();
-    JsonNode jsonNode = objectMapper.readTree(testData.toString());
     given(spec)
         .header("Authorization", "Bearer " + token)
         .get("/sip/services/internal/proxy/storage/" + analysisId + "/executions")
@@ -144,7 +306,6 @@ public class SipDslIT extends BaseIT {
   @Test
   public void testLastExecutionsData() throws IOException {
     ObjectMapper objectMapper = new ObjectMapper();
-    JsonNode jsonNode = objectMapper.readTree(testData.toString());
     given(spec)
         .header("Authorization", "Bearer " + token)
         .get("/sip/services/internal/proxy/storage/" + analysisId + "/lastExecutions/data")
