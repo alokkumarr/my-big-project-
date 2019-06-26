7a0d66112ea7a3c5610cecb6b35a710f55262c58
Sharad Dubey <Sharad.Dubey@synchronoss.com>
2019-06-26 17:43:49 +0530

    View analysis page of ES report implemented.

--- sip-web/src/app/modules/analyze/executed-view/report/executed-report-view.component.ts
+++ sip-web/src/app/modules/analyze/executed-view/report/executed-report-view.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
-import { Analysis, Artifact } from '../../types';
+import { Analysis, Artifact, isDSLAnalysis } from '../../types';
 import * as get from 'lodash/get';
 @Component({
   selector: 'executed-report-view',
   templateUrl: 'executed-report-view.component.html'
 })
 export class ExecutedReportViewComponent {
+  public analysisSorts: any;
+
   @Input('analysis')
   set setAnalysis(analysis: Analysis) {
     this.analysis = analysis;
@@ -13,12 +15,21 @@ export class ExecutedReportViewComponent {
     // TODO use the columns from the query
     const isEsReport = analysis.type === 'esReport';
     const isInQueryMode = analysis.edit;
-    const dataFields = get(analysis, 'sqlBuilder.dataFields');
+    const dataFields = isDSLAnalysis(analysis)
+      ? get(analysis, 'sipQuery.artifacts')
+      : get(analysis, 'sqlBuilder.dataFields');
 
     if (isInQueryMode) {
       this.artifacts = null;
     } else if (isEsReport) {
-      const containsArtifacts = <any>dataFields[0].tableName;
+      let containsArtifacts: String;
+      isDSLAnalysis(analysis)
+        ? (containsArtifacts = <String>(
+            analysis.sipQuery.artifacts[0].artifactsName
+          ))
+        : (containsArtifacts = <String>dataFields[0].tableName);
+
+      //const containsArtifacts = <any>dataFields[0].tableName;
       if (containsArtifacts) {
         this.artifacts = dataFields;
       } else {
@@ -31,6 +42,9 @@ export class ExecutedReportViewComponent {
       // DL report
       this.artifacts = dataFields;
     }
+    this.analysisSorts = isDSLAnalysis(analysis)
+      ? analysis.sipQuery.sorts
+      : analysis.sqlBuilder.sorts || analysis.sqlBuilder.orderByColumns;
   }
   @Input()
   dataLoader: Function;
