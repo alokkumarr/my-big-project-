c029f0a971bfca3180eee05b58f928e74fd1c972
alok.kumarr <alok.kumarr@synchronoss.com>
2019-07-10 14:13:05 +0530

    SIP-7439- Updates for /execute api to return total count

--- sip-services/sip-proxy-service/src/main/java/com/synchronoss/saw/storage/proxy/model/ExecuteAnalysisResponse.java
+++ sip-services/sip-proxy-service/src/main/java/com/synchronoss/saw/storage/proxy/model/ExecuteAnalysisResponse.java
@@ -3,32 +3,46 @@ package com.synchronoss.saw.storage.proxy.model;
 public class ExecuteAnalysisResponse {
 
   Object data;
+  long totalRows;
   String executionId;
 
-	/**
-	 * Gets data.
-	 *
-	 * @return value of data
-	 */
+  /**
+   * Gets data.
+   *
+   * @return value of data
+   */
   public Object getData() {
     return data;
   }
 
-	/** Sets data. */
-	public void setData(Object data) {
-		this.data = data;
-	}
+  /** Sets data. */
+  public void setData(Object data) {
+    this.data = data;
+  }
 
-	/**
-	 * Gets executionId.
-	 *
-	 * @return value of executionId
-	 */
+  /**
+   * Gets totalRows.
+   *
+   * @return value of totalRows
+   */
+  public long getTotalRows() {
+    return totalRows;
+  }
+
+  /** Sets totalRows. */
+  public void setTotalRows(long totalRows) {
+    this.totalRows = totalRows;
+  }
+  /**
+   * Gets executionId.
+   *
+   * @return value of executionId
+   */
   public String getExecutionId() {
     return executionId;
   }
 
-	/** Sets executionId. */
+  /** Sets executionId. */
   public void setExecutionId(String executionId) {
     this.executionId = executionId;
   }
