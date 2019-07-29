2b61dcb03cf0f0a3e167df2fd648a09c0b0f8c1d
Prabhulingappa A S <Prabhulingappa.AS@synchronoss.com>
2019-07-29 19:31:30 +0530

    SIP-7349: Resolve merge conflicts

--- sip-services/sip-metadata-service/src/main/java/com/synchronoss/saw/util/HBaseUtils.java
+++ sip-services/sip-metadata-service/src/main/java/com/synchronoss/saw/util/HBaseUtils.java
@@ -11,47 +11,45 @@ import org.slf4j.LoggerFactory;
 
 public class HBaseUtils {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(HBaseUtils.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(HBaseUtils.class);
 
-	Connection connection = null;
+  Connection connection = null;
 
-	/**
-	 * Create hbase connection to read binary store data.
-	 *
-	 * @return connection
-	 */
-	public Connection getConnection() {
-		try {
-			connection = ConnectionFactory.createConnection();
-		} catch (IOException ex) {
-			LOGGER.info("Error occurred during hbase connection", ex);
-		}
-		return connection;
-	}
+  /**
+   * Create hbase connection to read binary store data.
+   *
+   * @return connection
+   */
+  public Connection getConnection() {
+    try {
+      connection = ConnectionFactory.createConnection();
+    } catch (IOException ex) {
+      LOGGER.info("Error occurred during hbase connection", ex);
+    }
+    return connection;
+  }
 
-	/**
-	 * Get the hbase table to perform operation on binary store.
-	 *
-	 * @param connection The connection for Hbase
-	 * @param tablePath  Table location for binary store
-	 * @return table Operation to be performed on the table
-	 * @throws IOException Throw the exception while get table
-	 */
-	public Table getTable(Connection connection, String tablePath) throws IOException {
-		TableName tn = TableName.valueOf(tablePath);
-		return connection.getTable(tn);
-	}
+  /**
+   * Get the hbase table to perform operation on binary store.
+   *
+   * @param connection The connection for Hbase
+   * @param tablePath Table location for binary store
+   * @return table Operation to be performed on the table
+   * @throws IOException Throw the exception while get table
+   */
+  public Table getTable(Connection connection, String tablePath) throws IOException {
+    TableName tn = TableName.valueOf(tablePath);
+    return connection.getTable(tn);
+  }
 
-	/**
-	 * Close the connection once all operation performed.
-	 */
-	public void closeConnection() {
-		try {
-			if (connection != null) {
-				connection.close();
-			}
-		} catch (IOException ex) {
-			LOGGER.error(ex.getMessage());
-		}
-	}
+  /** Close the connection once all operation performed. */
+  public void closeConnection() {
+    try {
+      if (connection != null) {
+        connection.close();
+      }
+    } catch (IOException ex) {
+      LOGGER.error(ex.getMessage());
+    }
+  }
 }
