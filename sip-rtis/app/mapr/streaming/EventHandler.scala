6aadd2cd39433d9a9264ee496cd05ee8893d6ce9
alok.kumarr <alok.kumarr@synchronoss.com>
2019-08-09 09:23:00 +0530

    SIP-8025-reload-app_key-automatically-to-rtis-context

--- sip-rtis/app/mapr/streaming/EventHandler.scala
+++ sip-rtis/app/mapr/streaming/EventHandler.scala
@@ -1,6 +1,8 @@
 package mapr.streaming
+
+import java.util
 import java.util.Map.Entry
-import java.util.Properties
+import java.util.{Properties}
 import java.util.concurrent.{ExecutionException, TimeoutException}
 
 import com.typesafe.config.{Config, ConfigFactory, ConfigValue}
@@ -17,10 +19,10 @@ import scala.util.Random
 object EventHandler {
   val KEY_STREAM_TOPIC_SELECTOR_METHOD = "stream.topic.selectormethod"
   val TOPIC_SELECTOR_ROUND_ROBIN = "roundrobin"
-  val TOPIC_SELECTOR_RANDOM      = "random"
+  val TOPIC_SELECTOR_RANDOM = "random"
 
-  private var streamFile : String = null
-  var streamWaitTime : Int = 0
+  private var streamFile: String = null
+  var streamWaitTime: Int = 0
 
   val debugMode = System.getProperty("processing.mode", "normal").equalsIgnoreCase("debug")
 
@@ -29,22 +31,21 @@ object EventHandler {
     */
   var topicSelectorFunction: Array[mutable.HashMap[String, String]] => Int = null
 
-  def getStreamFile : String = streamFile
+  def getStreamFile: String = streamFile
 
 
   def getAppKeys = eventHandlerStreams.keySet
 
 
-
   private val m_log: Logger = LoggerFactory.getLogger(classOf[EventHandler].getName)
   private[streaming] val eventHandlerProperties = new scala.collection.mutable.HashMap[String, Properties]
   private[streaming] val eventHandlerStreams = new scala.collection.mutable.HashMap
-      [String,
-             (Array[mutable.HashMap[String, String]],
-              Array[mutable.HashMap[String, String]])
+    [String,
+      (Array[mutable.HashMap[String, String]],
+        Array[mutable.HashMap[String, String]])
       ]
   protected[streaming] val eventSenders = new scala.collection.mutable.HashMap[String, EventSender]
-  private[streaming] var streamPointer : mutable.Map[String, String] = new mutable.HashMap[String, String]
+  private[streaming] var streamPointer: mutable.Map[String, String] = new mutable.HashMap[String, String]
 
   private val rand = new Random()
 
@@ -55,8 +56,8 @@ object EventHandler {
     * @param stream - A MapR Stream
     * @return Int - Randomly selected topic number
     */
-  def randomSelector (stream: Array[mutable.HashMap[String, String]]): Int = {
-    if(stream.length > 1) rand.nextInt(stream.length) else 0
+  def randomSelector(stream: Array[mutable.HashMap[String, String]]): Int = {
+    if (stream.length > 1) rand.nextInt(stream.length) else 0
   }
 
 
@@ -71,7 +72,7 @@ object EventHandler {
     */
   def roundRobinSelector(stream: Array[mutable.HashMap[String, String]]): Int = {
     val topicNumber = {
-      if(stream.length != 0)
+      if (stream.length != 0)
         (streamNumber + 1) % stream.length
       else 0
     }
@@ -106,15 +107,14 @@ object EventHandler {
     for (c <- mapr_conf) {
       var key: String = null
       val properties: Properties = new Properties
-      var p_list, s_list : Array[mutable.HashMap[String, String]] = null
-      for (e <- c.entrySet ) {
-        e.getKey match
-        {
+      var p_list, s_list: Array[mutable.HashMap[String, String]] = null
+      for (e <- c.entrySet) {
+        e.getKey match {
           case StreamHelper.primaryStreamsKey => {
             val v = generateStreamList(e, StreamHelper.primaryStreamsKey)
             p_list = v.get
             if (v == None || p_list.size == 0) throw new RTException(ErrorCodes ConfigurationIsNotCorrect, "Could not find primary streams!")
-            }
+          }
           case StreamHelper.secondaryStreamsKey => {
             val v = generateStreamList(e, StreamHelper.secondaryStreamsKey)
             s_list = v.get
@@ -130,32 +130,32 @@ object EventHandler {
       }
 
       key = properties.remove("app_key").asInstanceOf[String]
-      val clazz =  properties.get("class").asInstanceOf[String]
+      val clazz = properties.get("class").asInstanceOf[String]
       if (clazz == null || clazz.isEmpty) {
         m_log.error(String.format(ErrorCodes.getDescription(ErrorCodes.InvalidAppConf), "class"))
         throw new RTException(ErrorCodes.ConfigurationIsNotCorrect, clazz)
       }
       m_log.debug(s"Create property for class ${clazz}, and Application key: ${key}, # of entries: " + properties.size)
 
-      p_list.foreach( primary_queue => {
+      p_list.foreach(primary_queue => {
         val pCompositeKey = key +
-                            StreamHelper.keyConcat +
-                            primary_queue(StreamHelper.streamKey) +
-                            StreamHelper.keyConcat +
-                            primary_queue(StreamHelper.topicKey)
+          StreamHelper.keyConcat +
+          primary_queue(StreamHelper.streamKey) +
+          StreamHelper.keyConcat +
+          primary_queue(StreamHelper.topicKey)
 
-          m_log debug ("Add sender for primary stream: " + pCompositeKey)
-          eventSenders(pCompositeKey) = new EventSender(primary_queue(StreamHelper.streamKey), primary_queue(StreamHelper.topicKey), properties)
+        m_log debug ("Add sender for primary stream: " + pCompositeKey)
+        eventSenders(pCompositeKey) = new EventSender(primary_queue(StreamHelper.streamKey), primary_queue(StreamHelper.topicKey), properties)
       })
 
-      s_list.foreach( secondary_queue => {
+      s_list.foreach(secondary_queue => {
         val pCompositeKey = key +
-                            StreamHelper.keyConcat +
-                            secondary_queue(StreamHelper.streamKey) +
-                            StreamHelper.keyConcat + secondary_queue(StreamHelper.topicKey)
+          StreamHelper.keyConcat +
+          secondary_queue(StreamHelper.streamKey) +
+          StreamHelper.keyConcat + secondary_queue(StreamHelper.topicKey)
 
-          m_log debug ("Add sender for secondary stream: " + pCompositeKey)
-          eventSenders(pCompositeKey) = new EventSender(secondary_queue(StreamHelper.streamKey), secondary_queue(StreamHelper.topicKey), properties)
+        m_log debug ("Add sender for secondary stream: " + pCompositeKey)
+        eventSenders(pCompositeKey) = new EventSender(secondary_queue(StreamHelper.streamKey), secondary_queue(StreamHelper.topicKey), properties)
       })
       eventHandlerStreams(key) = (p_list, s_list)
       eventHandlerProperties(key) = properties
@@ -164,36 +164,36 @@ object EventHandler {
   }
 
   import scala.collection.JavaConversions._
-  private def generateStreamList(e: Entry[String, ConfigValue], key: String) : Option[Array[mutable.HashMap[String, String]]] =
-  {
-    val ps : java.util.List[_ <: Config] = e.getValue.atKey(key).getConfigList(key)
-    val res : Array[mutable.HashMap[String, String]] = ps.map( topic_queue_pair =>
+
+  private def generateStreamList(e: Entry[String, ConfigValue], key: String): Option[Array[mutable.HashMap[String, String]]] = {
+    val ps: java.util.List[_ <: Config] = e.getValue.atKey(key).getConfigList(key)
+    val res: Array[mutable.HashMap[String, String]] = ps.map(topic_queue_pair =>
       topic_queue_pair.entrySet().foldLeft(new mutable.HashMap[String, String])((m, qt_entry)
       => m += (qt_entry.getKey ->
           (if ((qt_entry.getValue == null || qt_entry.getValue.render.isEmpty)) ""
-          else (qt_entry.getValue.render.replaceAll("\"", ""))) ))
+          else (qt_entry.getValue.render.replaceAll("\"", "")))))
     ).toArray
-    m_log.debug(key + ": " + res .mkString("[", ",", "]"))
+    m_log.debug(key + ": " + res.mkString("[", ",", "]"))
     Option(res)
   }
 
 
   def getEventHandler[T <: EventHandler](key: String): T = {
     if (eventHandlerProperties == null) {
-      m_log.error(String.format(ErrorCodes.getDescription(ErrorCodes.InvalidAppConf), "mapping - missing section " + key ))
+      m_log.error(String.format(ErrorCodes.getDescription(ErrorCodes.InvalidAppConf), "mapping - missing section " + key))
       return null.asInstanceOf[T]
     }
 
     val properties = eventHandlerProperties.getOrDefault(key, null)
 
-    if(properties == null) {
-      m_log.error(String.format(ErrorCodes.getDescription(ErrorCodes.ConfigurationIsNotCorrect), key ))
+    if (properties == null) {
+      m_log.error(String.format(ErrorCodes.getDescription(ErrorCodes.ConfigurationIsNotCorrect), key))
 
       return null.asInstanceOf[T]
     }
 
     var eh: EventHandler = null
-    val clazz =  properties.get("class").asInstanceOf[String]
+    val clazz = properties.get("class").asInstanceOf[String]
     m_log.debug("Key Handler = " + clazz)
 
     try {
@@ -218,17 +218,131 @@ object EventHandler {
     eh.asInstanceOf[T]
   }
 
-}
+  def buildOnDemandEventHandlerList(key: String): Boolean = {
+    import scala.collection.JavaConversions._
 
+    var validKey: Boolean = false
+    val conf: Config = ConfigFactory.load
+    streamFile = conf.getString("stream.file.location")
+    streamWaitTime = conf.getInt("stream.send.timeout")
+
+    val topicSelectorMethod = conf.getString(EventHandler.KEY_STREAM_TOPIC_SELECTOR_METHOD)
+
+    topicSelectorFunction = topicSelectorMethod match {
+      case EventHandler.TOPIC_SELECTOR_ROUND_ROBIN => roundRobinSelector
+      case EventHandler.TOPIC_SELECTOR_RANDOM => randomSelector
+      case _ => randomSelector
+    }
+
+    if (streamFile == null || streamFile.isEmpty)
+      streamFile = "/tmp/frontend-server.stream.checkpoint"
+
+    // hard coded object, this should be a part of API
+    val url = "http://localhost:9400/exports/appKey/?key=" + key;
+    val mapr_cofg: List[mutable.HashMap[String, Any]] = RTISConfiguration.getConfig(url)
+    println(mapr_cofg)
+
+    // break the event handling processing if key mismatch
+    mapr_cofg.foreach(m => {
+      m.entrySet().foreach(c => {
+        if (c.getValue.equals(key)) {
+          validKey = c.getValue.equals(key)
+        }
+      })
+    })
+    if (!validKey) return validKey
+
+    for (c <- mapr_cofg) {
+      var key: String = null
+      val properties: Properties = new Properties
+      var p_list, s_list: Array[mutable.HashMap[String, String]] = null
+
+      for (e <- c.entrySet) {
+        e.getKey match {
+          case StreamHelper.primaryStreamsKey => {
+            if (e.getValue.isInstanceOf[List[mutable.HashMap[String, String]]]) {
+              val v = generateCacheStreamList(e, StreamHelper.primaryStreamsKey)
+              p_list = v.get
+              if (v == None || p_list.size == 0) throw new RTException(ErrorCodes ConfigurationIsNotCorrect, "Could not find primary streams!")
+            }
+          }
+          case StreamHelper.secondaryStreamsKey => {
+            if (e.getValue.isInstanceOf[List[mutable.HashMap[String, String]]]) {
+              val v = generateCacheStreamList(e, StreamHelper.secondaryStreamsKey)
+              s_list = v.get
+              if (v == None || s_list.size == 0) throw new RTException(ErrorCodes ConfigurationIsNotCorrect, "Could not find secondary (hot swap) streams!")
+            }
+          }
+          case _ => {
+            if (e.getValue.isInstanceOf[String]) {
+              val keyValue = e.getValue.asInstanceOf[String]
+              val value: String = if (keyValue == null || keyValue.isEmpty) ""
+              else (keyValue.replaceAll("\"", ""))
+              m_log.debug(e.getKey + ": " + value)
+              properties.put(e.getKey, value)
+            }
+          }
+        }
+      }
+
+      key = properties.remove("app_key").asInstanceOf[String]
+      val clazz = properties.get("class").asInstanceOf[String]
+      if (clazz == null || clazz.isEmpty) {
+        m_log.error(String.format(ErrorCodes.getDescription(ErrorCodes.InvalidAppConf), "class"))
+        throw new RTException(ErrorCodes.ConfigurationIsNotCorrect, clazz)
+      }
+      m_log.debug(s"Create property for class ${clazz}, and Application key: ${key}, # of entries: " + properties.size)
+
+      p_list.foreach(primary_queue => {
+        val pCompositeKey = key +
+          StreamHelper.keyConcat +
+          primary_queue(StreamHelper.streamKey) +
+          StreamHelper.keyConcat +
+          primary_queue(StreamHelper.topicKey)
+
+        m_log debug ("Add sender for primary stream: " + pCompositeKey)
+        eventSenders(pCompositeKey) = new EventSender(primary_queue(StreamHelper.streamKey), primary_queue(StreamHelper.topicKey), properties)
+      })
+
+      s_list.foreach(secondary_queue => {
+        val pCompositeKey = key +
+          StreamHelper.keyConcat +
+          secondary_queue(StreamHelper.streamKey) +
+          StreamHelper.keyConcat + secondary_queue(StreamHelper.topicKey)
+
+        m_log debug ("Add sender for secondary stream: " + pCompositeKey)
+        eventSenders(pCompositeKey) = new EventSender(secondary_queue(StreamHelper.streamKey), secondary_queue(StreamHelper.topicKey), properties)
+      })
+
+      eventHandlerStreams(key) = (p_list, s_list)
+      eventHandlerProperties(key) = properties
+      StreamHelper.loadActiveStreams
+    }
+    validKey
+  }
+
+  import scala.collection.JavaConversions._
+
+  private def generateCacheStreamList(e: util.Map.Entry[String, Any], key: String): Option[Array[mutable.HashMap[String, String]]] = {
+    val ps: List[_ <: mutable.HashMap[String, String]] = e.getValue.asInstanceOf[List[mutable.HashMap[String, String]]]
+    val res: Array[mutable.HashMap[String, String]] = ps.map(topic_queue_pair =>
+      topic_queue_pair.entrySet().foldLeft(new mutable.HashMap[String, String])((m, qt_entry)
+      => m += (qt_entry.getKey ->
+          (if ((qt_entry.getValue == null || qt_entry.getValue.isEmpty)) ""
+          else (qt_entry.getValue.replaceAll("\"", "")))))
+    ).toArray
+    m_log.debug(key + ": " + res.mkString("[", ",", "]"))
+    Option(res)
+  }
+}
 
 
 abstract class EventHandler {
 
-  var myKey : String = null
+  var myKey: String = null
 
   def setStreams(eventHandlerStreams:
-                 (Array[mutable.HashMap[String, String]],Array[mutable.HashMap[String, String]])) =
-  {
+                 (Array[mutable.HashMap[String, String]], Array[mutable.HashMap[String, String]])) = {
     maprStreams(StreamHelper.primaryStreamsKey) = eventHandlerStreams._1
     maprStreams(StreamHelper.secondaryStreamsKey) = eventHandlerStreams._2
   }
@@ -242,29 +356,27 @@ abstract class EventHandler {
     *
     * @param topicSelector - Function which is used to select a topic from the stream
     */
-  def selectEventSender(topicSelector: Array[mutable.HashMap[String, String]] => Int) =
-  {
+  def selectEventSender(topicSelector: Array[mutable.HashMap[String, String]] => Int) = {
     current_stream = EventHandler.streamPointer(myKey)
 
     val stream_topic_map = maprStreams(current_stream)(topicSelector(maprStreams(current_stream)))
 
-    val compositeKey : String = myKey +
+    val compositeKey: String = myKey +
       StreamHelper.keyConcat +
       stream_topic_map(StreamHelper.streamKey) +
       StreamHelper.keyConcat +
       stream_topic_map(StreamHelper.topicKey)
 
-    m_log trace ("Select event sender: " + compositeKey )
-    eventSender = EventHandler.eventSenders( compositeKey)
+    m_log trace ("Select event sender: " + compositeKey)
+    eventSender = EventHandler.eventSenders(compositeKey)
 
   }
 
-  def sendMessage(msg:Any, messageID : String) : Unit =
-  {
-      if (eventSender == null) selectEventSender(EventHandler.topicSelectorFunction)
-      m_log debug "Send message to queue: " + eventSender.queue
+  def sendMessage(msg: Any, messageID: String): Unit = {
+    if (eventSender == null) selectEventSender(EventHandler.topicSelectorFunction)
+    m_log debug "Send message to queue: " + eventSender.queue
 
-      val f: Future[Unit] =
+    val f: Future[Unit] =
       Future {
         try {
           msg match {
@@ -273,19 +385,20 @@ abstract class EventHandler {
             case _ => throw new RTException(ErrorCodes.UnsupportedMsgType, msg.getClass.getName)
           }
         }
-        catch{
+        catch {
           case e: RTException => Future.failed(e)
-          case e: InterruptedException => Future( new RTException(ErrorCodes.StreamStale, e))
-          case e: ExecutionException => Future( new RTException(ErrorCodes.StreamStale, e))
-          case e: TimeoutException => Future( new RTException(ErrorCodes.StreamStale, e))
+          case e: InterruptedException => Future(new RTException(ErrorCodes.StreamStale, e))
+          case e: ExecutionException => Future(new RTException(ErrorCodes.StreamStale, e))
+          case e: TimeoutException => Future(new RTException(ErrorCodes.StreamStale, e))
         }
-     }
-
-      f onSuccess{ case _ => m_log trace "Sent message successfully to queue: " + eventSender.queue }
-      f onFailure{
-        case ex => m_log error "Could not send message to queue: " + eventSender.queue +
-        " " + ex.getMessage; m_log.debug(ExceptionUtils.getStackTrace(ex))
       }
+
+    f onSuccess { case _ => m_log trace "Sent message successfully to queue: " + eventSender.queue }
+    f onFailure {
+      case ex => m_log error "Could not send message to queue: " + eventSender.queue +
+        " " + ex.getMessage;
+        m_log.debug(ExceptionUtils.getStackTrace(ex))
+    }
 
   }
 
@@ -306,20 +419,19 @@ abstract class EventHandler {
   private var current_stream: String = StreamHelper.primaryStreamsKey
 
 
-
   def createMessage(q: scala.collection.immutable.Map[String, Seq[String]]): Unit = {
     m_log trace "create message "
-    raw_data = q.foldLeft[JsObject](JsObject(Nil))((acc,kv)=>acc++(Json obj (kv._1-> ( Json.toJson(kv._2)) )))
-    m_log trace "Print result: " + (Json prettyPrint( raw_data ))
+    raw_data = q.foldLeft[JsObject](JsObject(Nil))((acc, kv) => acc ++ (Json obj (kv._1 -> (Json.toJson(kv._2)))))
+    m_log trace "Print result: " + (Json prettyPrint (raw_data))
   }
 
   def createFlattenMessage(q: scala.collection.immutable.Map[String, String]): Unit = {
     m_log trace "create message "
-    raw_data = q.foldLeft[JsObject](JsObject(Nil))((acc,kv)=>acc++(Json obj (kv._1-> ( Json.toJson(kv._2)) )))
-    m_log trace "Print result: " + (Json prettyPrint( raw_data ))
+    raw_data = q.foldLeft[JsObject](JsObject(Nil))((acc, kv) => acc ++ (Json obj (kv._1 -> (Json.toJson(kv._2)))))
+    m_log trace "Print result: " + (Json prettyPrint (raw_data))
   }
 
- def processRequest(): (Boolean, String)
+  def processRequest(): (Boolean, String)
 
   var raw_data: JsObject = null
 
