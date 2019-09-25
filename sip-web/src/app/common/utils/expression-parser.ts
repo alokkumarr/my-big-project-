03ca5d14624d4f626f2eee8f77dd60986ed0bc70
Akash Agrawal <Akash.Agarwal@synchronoss.com>
2019-09-25 16:37:23 +0530

    SIP-8397: Add validation for column names in derived metric

--- sip-web/src/app/common/utils/expression-parser.ts
+++ sip-web/src/app/common/utils/expression-parser.ts
@@ -2,6 +2,7 @@ import * as mjs from 'mathjs';
 import { MathNode, parse } from 'mathjs';
 import * as isNil from 'lodash/isNil';
 import * as isEmpty from 'lodash/isEmpty';
+import * as lowerCase from 'lodash/lowerCase';
 
 /*
  ████████╗██╗   ██╗██████╗ ███████╗███████╗
@@ -32,7 +33,6 @@ interface IMathJS extends mjs.MathJsStatic {
 
 const { SymbolNode, OperatorNode, ConstantNode, FunctionNode } = mjs as IMathJS;
 
-// #region Type definitions for SIP compatible expressions
 export enum Operator {
   Add = '+',
   Subtract = '-',
@@ -57,12 +57,6 @@ interface OperatorExpression {
 
 type Expression = OperatorExpression | ColumnExpression | ConstantExpression;
 
-/* Use when the expression contains only a constant or a column. Backend needs this
-   wrapped inside an operand for some reason. */
-// interface LoneExpression {
-//   operand1: ColumnExpression | ConstantExpression;
-// }
-
 export enum ExpressionErrorType {
   StringParsingFailed,
   JsonParsingFailed
@@ -77,11 +71,20 @@ export class ExpressionError extends Error {
     return this.message;
   }
 }
-// #endregion
 
 export const SUPPORTED_AGGREGATES = ['SUM', 'AVG'];
 
-/*
+export class ExpressionParser {
+  private supportedColumns: string[]; // used to validate column names in expression
+
+  constructor(supportedColumns: string[] = null) {
+    if (!isEmpty(supportedColumns)) {
+      this.supportedColumns = supportedColumns.map(colName =>
+        lowerCase(colName)
+      );
+    }
+  }
+  /*
  ██╗   ██╗ █████╗ ██╗     ██╗██████╗  █████╗ ████████╗ ██████╗ ██████╗ ███████╗
  ██║   ██║██╔══██╗██║     ██║██╔══██╗██╔══██╗╚══██╔══╝██╔═══██╗██╔══██╗██╔════╝
  ██║   ██║███████║██║     ██║██║  ██║███████║   ██║   ██║   ██║██████╔╝███████╗
@@ -90,74 +93,87 @@ export const SUPPORTED_AGGREGATES = ['SUM', 'AVG'];
    ╚═══╝  ╚═╝  ╚═╝╚══════╝╚═╝╚═════╝ ╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚══════╝
 */
 
-const validateOperator = (operator: MathNode) => {
-  if (!Object.values(Operator).includes(<Operator>operator.op)) {
+  validateOperator(operator: MathNode) {
+    if (!Object.values(Operator).includes(<Operator>operator.op)) {
+      throw new ExpressionError(
+        ExpressionErrorType.StringParsingFailed,
+        `Operator not supported: ${operator.op}`
+      );
+    }
+  }
+
+  validateLoneColumn(columnNode: MathNode) {
     throw new ExpressionError(
       ExpressionErrorType.StringParsingFailed,
-      `Operator not supported: ${operator.op}`
-    );
-  }
-};
-
-const validateLoneColumn = (columnNode: MathNode) => {
-  throw new ExpressionError(
-    ExpressionErrorType.StringParsingFailed,
-    `No aggregate applied to column: ${columnNode.name}.
+      `No aggregate applied to column: ${columnNode.name}.
     Example usage: ${SUPPORTED_AGGREGATES[0]}(${columnNode.name})`
-  );
-};
-
-const validateAggregate = (aggregate: MathNode) => {
-  /* If no argument is provided for aggregate */
-  if (isEmpty(aggregate.args)) {
-    throw new ExpressionError(
-      ExpressionErrorType.StringParsingFailed,
-      `Missing column name for aggregate: ${aggregate.name}().
-      Example usage: ${aggregate.name}(column_name)`
     );
   }
 
-  /* If more than one argument is provided for aggregate */
-  if (aggregate.args.length > 1) {
-    throw new ExpressionError(
-      ExpressionErrorType.StringParsingFailed,
-      `Multiple arguments not supported in aggregate: ${aggregate.toString()}.
+  validateAggregate(aggregate: MathNode) {
+    /* If no argument is provided for aggregate */
+    if (isEmpty(aggregate.args)) {
+      throw new ExpressionError(
+        ExpressionErrorType.StringParsingFailed,
+        `Missing column name for aggregate: ${aggregate.name}().
       Example usage: ${aggregate.name}(column_name)`
-    );
-  }
+      );
+    }
 
-  /* If something other than a column name is provided. For example, SUM(a + b).
+    /* If more than one argument is provided for aggregate */
+    if (aggregate.args.length > 1) {
+      throw new ExpressionError(
+        ExpressionErrorType.StringParsingFailed,
+        `Multiple arguments not supported in aggregate: ${aggregate.toString()}.
+      Example usage: ${aggregate.name}(column_name)`
+      );
+    }
+
+    /* If something other than a column name is provided. For example, SUM(a + b).
      Here, a + b is not a column name. It's an expression. Not supported.
   */
-  if (aggregate.args[0].type !== 'SymbolNode') {
-    throw new ExpressionError(
-      ExpressionErrorType.StringParsingFailed,
-      `Invalid arguments for aggregate: ${aggregate.name}.
+    if (aggregate.args[0].type !== 'SymbolNode') {
+      throw new ExpressionError(
+        ExpressionErrorType.StringParsingFailed,
+        `Invalid arguments for aggregate: ${aggregate.name}.
       Replace '${aggregate.args[0].toString()}' with a column name.
       Example usage: ${aggregate.name}(column_name)`
-    );
+      );
+    }
+
+    /* If aggregate is not one of the supported aggregates */
+    const columnNode = aggregate.args[0];
+    if (
+      !SUPPORTED_AGGREGATES.some(agg => agg === aggregate.name.toUpperCase())
+    ) {
+      throw new ExpressionError(
+        ExpressionErrorType.StringParsingFailed,
+        `Aggregate not supported: ${aggregate.name} for column: ${columnNode.name}`
+      );
+    }
+
+    /* If column is not supported */
+    if (
+      this.supportedColumns &&
+      !this.supportedColumns.includes(lowerCase(columnNode.name))
+    ) {
+      throw new ExpressionError(
+        ExpressionErrorType.StringParsingFailed,
+        `Column with name '${columnNode.name}' does not exist.`
+      );
+    }
   }
 
-  /* If aggregate is not one of the supported aggregates */
-  const columnNode = aggregate.args[0];
-  if (!SUPPORTED_AGGREGATES.some(agg => agg === aggregate.name.toUpperCase())) {
-    throw new ExpressionError(
-      ExpressionErrorType.StringParsingFailed,
-      `Aggregate not supported: ${aggregate.name} for column: ${columnNode.name}`
-    );
+  validateConstant(constant: MathNode) {
+    if (typeof constant.value !== 'number' || isNil(constant.value)) {
+      throw new ExpressionError(
+        ExpressionErrorType.StringParsingFailed,
+        `Invalid constant: ${constant.value}`
+      );
+    }
   }
-};
 
-const validateConstant = (constant: MathNode) => {
-  if (typeof constant.value !== 'number' || isNil(constant.value)) {
-    throw new ExpressionError(
-      ExpressionErrorType.StringParsingFailed,
-      `Invalid constant: ${constant.value}`
-    );
-  }
-};
-
-/*
+  /*
   ██████╗ ██████╗ ███╗   ██╗██╗   ██╗███████╗██████╗ ████████╗███████╗██████╗ ███████╗
  ██╔════╝██╔═══██╗████╗  ██║██║   ██║██╔════╝██╔══██╗╚══██╔══╝██╔════╝██╔══██╗██╔════╝
  ██║     ██║   ██║██╔██╗ ██║██║   ██║█████╗  ██████╔╝   ██║   █████╗  ██████╔╝███████╗
@@ -166,107 +182,107 @@ const validateConstant = (constant: MathNode) => {
   ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚══════╝
 */
 
-/**
- * Transforms the mathjs expression tree to sip
- * compatible expression tree.
- *
- * Expression Tree Reference: https://mathjs.org/docs/expressions/expression_trees.html
- *
- * @param {MathNode} node
- * @returns {Expression}
- */
-const toJSON = (node: MathNode): Expression => {
-  switch (node.type) {
-    case 'OperatorNode':
-      validateOperator(node);
-      return {
-        operator: <Operator>node.op,
-        operand1: toJSON(node.args[0]),
-        operand2: toJSON(node.args[1])
-      };
-    case 'ConstantNode':
-      validateConstant(node);
-      return { value: node.value };
-    case 'FunctionNode':
-      validateAggregate(node);
-      return {
-        aggregate: node.name,
-        column: node.args[0].name
-      };
-    case 'SymbolNode':
-      validateLoneColumn(node);
-      return { column: node.name };
-    case 'ParenthesisNode':
-      return toJSON(node['content']);
-  }
-};
-
-/**
- * Converts operator symbol to its function name. This function
- * name is used in mathjs to establish precedence. Without
- * function names, mathjs would consider + and * as just characters
- * and won't know how to apply precedence rules.
- * Function reference: https://mathjs.org/docs/reference/functions.html
- *
- * @param {string} operator
- * @returns {string}
- */
-const operatorFunction = (operator: Operator): string => {
-  switch (operator) {
-    case Operator.Add:
-      return 'add';
-    case Operator.Subtract:
-      return 'subtract';
-    case Operator.Multiply:
-      return 'multiply';
-    case Operator.Divide:
-      return 'divide';
-    default:
-      return '';
-  }
-};
-
-/**
- * Converts a SIP compatible expression json to Mathjs
- * compatible expression tree.
- *
- * Expression Tree Reference: https://mathjs.org/docs/expressions/expression_trees.html
- *
- * @param {Expression} json
- * @returns {MathNode}
- */
-const fromJSON = (json: Expression): MathNode => {
-  if ((<OperatorExpression>json).operator) {
-    /* If the json is an operator object */
-    const operatorJSON = json as OperatorExpression;
-    return new OperatorNode(
-      operatorJSON.operator,
-      operatorFunction(operatorJSON.operator),
-      [fromJSON(operatorJSON.operand1), fromJSON(operatorJSON.operand2)]
-    );
-    /* else if ((<LoneExpression>json).operand1) {
-    return fromJSON((<LoneExpression>json).operand1);
-  }*/
-  } else if ((<ColumnExpression>json).column) {
-    /* If the json is a column object */
-    const columnJSON = json as ColumnExpression;
-    if (columnJSON.aggregate) {
-      /* If the json is a column object with aggregate */
-      return new FunctionNode(columnJSON.aggregate, [
-        new SymbolNode(columnJSON.column)
-      ]);
-    } else {
-      /* If the json is a column object without aggregate */
-      return new SymbolNode(columnJSON.column);
+  /**
+   * Transforms the mathjs expression tree to sip
+   * compatible expression tree.
+   *
+   * Expression Tree Reference: https://mathjs.org/docs/expressions/expression_trees.html
+   *
+   * @param {MathNode} node
+   * @returns {Expression}
+   */
+  toJSON(node: MathNode): Expression {
+    switch (node.type) {
+      case 'OperatorNode':
+        this.validateOperator(node);
+        return {
+          operator: <Operator>node.op,
+          operand1: this.toJSON(node.args[0]),
+          operand2: this.toJSON(node.args[1])
+        };
+      case 'ConstantNode':
+        this.validateConstant(node);
+        return { value: node.value };
+      case 'FunctionNode':
+        this.validateAggregate(node);
+        return {
+          aggregate: node.name,
+          column: node.args[0].name
+        };
+      case 'SymbolNode':
+        this.validateLoneColumn(node);
+        return { column: node.name };
+      case 'ParenthesisNode':
+        return this.toJSON(node['content']);
     }
-  } else {
-    /* If json is a simple constant */
-    const constantJSON = json as ConstantExpression;
-    return new ConstantNode(constantJSON.value);
   }
-};
 
-/*
+  /**
+   * Converts operator symbol to its function name. This function
+   * name is used in mathjs to establish precedence. Without
+   * function names, mathjs would consider + and * as just characters
+   * and won't know how to apply precedence rules.
+   * Function reference: https://mathjs.org/docs/reference/functions.html
+   *
+   * @param {string} operator
+   * @returns {string}
+   */
+  operatorFunction(operator: Operator): string {
+    switch (operator) {
+      case Operator.Add:
+        return 'add';
+      case Operator.Subtract:
+        return 'subtract';
+      case Operator.Multiply:
+        return 'multiply';
+      case Operator.Divide:
+        return 'divide';
+      default:
+        return '';
+    }
+  }
+
+  /**
+   * Converts a SIP compatible expression json to Mathjs
+   * compatible expression tree.
+   *
+   * Expression Tree Reference: https://mathjs.org/docs/expressions/expression_trees.html
+   *
+   * @param {Expression} json
+   * @returns {MathNode}
+   */
+  fromJSON(json: Expression): MathNode {
+    if ((<OperatorExpression>json).operator) {
+      /* If the json is an operator object */
+      const operatorJSON = json as OperatorExpression;
+      return new OperatorNode(
+        operatorJSON.operator,
+        this.operatorFunction(operatorJSON.operator),
+        [
+          this.fromJSON(operatorJSON.operand1),
+          this.fromJSON(operatorJSON.operand2)
+        ]
+      );
+    } else if ((<ColumnExpression>json).column) {
+      /* If the json is a column object */
+      const columnJSON = json as ColumnExpression;
+      if (columnJSON.aggregate) {
+        /* If the json is a column object with aggregate */
+        return new FunctionNode(columnJSON.aggregate, [
+          new SymbolNode(columnJSON.column)
+        ]);
+      } else {
+        /* If the json is a column object without aggregate */
+        return new SymbolNode(columnJSON.column);
+      }
+    } else {
+      /* If json is a simple constant */
+      const constantJSON = json as ConstantExpression;
+      return new ConstantNode(constantJSON.value);
+    }
+  }
+
+  /*
  ██████╗ ██╗   ██╗██████╗ ██╗     ██╗ ██████╗     █████╗ ██████╗ ██╗
  ██╔══██╗██║   ██║██╔══██╗██║     ██║██╔════╝    ██╔══██╗██╔══██╗██║
  ██████╔╝██║   ██║██████╔╝██║     ██║██║         ███████║██████╔╝██║
@@ -275,43 +291,39 @@ const fromJSON = (json: Expression): MathNode => {
  ╚═╝      ╚═════╝ ╚═════╝ ╚══════╝╚═╝ ╚═════╝    ╚═╝  ╚═╝╚═╝     ╚═╝
 */
 
-/**
- * Converts formula string to sip compatible expression json.
- *
- * @param {string} expr
- * @returns {Expression}
- */
-export const parseExpression = (expr: string): Expression => {
-  try {
-    return toJSON(parse(expr));
-    // const json = toJSON(parse(expr));
-    // if (!(<OperatorExpression>json).operator) {
-    //   return <LoneExpression>{
-    //     operand1: json
-    //   };
-    // } else {
-    //   return json;
-    // }
-  } catch (e) {
-    throw new ExpressionError(
-      ExpressionErrorType.StringParsingFailed,
-      e.message
-    );
+  /**
+   * Converts formula string to sip compatible expression json.
+   *
+   * @param {string} expr
+   * @returns {Expression}
+   */
+  parseExpression(expr: string): Expression {
+    try {
+      return this.toJSON(parse(expr));
+    } catch (e) {
+      throw new ExpressionError(
+        ExpressionErrorType.StringParsingFailed,
+        e.message
+      );
+    }
   }
-};
 
-/**
- * Converts sip compatible expression json to formula string.
- *
- * @param {Expression} json
- * @returns {string}
- */
-export const parseJSON = (json: Expression): string => {
-  try {
-    const expression = fromJSON(json).toString();
-    parseExpression(expression); // try to parse the expression again to make sure it's valid
-    return expression;
-  } catch (e) {
-    throw new ExpressionError(ExpressionErrorType.JsonParsingFailed, e.message);
+  /**
+   * Converts sip compatible expression json to formula string.
+   *
+   * @param {Expression} json
+   * @returns {string}
+   */
+  parseJSON(json: Expression): string {
+    try {
+      const expression = this.fromJSON(json).toString();
+      this.parseExpression(expression); // try to parse the expression again to make sure it's valid
+      return expression;
+    } catch (e) {
+      throw new ExpressionError(
+        ExpressionErrorType.JsonParsingFailed,
+        e.message
+      );
+    }
   }
-};
+}
