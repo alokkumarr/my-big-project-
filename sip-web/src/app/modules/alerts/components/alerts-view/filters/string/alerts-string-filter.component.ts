cf2959e1a955671a41b0cced016ad66fd4672b79
Barna Mumtyan <Barna.Mumtya@synchronoss.com>
2019-09-27 19:24:52 +0300

    SIP-8472: Enabled the attribute filter.

--- sip-web/src/app/modules/alerts/components/alerts-view/filters/string/alerts-string-filter.component.ts
+++ sip-web/src/app/modules/alerts/components/alerts-view/filters/string/alerts-string-filter.component.ts
@@ -1,30 +1,30 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { Select, Store } from '@ngxs/store';
+import { LoadAllAttributeValues } from '../../../../state/alerts.actions';
 import {
-  // ApplyAlertFilters,
-  // ResetAlertFilters,
-  LoadAllAttributeValues
-} from '../../../../state/alerts.actions';
-import { AlertFilterModel } from '../../../../alerts.interface';
+  AlertFilterModel,
+  AlertFilterEvent
+} from '../../../../alerts.interface';
 import { Observable } from 'rxjs';
 import { AlertsState } from '../../../../state/alerts.state';
 
 @Component({
   selector: 'alerts-string-filter',
-  templateUrl: './alerts-string-filter.component.html'
-  // styleUrls: ['./alerts-string-filter.component.scss']
+  templateUrl: './alerts-string-filter.component.html',
+  styleUrls: ['./alerts-string-filter.component.scss']
 })
 export class AlertsStringFilterComponent implements OnInit {
   alertFilterForm: FormGroup;
   @Select(AlertsState.getAllAttributeValues)
   attributeValues$: Observable<string[]>;
-
-  @Output() change = new EventEmitter<AlertFilterModel>();
+  filter: AlertFilterModel;
+  @Output() change = new EventEmitter<AlertFilterEvent>();
   @Input('stringFilter') set setStringFilter(stringFilter: AlertFilterModel) {
     const { modelValues } = stringFilter;
     const [value] = modelValues;
-    this.alertFilterForm.setValue({ value });
+    this.filter = stringFilter;
+    this.alertFilterForm.setValue({ value }, { emitEvent: false });
   }
 
   constructor(private fb: FormBuilder, private store: Store) {
@@ -37,17 +37,15 @@ export class AlertsStringFilterComponent implements OnInit {
 
   createForm() {
     this.alertFilterForm = this.fb.group({
-      value: ['', Validators.required]
+      value: ['']
+    });
+    this.alertFilterForm.valueChanges.subscribe(({ value }) => {
+      const formIsValid = !this.alertFilterForm.invalid;
+      if (formIsValid) {
+        console.log('isValid');
+        this.filter.modelValues = [value];
+        this.change.emit({ filter: this.filter, isValid: formIsValid });
+      }
     });
   }
-
-  // applyFilters() {
-  //   const filterValue = this.alertFilterForm.get('value').value;
-  //   const filters = [filterValue];
-  //   this.store.dispatch(new ApplyAlertFilters(filters));
-  // }
-
-  // resetFilters() {
-  //   this.store.dispatch(new ResetAlertFilters());
-  // }
 }
