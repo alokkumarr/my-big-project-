9ea1f1d094ae63ff73878e4f5ea43a184d72cc7c
Prabhulingappa A S <Prabhulingappa.AS@synchronoss.com>
2019-10-11 13:18:22 +0530

    SIP-8323: Dispatch columnName diff in FE and BE fixed

--- sip-services/sip-export-service/src/main/java/com/synchronoss/saw/export/util/ExportUtils.java
+++ sip-services/sip-export-service/src/main/java/com/synchronoss/saw/export/util/ExportUtils.java
@@ -78,10 +78,14 @@ public class ExportUtils {
       for (int visibleIndex = 0; visibleIndex < fields.size(); visibleIndex++) {
         for (Field field : fields) {
           String aliasName = field.getAlias() != null && !field.getAlias().isEmpty() ? field.getAlias() : null;
+          String displayName = field.getDisplayName() != null && !field.getDisplayName().isEmpty() ? field.getDisplayName() : null;
           // look for DL report
           if (sipQuery.getQuery() != null && !sipQuery.getQuery().isEmpty()) {
             if (field.getVisibleIndex() != null && field.getVisibleIndex().equals(visibleIndex)) {
               String[] split = StringUtils.isEmpty(field.getColumnName()) ? null : field.getColumnName().split("\\.");
+              if (displayName != null) {
+                  split = StringUtils.isEmpty(field.getDisplayName()) ? null : field.getDisplayName().split("\\.");
+              }
               String columnName;
               String aggregationName = field.getAggregate() != null ? field.getAggregate().value() : null;
               if (aggregationName != null && DISTINCT_COUNT_AGGREGATION.equalsIgnoreCase(aggregationName)) {
@@ -91,7 +95,8 @@ public class ExportUtils {
                 columnName = aggregationName != null ? aggregationName.trim() + "(" + split[0].trim() + ")" : split[0];
                 header.put(columnName.trim(), aliasName);
               } else {
-                columnName = aggregationName != null ? aggregationName.trim() + "(" + field.getColumnName().trim() + ")" : field.getColumnName();
+                String name = displayName != null ? displayName : field.getColumnName().trim();
+                columnName = aggregationName != null ? aggregationName.trim() + "(" + name + ")" : field.getColumnName();
                 header.put(columnName.trim(), aliasName);
               }
               break;
@@ -99,10 +104,14 @@ public class ExportUtils {
           } else {
             if (field.getVisibleIndex() != null && field.getVisibleIndex().equals(visibleIndex)) {
               String[] split = StringUtils.isEmpty(field.getColumnName()) ? null : field.getColumnName().split("\\.");
+              if (displayName != null) {
+                  split = StringUtils.isEmpty(field.getDisplayName()) ? null : field.getDisplayName().split("\\.");
+              }
               if (split != null && split.length >= 2) {
                 header.put(split[0], aliasName);
               } else {
-                header.put(field.getColumnName(), aliasName);
+                String name = displayName != null ? displayName : field.getColumnName().trim();
+                header.put(name, aliasName);
               }
               break;
             }
