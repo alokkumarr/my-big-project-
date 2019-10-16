51f26f79bc7f6c38555681fb49885f9b72dea831
Barna Mumtyan <Barna.Mumtya@synchronoss.com>
2019-10-16 15:02:27 +0300

    SIP-8108: Fixed some code review issues.

--- sip-web/src/app/modules/alerts/alerts.module.ts
+++ sip-web/src/app/modules/alerts/alerts.module.ts
@@ -1,13 +1,8 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModuleTs } from '../../common';
-// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxsModule } from '@ngxs/store';
-// import { DxTemplateModule } from 'devextreme-angular/core/template';
-// import { DxDataGridModule } from 'devextreme-angular/ui/data-grid';
 import { DxSelectBoxModule } from 'devextreme-angular/ui/select-box';
 
-// import { MaterialModule } from '../../material.module';
-// import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
 import { routes } from './routes';
 import { AlertsPageComponent } from './components/alerts-page/alerts-page.component';
@@ -48,12 +43,6 @@ const components = [
   imports: [
     CommonModuleTs,
     RouterModule.forChild(routes),
-    // FormsModule,
-    // ReactiveFormsModule,
-    // MaterialModule,
-    // FlexLayoutModule,
-    // DxTemplateModule,
-    // DxDataGridModule,
     DxSelectBoxModule,
     NgxsModule.forFeature([AlertsState])
   ],
