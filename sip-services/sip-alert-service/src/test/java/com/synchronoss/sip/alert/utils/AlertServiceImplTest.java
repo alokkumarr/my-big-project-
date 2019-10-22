11a1fda01c6a81868602788e41f12e5dad9dfc36
Prabhulingappa A S <Prabhulingappa.AS@synchronoss.com>
2019-10-22 17:11:01 +0530

    SIP-5829: Removing unnecessary files from code

--- sip-services/sip-alert-service/src/test/java/com/synchronoss/sip/alert/utils/AlertServiceImplTest.java
+++
@@ -1,7 +0,0 @@
-package com.synchronoss.sip.alert.utils;
-
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
-
-// @RunWith(SpringRunner.class)
-public class AlertServiceImplTest {}
