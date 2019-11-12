d6e8ffaffc15442c783de1180a18459fefaab224
Pradeep Tripathi <pradeep.manitripathi@synchronoss.com>
2019-11-12 14:26:32 +0530

    SIP-8666 : Added internal controller for sip-security.

---
+++ sip-security/sip-security/src/main/java/com/sncr/saw/security/app/controller/internal/SipDskController.java
@@ -0,0 +1,13 @@
+package com.sncr.saw.security.app.controller.internal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/internal/sip-security/dsk")
+public class SipDskController {
+    private static final Logger logger = LoggerFactory.getLogger(SipDskController.class);
+
+}
