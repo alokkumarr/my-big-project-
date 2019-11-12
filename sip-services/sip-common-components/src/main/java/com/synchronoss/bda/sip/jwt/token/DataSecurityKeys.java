a0b51c2d72aeccfe27f159370897db02499500d0
alok.kumarr <alok.kumarr@synchronoss.com>
2019-11-12 11:51:43 +0530

    SIP-6779-data-security-key-filter-does-not-work-for-scheduled-reports

--- sip-services/sip-common-components/src/main/java/com/synchronoss/bda/sip/jwt/token/DataSecurityKeys.java
+++ sip-services/sip-common-components/src/main/java/com/synchronoss/bda/sip/jwt/token/DataSecurityKeys.java
@@ -14,18 +14,34 @@ public class DataSecurityKeys implements Serializable {
   private String message;
   private List<TicketDSKDetails> dataSecurityKeys;
 
+  /**
+   * Gets message
+   *
+   * @return value of message
+   */
   public String getMessage() {
     return message;
   }
 
+  /**
+   * Sets value of message
+   */
   public void setMessage(String message) {
     this.message = message;
   }
 
+  /**
+   * Gets dataSecurityKeys
+   *
+   * @return value of dataSecurityKeys
+   */
   public List<TicketDSKDetails> getDataSecurityKeys() {
     return dataSecurityKeys;
   }
 
+  /**
+   * Sets value of dataSecurityKeys
+   */
   public void setDataSecurityKeys(List<TicketDSKDetails> dataSecurityKeys) {
     this.dataSecurityKeys = dataSecurityKeys;
   }
