73c64a225a8b1038e0489ce4928f7cc03fa424a0
Sunil Kumar B M <sunilkumar.bm@synchronoss.com>
2019-12-13 21:16:20 +0530

    SIP-9032: Flyway file added for dsk_eligible_fields table

---
+++ sip-security/sip-security/src/main/resources/db/migration/V24__DSK_ELIGIBLE_FIELDS.sql
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ Filename:  V24__DSK_ELIGIBLE_FIELDS.SQL
+ Purpose:   Creation of dsk_eligible_fields table
+ Date:      13-12-2019
+********************************************************************************/
+
+DROP TABLE IF EXISTS dsk_eligible_fields;
+
+CREATE TABLE dsk_eligible_fields
+(
+   CUSTOMER_SYS_ID bigint(20) NOT NULL,
+   PRODUCT_ID bigint(20) NOT NULL,
+   SEMANTIC_ID bigint(20) NOT NULL,
+   COLUMN_NAME varchar(100) NOT NULL,
+   DISPLAY_NAME varchar(100) NOT NULL,
+   ACTIVE_STATUS_ID tinyint(4) NOT NULL,
+   CREATED_TIME datetime NOT NULL,
+   CREATED_BY varchar(255) NOT NULL,
+   MODIFIED_TIME datetime NOT NULL,
+   MODIFIED_BY varchar(255) NOT NULL
+) ENGINE=InnoDB;
