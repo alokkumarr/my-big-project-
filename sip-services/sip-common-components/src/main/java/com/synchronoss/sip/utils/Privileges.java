3b03b83f2775badd4720e4e274fc2136fc262ff2
Prabhulingappa A S <pras0004@in-c02zl3gglvcg.synchronoss.net>
2019-12-13 13:52:28 +0530

    SIP-8897: Addressing PR comments

--- sip-services/sip-common-components/src/main/java/com/synchronoss/sip/utils/Privileges.java
+++ sip-services/sip-common-components/src/main/java/com/synchronoss/sip/utils/Privileges.java
@@ -2,40 +2,45 @@ package com.synchronoss.sip.utils;
 
 import static com.synchronoss.sip.utils.SipCommonUtils.decToBinary;
 
+import com.fasterxml.jackson.annotation.JsonValue;
 import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Privileges {
 
-  EnumMap<PrivilegeNames, Integer> privilegeCodes;
-
   private static final Logger logger = LoggerFactory.getLogger(SipCommonUtils.class);
 
   public enum PrivilegeNames {
-    ACCESS,
-    CREATE,
-    EXECUTE,
-    PUBLISH,
-    FORK,
-    EDIT,
-    EXPORT,
-    DELETE,
-    ALL
-  }
+    ACCESS(0),
+    CREATE(1),
+    EXECUTE(2),
+    PUBLISH(3),
+    FORK(4),
+    EDIT(5),
+    EXPORT(6),
+    DELETE(7),
+    ALL(8);
 
-  /** Default constructor to intaialize enumMap. */
-  public Privileges() {
-    privilegeCodes = new EnumMap<>(PrivilegeNames.class);
-    privilegeCodes.put(PrivilegeNames.ACCESS, 0);
-    privilegeCodes.put(PrivilegeNames.CREATE, 1);
-    privilegeCodes.put(PrivilegeNames.EXECUTE, 2);
-    privilegeCodes.put(PrivilegeNames.PUBLISH, 3);
-    privilegeCodes.put(PrivilegeNames.FORK, 4);
-    privilegeCodes.put(PrivilegeNames.EDIT, 5);
-    privilegeCodes.put(PrivilegeNames.EXPORT, 6);
-    privilegeCodes.put(PrivilegeNames.DELETE, 7);
-    privilegeCodes.put(PrivilegeNames.ALL, 8);
+    private static final Map<PrivilegeNames, Integer> CONSTANTS = new HashMap<>();
+
+    static {
+      for (PrivilegeNames c : values()) {
+        CONSTANTS.put(c, c.value);
+      }
+    }
+
+    private final Integer value;
+
+    PrivilegeNames(Integer value) {
+      this.value = value;
+    }
+
+    public int getIndex() {
+      return this.value;
+    }
   }
 
   /**
@@ -47,16 +52,15 @@ public class Privileges {
    */
   public boolean isPriviegePresent(PrivilegeNames privName, Long dec) {
     if (dec == null || privName == null) {
-      logger.error("PrivName or decimal code acn't be null!!");
+      logger.error("PrivName or decimal code can't be null!!");
       return false;
     }
     int[] privCode = decToBinary(dec);
-    int privValue = privilegeCodes.get(privName);
+    int privValue = privName.getIndex();
 
     logger.info(String.format("Privilege Name : %s , Privilege Value : %d ", privName, privValue));
 
-
-    if (privCode[privilegeCodes.get(PrivilegeNames.ALL)] == 1 || privCode[privValue] == 1) {
+    if (privCode[PrivilegeNames.ALL.getIndex()] == 1 || privCode[privValue] == 1) {
       return true;
     }
 
