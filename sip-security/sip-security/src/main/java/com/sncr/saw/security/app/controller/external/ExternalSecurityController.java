f2a6fae4d1d5bc3780bd83de24343004100e5381
Ramesh <chra0001@synchronoss.com>
2020-01-02 16:11:04 +0530

    SIP-8970: renaming the endpoint from external to v1

--- sip-security/sip-security/src/main/java/com/sncr/saw/security/app/controller/external/ExternalSecurityController.java
+++ sip-security/sip-security/src/main/java/com/sncr/saw/security/app/controller/external/ExternalSecurityController.java
@@ -43,7 +43,7 @@ import java.util.List;
  */
 @Api(value = "The controller provides to perform external admin security operation in synchronoss insight platform ")
 @RestController
-@RequestMapping("/sip-security/auth/admin/external")
+@RequestMapping("/sip-security/auth/admin/v1")
 public class ExternalSecurityController {
 
   @Autowired
