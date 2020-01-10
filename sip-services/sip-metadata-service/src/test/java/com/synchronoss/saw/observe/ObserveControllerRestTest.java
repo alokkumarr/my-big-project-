0be80775aeb5e68ee6108112de9381abdbf21b00
alok.kumarr <alok.kumarr@synchronoss.com>
2020-01-10 14:21:38 +0530

    feature/SIP-8904-changes for review comments.

--- sip-services/sip-metadata-service/src/test/java/com/synchronoss/saw/observe/ObserveControllerRestTest.java
+++ sip-services/sip-metadata-service/src/test/java/com/synchronoss/saw/observe/ObserveControllerRestTest.java
@@ -74,7 +74,7 @@ public class ObserveControllerRestTest {
     String expected =
         getObserveResponseString(
             ObserveUtils.prepareResponse(observe,
-                "UNAUTHORIZED ACCESS : User don't have the ACCESS dashboard!!"));
+                "UNAUTHORIZED ACCESS : User don't have the Access dashboard!!"));
     JSONAssert.assertEquals(result.getResponse().getContentAsString(), expected, false);
   }
 
