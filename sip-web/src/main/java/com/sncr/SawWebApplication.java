391f68997c3d79ebbcd20c560ec1fd765b0a52ba
Ramesh <chra0001@synchronoss.com>
2020-01-20 13:05:37 +0530

    SIP-9898:Modifying the spring boot version to 2.1.0 in sip-web

--- sip-web/src/main/java/com/sncr/SawWebApplication.java
+++ sip-web/src/main/java/com/sncr/SawWebApplication.java
@@ -1,10 +1,9 @@
 package com.sncr;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @Configuration
-@EnableAutoConfiguration
 @EnableOAuth2Client
 @RestController
 public class SawWebApplication  extends SpringBootServletInitializer{
@@ -22,10 +20,10 @@ public class SawWebApplication  extends SpringBootServletInitializer{
 	protected SpringApplicationBuilder configure(
 			SpringApplicationBuilder builder) {
 		return builder.sources(SawWebApplication.class);
-	}	
-	
+	}
+
     public static void main(String[] args) {
-    	SpringApplication.run(SawWebApplication.class, args);    	
+    	SpringApplication.run(SawWebApplication.class, args);
     }
-	
+
 }
