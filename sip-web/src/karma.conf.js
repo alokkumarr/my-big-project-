6d218f0f5054373e07c4786d40f4fc627df8bf8a
Anirudh Venkatesh <Anirudh.NV@Synchronoss.com>
2020-01-24 17:40:30 +0530

    SIP-9059: checking bamboo build failure

--- sip-web/src/karma.conf.js
+++ sip-web/src/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     browsers: ['Chrome'],
-    browserDisconnectTimeout: 20000,
+    browserDisconnectTimeout: 12000,
     browserDisconnectTolerance: 3,
     browserNoActivityTimeout: 60000,
     customLaunchers: {
