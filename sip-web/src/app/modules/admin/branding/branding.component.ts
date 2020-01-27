4d217f913c014b1ebc2a7bc0f0d95097269609cf
Anirudh Venkatesh <Anirudh.NV@Synchronoss.com>
2020-01-27 18:11:37 +0530

    SIP-9059: increasing contrast of primary color to distingusih between text and background color in sidenav menu

--- sip-web/src/app/modules/admin/branding/branding.component.ts
+++ sip-web/src/app/modules/admin/branding/branding.component.ts
@@ -28,23 +28,15 @@ export class AdminBrandingComponent {
   ) {}
 
   ngOnInit(): void {
-    // this.fb.group({
-    //   color: [this.primaryColor, [Validators.required,
-    //     Validators.maxLength(30)]],
-    //   logo: ['', []]
-    // });
-
     this.saveForm = new FormGroup({
       color: new FormControl(this.primaryColor, [
         Validators.required
       ]),
       logo: new FormControl({value: '', disabled: true})
-
     });
 
     const token = this._jwtService.getTokenObj();
     this.ticket = token.ticket;
-    console.log(this.ticket);
 
     this._brandingService.getBrandingDetails().subscribe(data => {
       this.primaryColor = isEmpty(data.brandColor) ? DEFAULT_BRANDING_COLOR : data.brandColor;
