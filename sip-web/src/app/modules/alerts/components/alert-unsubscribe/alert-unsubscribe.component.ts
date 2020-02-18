5a7b48de11fbd23ac08c0fa0c6ba146463b41b60
Anirudh Venkatesh <Anirudh.NV@Synchronoss.com>
2020-02-18 20:14:20 +0530

    SIP-6333: Validations and CSS changes for new alert un subscribe option

--- sip-web/src/app/modules/alerts/components/alert-unsubscribe/alert-unsubscribe.component.ts
+++ sip-web/src/app/modules/alerts/components/alert-unsubscribe/alert-unsubscribe.component.ts
@@ -3,6 +3,7 @@ import { AlertUnsubscribeService } from './alert-unsubscribe.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { JwtService } from './../../../../common/services/jwt.service';
 import { UserService } from './../../../../common/services/user.service';
+import { ToastService } from '../../../../common/services/toastMessage.service';
 
 @Component({
   selector: 'alert-unsubscribe',
@@ -11,32 +12,33 @@ import { UserService } from './../../../../common/services/user.service';
 })
 export class AlertUnsubscribe implements OnInit {
   userLoggedIn: boolean;
+  alertToken: string;
+  parsedToken: {};
 
   constructor(
     private _alertUnsubscribeService: AlertUnsubscribeService,
     private _jwtService: JwtService,
     private _userService: UserService,
     public _router: Router,
-    public _route: ActivatedRoute
+    public _route: ActivatedRoute,
+    public _toastMessage: ToastService,
   ) { }
 
   ngOnInit() {
     this.userLoggedIn = this._userService.isLoggedIn();
-    console.log(this.userLoggedIn);
     this._route.queryParams.subscribe(({ token }) => {
-      console.log({token});
-      console.log(this._jwtService.parseJWT(token));
+      this.parsedToken = this._jwtService.parseJWT(token);
+      this.alertToken = token;
     });
   }
 
   unSubscribeAlert() {
-    const token = 'token';
-    this._alertUnsubscribeService.unsubscribeAnAlert(token).subscribe(data => {
-      console.log(data);
+    this._alertUnsubscribeService.unsubscribeAnAlert(this.alertToken).subscribe(data => {
+      this._toastMessage.success(data);
     });
   }
 
   close() {
-    this._router.navigate([]);
+    this._router.navigate(['']);
   }
 }
--- sip-web/src/app/modules/alerts/components/alert-unsubscribe/alert-unsubscribe.service.ts
+++ sip-web/src/app/modules/alerts/components/alert-unsubscribe/alert-unsubscribe.service.ts
@@ -2,19 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import AppConfig from '../../../../../../appConfig';
 import { Observable } from 'rxjs';
-export interface Color {
-  name: string;
-  hex: string;
-  darkContrast: boolean;
-}
-
-
 @Injectable()
 export class AlertUnsubscribeService {
   constructor(private _http: HttpClient) {}
 
   unsubscribeAnAlert(token): Observable<any> {
-    const url = AppConfig.login.url;
+    console.log(token);
+    const url = AppConfig.api.url;
+
     return this._http.get(`${url}/alerts/subscriber/deactivate?token=${token}`);
   }
 
