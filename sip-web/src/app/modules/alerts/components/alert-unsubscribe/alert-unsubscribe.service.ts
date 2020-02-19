80a4af0b2f8fb9b4e33d22a32f39f5453b1c311b
Anirudh Venkatesh <Anirudh.NV@Synchronoss.com>
2020-02-19 19:27:25 +0530

    SIP-6333: adding observables

--- sip-web/src/app/modules/alerts/components/alert-unsubscribe/alert-unsubscribe.service.ts
+++ sip-web/src/app/modules/alerts/components/alert-unsubscribe/alert-unsubscribe.service.ts
@@ -1,22 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import AppConfig from '../../../../../../appConfig';
-// import { Observable } from 'rxjs';
+import { Observable } from 'rxjs';
+
 @Injectable()
 export class AlertUnsubscribeService {
   constructor(private _http: HttpClient) {}
 
-  unsubscribeAnAlert(token) {
+  unsubscribeAnAlert(token): Observable<any> {
     const url = AppConfig.api.url;
     const endpoint = `${url}/alerts/subscriber/deactivate?token=${token}`;
     const headers = new HttpHeaders().set('Content-Type', 'text/plain; charset=utf-8');
     return this._http
-      .post(endpoint, '',  { headers, responseType: 'text' as 'json'})
-      .toPromise()
-      .then(response => {
-        console.log(response);
-        return response;
-      });
+      .post(endpoint, '',  { headers, responseType: 'text' as 'json'});
   }
-
 }
