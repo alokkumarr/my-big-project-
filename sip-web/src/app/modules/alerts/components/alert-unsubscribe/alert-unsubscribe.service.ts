99b76e12f9db4cd002fa79c8a383ee2c5f12e2bf
Anirudh Venkatesh <Anirudh.NV@Synchronoss.com>
2020-02-19 17:55:24 +0530

    SIP-6333: accept response as string instead of json

--- sip-web/src/app/modules/alerts/components/alert-unsubscribe/alert-unsubscribe.service.ts
+++ sip-web/src/app/modules/alerts/components/alert-unsubscribe/alert-unsubscribe.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import AppConfig from '../../../../../../appConfig';
 // import { Observable } from 'rxjs';
 @Injectable()
@@ -7,17 +7,16 @@ export class AlertUnsubscribeService {
   constructor(private _http: HttpClient) {}
 
   unsubscribeAnAlert(token) {
-    console.log(token);
     const url = AppConfig.api.url;
     const endpoint = `${url}/alerts/subscriber/deactivate?token=${token}`;
+    const headers = new HttpHeaders().set('Content-Type', 'text/plain; charset=utf-8');
     return this._http
-      .post(endpoint, '')
+      .post(endpoint, '',  { headers, responseType: 'text' as 'json'})
       .toPromise()
       .then(response => {
         console.log(response);
         return response;
       });
-    // return this._http.post(endpoint, httpOptions);
   }
 
 }
