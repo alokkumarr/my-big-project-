fb27e9e6608ea9979232816b13ed7677e8937164
jvad0003 <Jeevan.YadavNS@synchronoss.com>
2020-02-26 10:56:06 +0530

    resolved conflicts

---
+++ sip-web/e2e/v2/conf/protractor.conf.js
@@ -0,0 +1,241 @@
+'use strict';
+var appRoot = require('app-root-path');
+var retry = require('protractor-retry').retry;
+var fs = require('fs');
+var argv = require('yargs').argv;
+const SuiteSetup = require('../helpers/SuiteSetup');
+const logger = require('./logger')(__filename);
+const testSuites = require('./testSuites');
+const Constants = require('../helpers/Constants');
+/**
+ * Sets the amount of time to wait for a page load to complete before returning an error.  If the timeout is negative,
+ * page loads may be indefinite.
+ * maximum time to wait for a page load i.e. when user refresh page/ first time open browser and type saw app url
+ */
+const pageLoadTimeout = 30000; //-- !!!DON'T CHANGE because it will increase overall test execution time
+
+const implicitlyWait = 1000; //should not be more than 5 seconds, this will impact over all execution time
+
+/**
+ * This is maximum time to wait for an element visible
+ * if element is not visible within this time then protractor will throw timeout error & mark test as failure
+ * and continue to execute other test.
+ * 30 seconds in docker and 20 seconds in local
+ */
+const fluentWait = SuiteSetup.distRun() ? 30000 : 20000; //-- !!!DON'T CHANGE because it will impact overall test execution time
+
+/**
+ * Before performing any action, Protractor waits until there are no pending asynchronous tasks in your Angular
+ * application. This means that all timeouts and http requests are finished.
+ * https://github.com/angular/protractor/blob/master/docs/timeouts.md
+ * https://github.com/angular/protractor/blob/master/lib/config.ts
+ * 3 min -- All http/https calls should be completed -- !!!DON'T CHANGE because it will impact overall test execution time
+ */
+const allScriptsTimeout = 180000;
+
+/**
+ *  https://github.com/angular/protractor/blob/master/docs/timeouts.md
+ * https://github.com/angular/protractor/blob/master/lib/config.ts
+ * Max total time before throwing NO ACTIVE SESSION_ID
+ * 60 min
+ */
+const timeoutInterval = 3600000;
+
+/**
+ * number of retries in case of failure, executes all failed tests
+ */
+let maxRetriesForFailedTests = SuiteSetup.distRun() ? 3 : 2;
+
+/**
+ * Waits ms after page is loaded
+ */
+const pageResolveTimeout = 1000;
+let run = SuiteSetup.islocalRun();
+const browserGetPageTimeout = 300000; // 5 min as sometime because of network slowness it can cause issues
+
+/**
+ * All tests are running for customer
+ */
+const customerCode = 'SYNCHRONOSS';
+
+exports.timeouts = {
+  fluentWait: fluentWait,
+  pageResolveTimeout: pageResolveTimeout,
+  timeoutInterval: timeoutInterval
+};
+
+exports.config = {
+  framework: 'jasmine2',
+  allScriptsTimeout: allScriptsTimeout,
+  customerCode: customerCode,
+  useAllAngular2AppRoots: true,
+  directConnect: true,
+  getPageTimeout: browserGetPageTimeout,
+  baseUrl: 'http://localhost:3000',
+  capabilities: {
+    browserName: 'chrome',
+    shardTestFiles: true,
+    maxInstances: 10,
+    chromeOptions: {
+      args: [
+        'disable-extensions',
+        'disable-web-security',
+        '--start-fullscreen', // enable for Mac OS
+        '--headless', // start on background
+        '--disable-gpu',
+        '--window-size=2880,1800'
+      ]
+    },
+    'moz:firefoxOptions': {
+      args: ['--headless']
+    }
+  },
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: timeoutInterval,
+    isVerbose: true,
+    showTiming: true,
+    includeStackTrace: true,
+    realtimeFailure: true,
+    showColors: true
+  },
+  suites: {
+    smoke: testSuites.SMOKE,
+    sanity: testSuites.SANITY,
+    /**
+     * This is default suite contains basic functionality aorund analyze + observe module
+     * Gets triggered on each push
+     */
+    critical: testSuites.CRITICAL,
+    /**
+     * This suite is full regression and is triggered by e2e bamboo plan i.e
+     * https://bamboo.synchronoss.net:8443/browse/BDA-TSA
+     */
+    regression: testSuites.REGRESSION,
+    /**
+     * This suite is for developing new tests and quickly debug if something breaking
+     */
+    development: testSuites.DEVELOPMENT
+  },
+  onCleanUp: results => {
+    retry.onCleanUp(results);
+  },
+  onPrepare: () => {
+    retry.onPrepare();
+
+    browser
+      .manage()
+      .timeouts()
+      .implicitlyWait(implicitlyWait);
+
+    // Allure reporter start
+    let AllureReporter = require('jasmine-allure-reporter');
+    jasmine.getEnv().addReporter(
+      new AllureReporter({
+        resultsDir: 'target/allure-results'
+      })
+    );
+    // Add screenshot to allure report. screenshot are taken after each test
+    jasmine.getEnv().afterEach(function(done) {
+      browser.takeScreenshot().then(function(png) {
+        allure.createAttachment(
+          'Screenshot',
+          function() {
+            return new Buffer(png, 'base64');
+          },
+          'image/png'
+        )();
+        done();
+      });
+    });
+    // Allure reporter done
+
+    // Get failed test data
+    jasmine.getEnv().addReporter(
+      new (function() {
+        this.specDone = function(result) {
+          if (result.status !== 'passed') {
+            logger.debug('Test is failed: ' + JSON.stringify(result.testInfo));
+            new SuiteSetup().failedTestData(result.testInfo);
+          }
+          // Add executed test status in a result.json file which contains pass and fail tests
+          // This fill be used for converting to junit xml so that e2e results can display under test status in bamboo
+          new SuiteSetup().addToExecutedTests(result);
+        };
+      })()
+    );
+
+    browser.baseUrl = JSON.parse(fs.readFileSync(Constants.E2E_OUTPUT_BASE_DIR + '/url.json', 'utf8')).baseUrl;
+
+    browser.get(browser.baseUrl);
+    return browser.wait(() => {
+      return browser.getCurrentUrl().then(url => {
+        return /login/.test(url);
+      });
+    }, pageResolveTimeout);
+  },
+  beforeLaunch: () => {
+    process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+    if (!run.localRun || (run.localRun && run.firstRun)) {
+      logger.info('Doing cleanup and setting up test data for e2e tests....');
+      // Generate test data
+      let appUrl = SuiteSetup.getSawWebUrl();
+      if (!appUrl) {
+        logger.error(
+          'appUrl can not be null or undefined hence exiting the e2e suite...appUrl:' +
+            appUrl +
+            ', hence exiting test suite and failing it...'
+        );
+        process.exit(1);
+      }
+
+      try {
+        logger.info('Generating test for this run...');
+
+        let APICommonHelpers = require('../helpers/api/APICommonHelpers');
+
+        let apiBaseUrl = APICommonHelpers.getApiUrl(appUrl);
+        let token = APICommonHelpers.generateToken(apiBaseUrl);
+        if (!token) {
+          logger.error(
+            'cleanup and setup stage : Token generation failed hence marking test suite failure, Please refer the logs for more information.'
+          );
+          process.exit(1);
+        }
+        let TestDataGenerator = require('../helpers/data-generation/TestDataGenerator');
+        new TestDataGenerator().generateUsersRolesPrivilegesCategories(apiBaseUrl, token);
+      } catch (e) {
+        logger.error(
+          'There is some error during cleanup and setting up test data for e2e tests, ' +
+            'hence exiting test suite and failing it....' +
+            e
+        );
+        process.exit(1);
+      }
+    }
+  },
+  afterLaunch: () => {
+    process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 1;
+    // Delete old e2e unique id.
+    if (!run.localRun) {
+      if (fs.existsSync('target/e2e/e2eId.json')) {
+        // delete and create new always
+        fs.unlinkSync('target/e2e/e2eId.json');
+      }
+      SuiteSetup.failedTestDataForRetry();
+
+      let retryStatus = retry.afterLaunch(maxRetriesForFailedTests);
+      if (retryStatus === 1) {
+        // retryStatus 1 means there are some failures & there are no retry left, hence mark test suite failure
+        logger.error('There are some failures hence marking test suite failed');
+        // TODO: Convert testResult json to junit.xml file
+        SuiteSetup.convertJsonToJunitXml();
+        process.exit(1); // this will mark build failure as well
+      } else if (retryStatus === 0) {
+        // retryStatus 0 means there are no failures
+        // TODO: Convert testResult json to junit.xml file
+        SuiteSetup.convertJsonToJunitXml();
+      }
+      return retryStatus;
+    }
+  }
+};
---
+++ sip-web/e2e/v2/helpers/SuiteSetup.js
@@ -0,0 +1,486 @@
+'use strict';
+
+const path = require('path');
+var fs = require('fs');
+var convert = require('xml-js');
+const globalVariables = require('../helpers/data-generation/globalVariables');
+const logger = require('../conf/logger')(__filename);
+const Constants = require('../helpers/Constants');
+const Utils = require('./Utils');
+class SuiteSetup {
+  /* Return true if end-to-end tests are run against distribution
+   * package built with Maven and deployed to a local Docker container
+   * (as happens for example on the Bamboo continuous integration
+   * server), as opposed to a local saw-web front-end development
+   * server
+   * */
+  static distRun() {
+    if (!process.env.PWD) {
+      process.env.PWD = process.cwd();
+    }
+    return process.env.PWD.endsWith('/dist');
+  }
+
+  root(...args) {
+    return path.join(process.cwd(), ...args);
+  }
+
+  failedTestData(testInfo) {
+    logger.debug('writing failed test cases to failure object');
+    let failedTestsData = {};
+    if (!fs.existsSync(Constants.E2E_OUTPUT_BASE_DIR + '/retry')) {
+      fs.mkdirSync(Constants.E2E_OUTPUT_BASE_DIR + '/retry', {
+        recursive: true
+      });
+    }
+
+    if (
+      fs.existsSync(
+        Constants.E2E_OUTPUT_BASE_DIR + '/retry/failedTestData.json'
+      )
+    ) {
+      let existingFailures = JSON.parse(
+        fs.readFileSync(
+          Constants.E2E_OUTPUT_BASE_DIR + '/retry/failedTestData.json',
+          'utf8'
+        )
+      );
+      // There are already failed tests so add to existing list
+      logger.debug('existingFailures---' + JSON.stringify(existingFailures));
+      // add new failures to existing
+      this.writeToJsonFile(
+        existingFailures,
+        testInfo,
+        Constants.E2E_OUTPUT_BASE_DIR + '/retry/failedTestData.json'
+      );
+    } else {
+      logger.debug('first failure... ');
+      // Write new failed test list json file;
+      this.writeToJsonFile(
+        failedTestsData,
+        testInfo,
+        Constants.E2E_OUTPUT_BASE_DIR + '/retry/failedTestData.json'
+      );
+    }
+  }
+
+  passTestData(testInfo) {
+    logger.debug('writing pass test cases to success object');
+    let passTestsData = {};
+
+    if (!fs.existsSync(Constants.E2E_OUTPUT_BASE_DIR + '/result')) {
+      fs.mkdirSync(Constants.E2E_OUTPUT_BASE_DIR + '/result', {
+        recursive: true
+      });
+    }
+
+    if (
+      fs.existsSync(Constants.E2E_OUTPUT_BASE_DIR + '/result/passTestData.json')
+    ) {
+      let existingPassTests = JSON.parse(
+        fs.readFileSync(
+          Constants.E2E_OUTPUT_BASE_DIR + '/result/passTestData.json',
+          'utf8'
+        )
+      );
+      // There are already failed tests so add to existing list
+      logger.debug(
+        'existingSuccess tests---' + JSON.stringify(existingPassTests)
+      );
+      // add new failures to existing
+      this.writeToJsonFile(
+        existingPassTests,
+        testInfo,
+        Constants.E2E_OUTPUT_BASE_DIR + '/result/passTestData.json'
+      );
+    } else {
+      logger.debug('first success test... ');
+      // Write new pass test list json file;
+      this.writeToJsonFile(
+        passTestsData,
+        testInfo,
+        Constants.E2E_OUTPUT_BASE_DIR + '/result/passTestData.json'
+      );
+    }
+  }
+
+  writeToJsonFile(testDataObject, testInfo, fileLocation) {
+    if (!testDataObject[testInfo.feature]) {
+      testDataObject[testInfo.feature] = {};
+    }
+    if (!testDataObject[[testInfo.feature]][[testInfo.dataProvider]]) {
+      testDataObject[[testInfo.feature]][[testInfo.dataProvider]] = {};
+    }
+    if (
+      !testDataObject[[testInfo.feature]][[testInfo.dataProvider]][
+        [testInfo.testId]
+      ]
+    ) {
+      testDataObject[[testInfo.feature]][[testInfo.dataProvider]][
+        [testInfo.testId]
+      ] = testInfo.data;
+      fs.writeFileSync(fileLocation, JSON.stringify(testDataObject), {
+        encoding: 'utf8'
+      });
+    }
+  }
+
+  addToExecutedTests(currentResult) {
+    let testResultStatus = {};
+
+    if (!fs.existsSync(Constants.E2E_OUTPUT_BASE_DIR + '/result')) {
+      fs.mkdirSync(Constants.E2E_OUTPUT_BASE_DIR + '/result', {
+        recursive: true
+      });
+    }
+    if (
+      fs.existsSync(Constants.E2E_OUTPUT_BASE_DIR + '/result/testResult.json')
+    ) {
+      testResultStatus = JSON.parse(
+        fs.readFileSync(
+          Constants.E2E_OUTPUT_BASE_DIR + '/result/testResult.json',
+          'utf8'
+        )
+      );
+      // There are already some test exits then add new one or replace the existing one with latest result
+      logger.silly('existingResult---' + JSON.stringify(testResultStatus));
+    } else {
+      logger.silly('first test result... ');
+    }
+    testResultStatus[currentResult.testInfo.testId] = currentResult;
+    fs.writeFileSync(
+      Constants.E2E_OUTPUT_BASE_DIR + '/result/testResult.json',
+      JSON.stringify(testResultStatus),
+      { encoding: 'utf8' }
+    );
+  }
+
+  static readAllData(dir = null, currentSuite = null) {
+    let completeTestData = {};
+    let location;
+    if (dir) {
+      location = dir;
+    } else {
+      location = '../sip-web/e2e/v2/testdata/all-tests';
+    }
+    const dirCont = fs.readdirSync(location);
+    const files = dirCont.filter(elm => /.*\.(json)/gi.test(elm));
+    files.forEach(function(file) {
+      logger.info('Reading file for executing test suite: ' + file);
+      let data = JSON.parse(fs.readFileSync(location + '/' + file, 'utf8'));
+      completeTestData = Object.assign(data, completeTestData);
+    });
+    // Filter the data based on suite
+    if (currentSuite) {
+      completeTestData = this.filterDataBySuite(completeTestData, currentSuite);
+    }
+    return completeTestData;
+  }
+
+  // Filter the data and return only matching records
+  // If suites property doesn't exist then it means this test applicable for all suite
+  static filterDataBySuite(data, currentsuite) {
+    Object.keys(data).forEach(feature => {
+      Object.keys(data[feature]).forEach(dp => {
+        Object.keys(data[feature][dp]).forEach(testcase => {
+          if (data[feature][dp][testcase].hasOwnProperty('suites')) {
+            if (!data[feature][dp][testcase].suites.includes(currentsuite)) {
+              delete data[feature][dp][testcase];
+            }
+          }
+        });
+      });
+    });
+
+    return data;
+  }
+
+  static failedTestDataForRetry() {
+    logger.info('Generating failed test data set for next retry from failures');
+
+    if (
+      fs.existsSync(
+        Constants.E2E_OUTPUT_BASE_DIR + '/retry/failedTestDataForRetry.json'
+      )
+    ) {
+      fs.unlinkSync(
+        Constants.E2E_OUTPUT_BASE_DIR + '/retry/failedTestDataForRetry.json'
+      );
+    }
+    if (
+      fs.existsSync(
+        Constants.E2E_OUTPUT_BASE_DIR + '/retry/failedTestData.json'
+      )
+    ) {
+      fs.renameSync(
+        Constants.E2E_OUTPUT_BASE_DIR + '/retry/failedTestData.json',
+        Constants.E2E_OUTPUT_BASE_DIR + '/retry/failedTestDataForRetry.json'
+      );
+      logger.debug(
+        'Old failures json file is deleted and converted to failure data set ' +
+          'i.e. ' +
+          Constants.E2E_OUTPUT_BASE_DIR +
+          '/retry/failedTestData.json converted to ' +
+          Constants.E2E_OUTPUT_BASE_DIR +
+          '/retry/failedTestDataForRetry.json'
+      );
+    } else if (
+      !fs.existsSync(Constants.E2E_OUTPUT_BASE_DIR + '/result/testResult.json')
+    ) {
+      // if result is not created then it means something is wrong.
+      logger.error('oopps....!!! There are unexpected failures!');
+    } else {
+      logger.info('Yahooo....!!! There are no failures!');
+    }
+    // Delete the old file so that it can be again re-rewritten and used by another set of failures in next retry
+    if (
+      fs.existsSync(
+        Constants.E2E_OUTPUT_BASE_DIR + '/retry/failedTestData.json'
+      )
+    ) {
+      fs.unlinkSync(
+        Constants.E2E_OUTPUT_BASE_DIR + '/retry/failedTestData.json'
+      );
+      logger.debug(
+        'Old failures json file is deleted and converted to failure data set'
+      );
+    }
+  }
+
+  static getSawWebUrl() {
+    let url;
+    let isHttps;
+
+    if (!fs.existsSync('target')) {
+      fs.mkdirSync('target');
+    }
+    if (!fs.existsSync(Constants.E2E_OUTPUT_BASE_DIR)) {
+      fs.mkdirSync(Constants.E2E_OUTPUT_BASE_DIR);
+    }
+
+    if (fs.existsSync(Constants.E2E_OUTPUT_BASE_DIR + '/url.json')) {
+      url = JSON.parse(
+        fs.readFileSync(Constants.E2E_OUTPUT_BASE_DIR + '/url.json', 'utf8')
+      ).baseUrl;
+    } else {
+      process.argv.forEach(function(val) {
+        if (val.includes('--isHttps')) {
+          isHttps = val.split('=')[1];
+          return;
+        }
+      });
+
+      process.argv.forEach(function(val) {
+        if (val.includes('--baseUrl')) {
+          url = val.split('=')[1];
+          let urlObject = {
+            baseUrl: Utils.getUrl(url, isHttps)
+          };
+          url = urlObject.baseUrl;
+          fs.writeFileSync(
+            Constants.E2E_OUTPUT_BASE_DIR + '/url.json',
+            JSON.stringify(urlObject),
+            {
+              encoding: 'utf8'
+            }
+          );
+          return;
+        }
+      });
+    }
+    logger.info('Application url used for running e2e: ' + url);
+    return url;
+  }
+
+  static islocalRun() {
+    let localRunDetails = {
+      localRun: false,
+      firstRun: false
+    };
+
+    if (!fs.existsSync('target')) {
+      fs.mkdirSync('target');
+    }
+    if (!fs.existsSync(Constants.E2E_OUTPUT_BASE_DIR)) {
+      fs.mkdirSync(Constants.E2E_OUTPUT_BASE_DIR);
+    }
+
+    if (fs.existsSync(Constants.E2E_OUTPUT_BASE_DIR + '/localRun.json')) {
+      localRunDetails = {
+        localRun: true,
+        firstRun: false
+      };
+
+      fs.writeFileSync(
+        Constants.E2E_OUTPUT_BASE_DIR + '/localRun.json',
+        JSON.stringify(localRunDetails),
+        {
+          encoding: 'utf8'
+        }
+      );
+    } else {
+      process.argv.forEach(function(val) {
+        if (val.includes('--localRun')) {
+          localRunDetails = {
+            localRun: true,
+            firstRun: true
+          };
+          fs.writeFileSync(
+            Constants.E2E_OUTPUT_BASE_DIR + '/localRun.json',
+            JSON.stringify(localRunDetails),
+            {
+              encoding: 'utf8'
+            }
+          );
+          return;
+        }
+      });
+    }
+    if (localRunDetails.localRun) {
+      if (!localRunDetails.firstRun) {
+        logger.warn(
+          'End to end tests are running locallly but this is not first run hence disabling data generation & retry'
+        );
+      } else {
+        logger.warn(
+          'End to end tests are running locallly but this is first run hence enabling data generation'
+        );
+      }
+    } else {
+      logger.warn(
+        'End to end tests are running with retry and data generation enabled'
+      );
+    }
+    return localRunDetails;
+  }
+
+  static getTestData() {
+    if (
+      fs.existsSync(
+        Constants.E2E_OUTPUT_BASE_DIR + '/retry/failedTestDataForRetry.json'
+      )
+    ) {
+      let data = JSON.parse(
+        fs.readFileSync(
+          Constants.E2E_OUTPUT_BASE_DIR + '/retry/failedTestDataForRetry.json',
+          'utf8'
+        )
+      );
+      logger.warn(
+        'This is retry execution!! Executing with failed test data set, because failedTestDataForRetry found in  ' +
+          Constants.E2E_OUTPUT_BASE_DIR +
+          '/retry/failedTestDataForRetry.json: data--->' +
+          JSON.stringify(data)
+      );
+      return data;
+    } else {
+      let suiteName;
+
+      if (!fs.existsSync(Constants.E2E_OUTPUT_BASE_DIR)) {
+        fs.mkdirSync(Constants.E2E_OUTPUT_BASE_DIR, {
+          recursive: true
+        });
+      }
+
+      if (fs.existsSync(Constants.E2E_OUTPUT_BASE_DIR + '/suite.json')) {
+        suiteName = JSON.parse(
+          fs.readFileSync(Constants.E2E_OUTPUT_BASE_DIR + '/suite.json', 'utf8')
+        ).suiteName;
+      } else {
+        process.argv.forEach(function(val) {
+          if (val.includes('--suite')) {
+            suiteName = val.split('=')[1];
+            let suiteObject = {
+              suiteName: suiteName
+            };
+            fs.writeFileSync(
+              Constants.E2E_OUTPUT_BASE_DIR + '/suite.json',
+              JSON.stringify(suiteObject),
+              {
+                encoding: 'utf8'
+              }
+            );
+            return;
+          }
+        });
+      }
+      if (suiteName) {
+        const suite = suiteName.trim().toLowerCase();
+        logger.warn(`Executing with ${suite} suite test data set.....`);
+        return SuiteSetup.readAllData(null, suite);
+      } else {
+        logger.warn('Executing with critical suite test data set....');
+        return SuiteSetup.readAllData(null, 'critical');
+      }
+    }
+  }
+
+  static convertJsonToJunitXml() {
+    if (
+      fs.existsSync(Constants.E2E_OUTPUT_BASE_DIR + '/result/testResult.json')
+    ) {
+      let testResultStatus = JSON.parse(
+        fs.readFileSync(
+          Constants.E2E_OUTPUT_BASE_DIR + '/result/testResult.json',
+          'utf8'
+        )
+      );
+
+      let totalTests = Object.keys(testResultStatus).length;
+      let failedCount = 0;
+      let failedTests = '';
+      let passedTests = '';
+
+      for (let key in testResultStatus) {
+        if (testResultStatus.hasOwnProperty(key)) {
+          if (testResultStatus[key].status.toLowerCase() === 'failed') {
+            failedCount++;
+            failedTests += `<testcase classname="${Utils.replaceSpecialCharsNotAllowedInXml(
+              testResultStatus[key].fullName
+            )}" name="${Utils.replaceSpecialCharsNotAllowedInXml(
+              testResultStatus[key].description
+            )}">
+                                    <failure type="exception" message="${Utils.replaceSpecialCharsNotAllowedInXml(
+                                      testResultStatus[key]
+                                        .failedExpectations[0].message
+                                    )}">
+                                     <![CDATA[${
+                                       testResultStatus[key]
+                                         .failedExpectations[0].stack
+                                     }]]>
+                                    </failure>
+                                </testcase>`;
+          } else if (testResultStatus[key].status.toLowerCase() === 'passed') {
+            passedTests += `<testcase classname="${Utils.replaceSpecialCharsNotAllowedInXml(
+              testResultStatus[key].fullName
+            )}" name="${Utils.replaceSpecialCharsNotAllowedInXml(
+              testResultStatus[key].description
+            )}" />`;
+          }
+        }
+      }
+      if (totalTests > 0) {
+        let suiteName = JSON.parse(
+          fs.readFileSync(Constants.E2E_OUTPUT_BASE_DIR + '/suite.json', 'utf8')
+        ).suiteName;
+        let testSuiteStart = `	<testsuite name="executed suite: ${suiteName}" timestamp="${new Date()}" failures="${failedCount}" tests="${totalTests}">`;
+        let testSuiteEnd = `</testsuite>`;
+        let xmlDocument = `<?xml version="1.0" encoding="UTF-8" ?>`;
+        xmlDocument += testSuiteStart;
+        xmlDocument += failedTests;
+        xmlDocument += passedTests;
+
+        xmlDocument += testSuiteEnd;
+        logger.debug('xmlDocument e2e tests: ' + xmlDocument);
+
+        fs.writeFileSync(
+          Constants.E2E_OUTPUT_BASE_DIR + '/myjunit.xml',
+          xmlDocument,
+          { encoding: 'utf8' }
+        );
+      }
+    }
+  }
+}
+
+module.exports = SuiteSetup;
