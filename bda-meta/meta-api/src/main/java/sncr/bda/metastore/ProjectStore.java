31c09c60f6b96a04a9732e5f5f8149ecdb02e24a
Gopinath Ramisetty <gopinath.ramisetty@synchronoss.com>
2020-02-26 15:20:43 -0700

    SIP-8963 : Adding Tags to Dataset and searching datasets with Tags

--- bda-meta/meta-api/src/main/java/sncr/bda/metastore/ProjectStore.java
+++ bda-meta/meta-api/src/main/java/sncr/bda/metastore/ProjectStore.java
@@ -15,6 +15,7 @@ import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * Created by srya0001 on 12/1/2017.
@@ -25,6 +26,7 @@ public class ProjectStore extends MetadataStore implements WithSearchInMetastore
     private static String TABLE_NAME = "projects";
     public static final String PLP = "projectLevelParameters";
     protected JsonParser jsonParser;
+    public static final String TAGS = "allowableTags";
 
     public ProjectStore(String altRoot) throws Exception {
         super(TABLE_NAME, altRoot);
@@ -40,26 +42,51 @@ public class ProjectStore extends MetadataStore implements WithSearchInMetastore
 
 
     public void createProjectRecord(String name, String desc, Map<String, String> p) throws Exception {
-        if (name == null || name.isEmpty())
-            throw new IllegalArgumentException("Project name cannot be null or empty");
-        JsonObject jo = new JsonObject();
-        jo.add("description", new JsonPrimitive((desc == null || desc.isEmpty())?"no description":desc));
-        JsonArray plp = new JsonArray();
-        for ( String propKey: p.keySet()){
-            JsonObject plpjo = new JsonObject();
-            jo.add(propKey, new JsonPrimitive(p.get(propKey)));
-            plp.add( plpjo );
-        }
-        jo.add(PLP, plp);
-        create(name, jo);
+        this.createProjectRecord(name, Optional.ofNullable(desc), Optional.empty(),  Optional.ofNullable(p));
     }
 
     public void createProjectRecord(String name, String desc, JsonArray p) throws Exception {
+        HashMap<String, String> nParams = new HashMap<>();
+        p.forEach( el -> {
+            JsonObject jo = el.getAsJsonObject();
+            if (!jo.has("name") || !jo.has("value")) {
+                logger.error("Invalid parameter entry in parameter set. Skip it");
+            }
+            else{
+                nParams.put(jo.get("name").getAsString(), jo.get("value").getAsString());
+            }
+        });
+        this.createProjectRecord(name, Optional.ofNullable(desc), Optional.empty(),  Optional.ofNullable(nParams));
+    }
+
+    public void createProjectRecord(String name, Optional<String> optDesc, Optional<String[]> optTags, Optional<Map<String, String>> optParameters) throws Exception {
         if (name == null || name.isEmpty())
             throw new IllegalArgumentException("Project name cannot be null or empty");
         JsonObject jo = new JsonObject();
-        jo.add("description", new JsonPrimitive((desc == null || desc.isEmpty())?"no description":desc));
-        jo.add(PLP, p);
+        if(optDesc.isPresent()){
+            String desc = optDesc.get().trim();
+            jo.add("description", new JsonPrimitive(desc.isEmpty()?"no description":desc));
+        }
+        if(optTags.isPresent()){
+            String[] tags = optTags.get();
+            JsonArray tagArray = new JsonArray();
+            for ( String tag : tags){
+                if(tag != null && !tag.trim().isEmpty()){
+                    tagArray.add(new JsonPrimitive(tag.trim()));
+                }
+            }
+            jo.add(TAGS, tagArray);
+        }
+        if(optParameters.isPresent()){
+            Map<String, String> parameters = optParameters.get();
+            JsonArray plp = new JsonArray();
+            for ( String propKey: parameters.keySet()){
+                JsonObject plpjo = new JsonObject();
+                jo.add(propKey.trim(), new JsonPrimitive(parameters.get(propKey)));
+                plp.add( plpjo );
+            }
+            jo.add(PLP, plp);
+        }
         create(name, jo);
     }
 
@@ -100,54 +127,75 @@ public class ProjectStore extends MetadataStore implements WithSearchInMetastore
 */
 
     public void updateProjectRecord(String name, Map<String, String> newParameters) throws Exception {
+        this.updateProjectRecord(name, Optional.empty(), Optional.empty(),  Optional.ofNullable(newParameters));
+    }
+
+    public void updateProjectRecord(String name, Optional<String> optNewDesc, Optional<String[]> optNewTags, Optional<Map<String, String>> optNewParameters) throws Exception {
         Document prj = table.findById(name);
         if (prj == null)
-             throw new Exception("ProjectService with name: " + name + " not found");
+            throw new Exception("ProjectService with name: " + name + " not found");
 
-        JsonArray je = new JsonArray();
         JsonObject fromDoc = this.toJsonElement(prj).getAsJsonObject();
-        JsonArray plp = fromDoc.getAsJsonArray(PLP);
-        if (plp == null ) {
-            JsonArray finalPlp = new JsonArray();
-            newParameters.forEach((k, v) ->{
-                JsonObject o = new JsonObject();
-                o.add("name", new JsonPrimitive(k));
-                o.add("value", new JsonPrimitive(v));
-                finalPlp.add(o);
-            });
-            _updatePath(name, null, PLP, finalPlp);
+
+        if(optNewDesc.isPresent()) {
+            String desc = optNewDesc.get().trim();
+            _updatePath(name, null, "description", new JsonPrimitive(desc.isEmpty()?"no description":desc));
         }
-        else {
-            JsonArray newPLP = new JsonArray();
-            newParameters.forEach((k, v) -> {
+
+        if(optNewTags.isPresent()){
+            String[] tags = optNewTags.get();
+            JsonArray newTags = new JsonArray();
+            for ( String tag : tags){
+                if(tag != null && !tag.trim().isEmpty()){
+                    newTags.add(new JsonPrimitive(tag.trim()));
+                }
+            }
+            _updatePath(name, null, TAGS, newTags);
+        }
+
+        if(optNewParameters.isPresent()){
+            JsonArray plp = fromDoc.getAsJsonArray(PLP);
+            if (plp == null ) {
+                JsonArray finalPlp = new JsonArray();
+                optNewParameters.get().forEach((k, v) ->{
+                    JsonObject o = new JsonObject();
+                    o.add("name", new JsonPrimitive(k));
+                    o.add("value", new JsonPrimitive(v));
+                    finalPlp.add(o);
+                });
+                _updatePath(name, null, PLP, finalPlp);
+            }
+            else {
+                JsonArray newPLP = new JsonArray();
+                optNewParameters.get().forEach((k, v) -> {
                         JsonObject newEntry;
                         newEntry = new JsonObject();
                         newEntry.add("name", new JsonPrimitive(k));
                         newEntry.add("value", new JsonPrimitive(v));
                         newPLP.add(newEntry);
                     }
-            );
-            plp.forEach( plpel ->
-            {
-                JsonObject plpJo = plpel.getAsJsonObject();
-                if (!plpJo.has("name")){
-                  logger.error("Incorrect parameter entry, ignore it");
-                }
-                else{
-                    boolean found = false;
-                    for (JsonElement el: newPLP) {
-                        if (el.getAsJsonObject()
-                            .get("name")
-                            .getAsString()
-                            .equalsIgnoreCase(plpJo.get("name").getAsString())) {
-                            found = true;
-                            break;
+                );
+                plp.forEach(plpel ->
+                {
+                    JsonObject plpJo = plpel.getAsJsonObject();
+                    if (!plpJo.has("name")) {
+                        logger.error("Incorrect parameter entry, ignore it");
+                    } else {
+                        boolean found = false;
+                        for (JsonElement el : newPLP) {
+                            if (el.getAsJsonObject()
+                                .get("name")
+                                .getAsString()
+                                .equalsIgnoreCase(plpJo.get("name").getAsString())) {
+                                found = true;
+                                break;
+                            }
                         }
+                        if (!found) newPLP.add(plpJo);
                     }
-                    if (!found) newPLP.add(plpJo);
-                }
-            });
-            _updatePath(name, null, PLP, plp);
+                });
+                _updatePath(name, null, PLP, newPLP);
+            }
         }
         table.flush();
     }
