bd2a50e7b0f3125216ef787228a83fa1c93d9eaa
Gopinath Ramisetty <gopinath.ramisetty@synchronoss.com>
2020-03-02 12:34:41 -0700

    SIP-8963 : Adding category to Dataset and searching datasets with category

--- sip-xdf/xdf-parser/src/main/java/sncr/xdf/parser/NGParser.java
+++ sip-xdf/xdf-parser/src/main/java/sncr/xdf/parser/NGParser.java
@@ -971,6 +971,7 @@ public class NGParser extends AbstractComponent implements WithDLBatchWriter, Wi
         NGParser component = null;
         int rc= 0;
         Exception exception = null;
+        ComponentConfiguration cfg = null;
         try {
             long start_time = System.currentTimeMillis();
 
@@ -1004,7 +1005,7 @@ public class NGParser extends AbstractComponent implements WithDLBatchWriter, Wi
                 ComponentServices.TransformationMetadata,
                 ComponentServices.Spark,
             };
-            ComponentConfiguration cfg = NGParser.analyzeAndValidate(configAsStr);
+            cfg = NGParser.analyzeAndValidate(configAsStr);
             ngCtxSvc = new NGContextServices(pcs, xdfDataRootSys, cfg, appId, "parser", batchId);
             ngCtxSvc.initContext();
             ngCtxSvc.registerOutputDataSet();
@@ -1023,7 +1024,7 @@ public class NGParser extends AbstractComponent implements WithDLBatchWriter, Wi
         }catch (Exception ex) {
             exception = ex;
         }
-        rc = NGComponentUtil.handleErrors(Optional.ofNullable(component), rc, exception);
+        rc = NGComponentUtil.handleErrors(Optional.ofNullable(component), Optional.ofNullable(cfg), rc, exception);
         System.exit(rc);
     }
     
