91ceedaaddb2a205ad8ee55be7d869a743d85f83
Akash Agrawal <Akash.Agarwal@synchronoss.com>
2020-03-18 14:04:48 +0530

    SIP-8642: Add publish option to save

--- sip-web/src/app/modules/analyze/designer/save/designer-save.component.ts
+++ sip-web/src/app/modules/analyze/designer/save/designer-save.component.ts
@@ -5,10 +5,15 @@ import {
   USER_ANALYSIS_SUBCATEGORY_NAME
 } from '../../../../common/consts';
 import { JwtService } from '../../../../common/services';
-import { validateEntityName,
+import {
+  validateEntityName,
   entityNameErrorMessage
 } from './../../../../common/validators/field-name-rule.validator';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatCheckboxChange } from '@angular/material';
+import { AnalyzeService } from '../../services/analyze.service';
+import { PRIVILEGES } from '../../consts';
+import * as find from 'lodash/find';
 
 @Component({
   selector: 'designer-save',
@@ -17,31 +22,36 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class DesignerSaveComponent implements OnInit {
   @Output() public nameChange: EventEmitter<string> = new EventEmitter();
   @Output() public descriptionChange: EventEmitter<string> = new EventEmitter();
+  @Output() public categoryChange: EventEmitter<number> = new EventEmitter();
   @Input() public analysis: Analysis;
   @Input() public designerMode: string;
+
+  publishingToOtherCategory = false;
+  categories = [];
 
   userCategoryName = USER_ANALYSIS_CATEGORY_NAME;
   userSubCategoryName = USER_ANALYSIS_SUBCATEGORY_NAME;
 
-  public categories;
   public saveForm: FormGroup;
 
-  constructor(private jwtService: JwtService,
+  constructor(
+    private jwtService: JwtService,
     public fb: FormBuilder,
-    ) {}
+    private analyzeService: AnalyzeService
+  ) {}
 
   ngOnInit() {
     this.saveForm = this.fb.group({
-      name: [this.analysis.name, [Validators.required,
-        Validators.maxLength(30)],
+      name: [
+        this.analysis.name,
+        [Validators.required, Validators.maxLength(30)],
         this.validatePattern
       ]
     });
 
-    this.analysis.categoryId =
-      this.designerMode === 'new'
-        ? this.jwtService.userAnalysisCategoryId
-        : this.analysis.categoryId;
+    this.analyzeService.getCategories(PRIVILEGES.PUBLISH).then(response => {
+      this.categories = response;
+    });
   }
 
   displayErrorMessage(state) {
@@ -51,7 +61,7 @@ export class DesignerSaveComponent implements OnInit {
   validatePattern(control) {
     return new Promise((resolve, reject) => {
       if (/[`~!@#$%^&*()+={}|"':;?/>.<,*:/?[\]\\]/g.test(control.value)) {
-          resolve({ nameIsInValid: true });
+        resolve({ nameIsInValid: true });
       } else {
         resolve(null);
       }
@@ -72,5 +82,25 @@ export class DesignerSaveComponent implements OnInit {
 
   onDescriptionChange(description) {
     this.descriptionChange.emit(description);
+  }
+
+  onCategoryChange($event) {}
+
+  onPublishToggle({ checked }: MatCheckboxChange) {
+    this.publishingToOtherCategory = checked;
+
+    if (!checked) {
+      this.categoryChange.emit(this.jwtService.userAnalysisCategoryId);
+    } else {
+      const category = find(
+        this.categories,
+        cat => cat.children && cat.children.length > 0
+      );
+      if (category) {
+        this.categoryChange.emit(category.children[0].id);
+      } else {
+        this.categoryChange.emit(this.jwtService.userAnalysisCategoryId);
+      }
+    }
   }
 }
