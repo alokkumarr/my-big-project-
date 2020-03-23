c462f1dab2f7db00f0590affb0d78fab3a1088fc
Anirudh Venkatesh <Anirudh.NV@Synchronoss.com>
2020-03-23 11:39:17 +0530

    SIP-10171: fixing 401 error when accessed from analyze module

--- sip-web/src/app/common/dsk-filter-group/dsk-filter-group.component.ts
+++ sip-web/src/app/common/dsk-filter-group/dsk-filter-group.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { Router } from '@angular/router';
 import {
   DSKFilterGroup,
   DSKFilterField,
@@ -74,13 +75,16 @@ export class DskFilterGroupComponent implements OnInit {
 
   constructor(
     jwtService: JwtService,
-    dskFilterService: DskFiltersService
+    dskFilterService: DskFiltersService,
+    router: Router
   ) {
+    if (!(router.url).includes('/analyze/')) {
       dskFilterService
       .getEligibleDSKFieldsFor(jwtService.customerId, jwtService.productId)
       .subscribe(fields => {
         this.dskEligibleFields = fields;
       });
+    }
   }
 
   ngOnInit() {
