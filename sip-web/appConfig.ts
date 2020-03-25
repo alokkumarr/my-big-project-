06d899103107144267a2db75d254971aa1d58ce3
Anirudh Venkatesh <Anirudh.NV@Synchronoss.com>
2020-03-25 13:25:03 +0530

    SIP-10171: updating docker ip

--- sip-web/appConfig.ts
+++ sip-web/appConfig.ts
@@ -14,7 +14,7 @@ if (environment.production) {
   apiUrl = window.location.origin;
 } else {
   // Note: To run against a local Docker environment, use localhost
-  apiUrl = 'http://3.90.58.226';
+  apiUrl = 'http://18.209.62.141';
   // apiUrl = 'https://saw-cert-sip-vaste.sncrcorp.net';
   // apiUrl = 'http://saw-rd601.ana.dev.vaste.sncrcorp.net';
   // apiUrl = 'http://saw-rd602.ana.dev.vaste.sncrcorp.net';
