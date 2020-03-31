5612f1d0a480bbabf357caa4f9e1a315ac612afc
jvad0003 <Jeevan.YadavNS@synchronoss.com>
2020-03-31 11:26:09 +0530

    removed unused import

--- sip-web/e2e/v2/tests/charts/createAndDelete.test.js
+++ sip-web/e2e/v2/tests/charts/createAndDelete.test.js
@@ -12,7 +12,6 @@ const LoginPage = require('../../pages/LoginPage');
 const AnalyzePage = require('../../pages/AnalyzePage');
 const ChartDesignerPage = require('../../pages/ChartDesignerPage');
 const ExecutePage = require('../../pages/ExecutePage');
-const Constants = require('../../helpers/Constants');
 const users = require('../../helpers/data-generation/users');
 
 describe('Executing create and delete chart tests from charts/createAndDelete.test.js', () => {
