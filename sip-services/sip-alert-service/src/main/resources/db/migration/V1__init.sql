99521d0f31f540b741daba051342f79a7e257040
Sunil Kumar B M <sunilkumar.bm@synchronoss.com>
2020-04-02 11:48:36 +0530

    SIP-10164: Channel Value type changed

--- sip-services/sip-alert-service/src/main/resources/db/migration/V1__init.sql
+++ sip-services/sip-alert-service/src/main/resources/db/migration/V1__init.sql
@@ -13,7 +13,7 @@ CREATE TABLE `NOTIFICATION_SUBSCRIBER`
   SUBSCRIBER_ID     VARCHAR(50)  NOT NULL COMMENT 'Unique subscriber id',
   SUBSCRIBER_NAME   VARCHAR(100) COMMENT 'Name of the subscriber',
   CHANNEL_TYPE      VARCHAR(20) NOT NULL COMMENT 'Type of the notification channel',
-  CHANNEL_VALUE     TEXT  NOT NULL COMMENT 'Channel value in stringified json form',
+  CHANNEL_VALUE     VARCHAR(1000) UNIQUE NOT NULL COMMENT 'Channel value in stringified json form',
   CUSTOMER_CODE     VARCHAR(50) NOT NULL COMMENT 'Customer for which subscriber is associated with',
   ACTIVE            TINYINT(4) NOT NULL DEFAULT 1 COMMENT 'Indicates whether subscriber is active or not',
   CREATED_TIME      DATETIME NULL COMMENT 'Created timestamp',
