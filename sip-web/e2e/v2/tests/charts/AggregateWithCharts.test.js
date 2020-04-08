1b61a209492251251d9be00e14127d7dc018b06a
jvad0003 <Jeevan.YadavNS@synchronoss.com>
2020-04-08 13:12:24 +0530

    Modified Pivot and Chart Test Cases

--- sip-web/e2e/v2/tests/charts/AggregateWithCharts.test.js
+++ sip-web/e2e/v2/tests/charts/AggregateWithCharts.test.js
@@ -70,7 +70,7 @@ describe('Executing Aggregate for charts tests from charts/AggregateWithCharts.t
         const chartDescription = `e2e chart description ${now}`;
         const loginPage = new LoginPage();
         loginPage.loginAs(data.user, /analyze/);
-
+        browser.sleep(5000);
         const analyzePage = new AnalyzePage();
         analyzePage.clickOnAddAnalysisButton();
         analyzePage.clickOnChartType(data.chartType);
@@ -94,8 +94,12 @@ describe('Executing Aggregate for charts tests from charts/AggregateWithCharts.t
         }
         chartDesignerPage.clickOnDataOptionsTab();
         chartDesignerPage.clickOnDataOptions(dataOption);
-        chartDesignerPage.clickOnSelectAndChooseAggregate(selectedAggregate); // Open the aggregate-chooser component.
-        chartDesignerPage.clickOnSelectAndChooseAggregate(data.aggregate.value); // Select the aggregate form the list.
+        if(data.chartType === 'chart:scatter') {
+        chartDesignerPage.clickAndChooseAggregateForScatterChart(data.aggregate.value);
+        }else {
+          chartDesignerPage.clickOnSelectAndChooseAggregate(selectedAggregate); // Open the aggregate-chooser component.
+          chartDesignerPage.clickOnSelectAndChooseAggregate(data.aggregate.value); // Select the aggregate form the list.
+        }
 
         // Save the analysis
         chartDesignerPage.clickOnSave();
