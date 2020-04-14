2b6306c9869a337e90b9b54813fcb323c72d70ec
Anirudh Venkatesh <Anirudh.NV@Synchronoss.com>
2020-04-14 20:15:02 +0530

    SIP-10171: fixing promot validation errors

--- sip-web/src/app/common/components/filter-prompt-layout/filter-prompt-layout.component.ts
+++ sip-web/src/app/common/components/filter-prompt-layout/filter-prompt-layout.component.ts
@@ -88,7 +88,11 @@ export class FilterPromptLayout implements OnInit {
   resetFilters(tree) {
     for (var i in tree) {
       if (tree[i].model) {
-        delete tree[i].model;
+        if (this.data.isInRuntimeMode) {
+          if (tree[i].isRuntimeFilter) {
+            delete tree[i].model;
+          }
+        }
       }
       if (typeof tree[i] == 'object') this.resetFilters(tree[i])
     }
