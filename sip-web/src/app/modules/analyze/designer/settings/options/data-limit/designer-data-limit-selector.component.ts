17af8cca91d1eef1164c6a425e03e854ad1714c7
Anirudh Venkatesh <Anirudh.NV@Synchronoss.com>
2020-04-17 13:48:48 +0530

    SIP-10018: adding validations to dimensio select

--- sip-web/src/app/modules/analyze/designer/settings/options/data-limit/designer-data-limit-selector.component.ts
+++ sip-web/src/app/modules/analyze/designer/settings/options/data-limit/designer-data-limit-selector.component.ts
@@ -5,6 +5,7 @@ import * as debounce from 'lodash/debounce';
 import { Store } from '@ngxs/store';
 import { ArtifactColumnChart, DesignerChangeEvent } from '../../../types';
 import { DesignerUpdateArtifactColumn } from '../../../actions/designer.actions';
+import * as isEmpty from 'lodash/isEmpty';
 
 const MAX_LIMIT = 99;
 const LIMIT_DEBOUNCE_DELAY = 400;
@@ -15,7 +16,9 @@ const LIMIT_DEBOUNCE_DELAY = 400;
 })
 export class DesignerDataLimitSelectorComponent implements OnInit {
   @Output() change: EventEmitter<DesignerChangeEvent> = new EventEmitter();
+  @Output() stateChange: EventEmitter<any> = new EventEmitter();
   @Input() artifactColumn: ArtifactColumnChart;
+  @Input() state: boolean;
   public limitType;
   public limitValue;
   public isInTabletMode = false;
@@ -44,6 +47,8 @@ export class DesignerDataLimitSelectorComponent implements OnInit {
   onLimitDataChange(value, type) {
     this.limitValue = value;
     this.limitType = type;
+
+    this.state = isEmpty(this.limitType) ||  isEmpty(this.limitValue);
     this.resetInvalidValueIfNeeded(this.limitValue);
     if (
       !this.limitType ||
@@ -74,6 +79,7 @@ export class DesignerDataLimitSelectorComponent implements OnInit {
       })
     );
     this.change.emit({ subject: 'fetchLimit' });
+    this.stateChange.emit({ data: this.state });
   }
 
   resetInvalidValueIfNeeded(value) {
