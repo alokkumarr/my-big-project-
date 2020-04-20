8349d2806815d22558ec398f812547687e11ce2c
alok.kumarr <alok.kumarr@synchronoss.com>
2020-04-20 16:40:22 +0530

    SIP-6195 - Merge conflicts and get header row logic

--- sip-xdf/xdf-parser/src/main/java/sncr/xdf/parser/spark/HeaderMapFilter.java
+++ sip-xdf/xdf-parser/src/main/java/sncr/xdf/parser/spark/HeaderMapFilter.java
@@ -24,13 +24,13 @@ public class HeaderMapFilter implements Function<String, Iterable<String>> {
   @Override
   public Iterable<String> call(String s) {
     List<String> list = Arrays.asList(s.split(lineSeparator));
-    if(headerLength < 1){
+    if (headerLength < 1) {
       throw new XDFException(XDFReturnCode.CONFIG_ERROR, "headerSize should not be less than 1.");
-    }else if(fieldDefRowNumber != null && fieldDefRowNumber < 0){
+    } else if (fieldDefRowNumber != null && fieldDefRowNumber < 0) {
       throw new XDFException(XDFReturnCode.CONFIG_ERROR, "fieldDefRowNumber should not be less than 0.");
-    }else if(fieldDefRowNumber != null && headerLength < fieldDefRowNumber){
+    } else if (fieldDefRowNumber != null && headerLength < fieldDefRowNumber) {
       throw new XDFException(XDFReturnCode.CONFIG_ERROR, "fieldDefRowNumber should not be greater than headerSize");
-    } else if (HEADER_ROW == null && headerLength == 1 && list.size() > 0) {
+    } else if (HEADER_ROW == null && headerLength == 1 && list != null && !list.isEmpty()) {
       HEADER_ROW = list.get(headerLength - 1);
     }
     return list.stream().skip(headerLength).collect(Collectors.toList());
